0814e829b01fa621a598e4ea16b948db03d18695
ssa1 <>
2006-09-11 15:57:04 +0000

    Recreating the path that is found.

--- GraphMatch/LinkedNode.h
+++ GraphMatch/LinkedNode.h
@@ -32,6 +32,7 @@ public:
 	unsigned long long GetN1Bitmap();
 	unsigned long long GetN2Bitmap();
 	bool operator==(LinkedNode &other);
+	bool IsUserSpecifiedSolution();
 	static void AddNodeToBitmap(unsigned long long & bitmap, int node);
 	static void RemoveNodeFromBitmap(unsigned long long & bitmap, int node);
 	static bool IsNodeInBitmap(unsigned long long bitmap, int node);
@@ -129,8 +130,18 @@ void LinkedNode::PrintNodeConcise(int rank, bool endOfLine, bool printCostBreakd
 		n2[i] = temp;
 	}
 
+#ifdef VERBOSE
+	if(IsUserSpecifiedSolution()) {
+		printf("*");
+	} else {
+		printf(" ");
+	}
+#else
+	printf(" ");
+#endif
+
 	if(rank != -1) {
-		printf(" %d)", rank);
+		printf("%d)", rank);
 	}
 	printf("\t");
 	for(int i = 0; i < top; i++) {
@@ -144,6 +155,61 @@ void LinkedNode::PrintNodeConcise(int rank, bool endOfLine, bool printCostBreakd
 	if(endOfLine) {
 		printf("\n");
 	}
+}
+
+bool LinkedNode::IsUserSpecifiedSolution() {
+	bool used[MAX_NODES];
+	int n1[MAX_NODES];
+	int n2[MAX_NODES];
+	int top = 0;
+	for(int i = 0; i < MAX_NODES; i++) {
+		used[i] = false;
+	}
+
+	LinkedNodeStub * currentNode = this;
+	bool continueLoop = true;
+	while(continueLoop) {
+		if(currentNode->parentNode == NULL) {
+			 break;
+		}
+		n1[top] = currentNode->n1Node;
+		n2[top] = currentNode->n2Node;
+		used[currentNode->n1Node] = true;
+		top++;
+		currentNode = currentNode->parentNode;		
+	}
+
+	for(int i = 1; i <= this->depth; i++) {
+		if(!used[i]) {
+			n1[top] = i;
+			n2[top] = -1;
+			top++;
+		}
+	}
+
+	int minIndex;
+	int temp;
+	for(int i = 0; i < top - 1; i++) {
+		minIndex = i;
+		for(int j = i+1; j < top; j++) {
+			if(n1[minIndex] > n1[j]) {
+				minIndex = j;
+			}
+		}
+		temp = n1[minIndex];
+		n1[minIndex] = n1[i];
+		n1[i] = temp;
+
+		temp = n2[minIndex];
+		n2[minIndex] = n2[i];
+		n2[i] = temp;
+	}
+
+	bool isSolution = true;
+	for(int i = 0; i < top; i++) {
+		isSolution = (isSolution && (n2[i] == SOLUTION[i]));
+	}
+	return isSolution;
 }
 
 unsigned long long LinkedNode::GetN1Bitmap() {
