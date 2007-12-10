468d7a7d6d0328958e6774aae5141aae03376366
ssa1 <>
2007-12-10 19:34:16 +0000

    Time manager to get timing

--- GraySkeletonCPP/TimeManager.h
+++ GraySkeletonCPP/TimeManager.h
@@ -9,12 +9,25 @@ using namespace std;
 
 namespace wustl_mm {
 	namespace GraySkeletonCPP {
+		struct stopwatch_rec {
+			clock_t startTime;
+			double cumulatedTime;
+		};
+
 		class TimeManager {
 		public:			
 			TimeManager();
 			void PushCurrentTime();
 			void PopAndDisplayTime(string textFormat);
+
+			int StartStopWatch();
+			void PauseStopWatch(int id);
+			void ResumeStopWatch(int id);
+			void DisplayStopWatch(int id, string textFormat);
+
+		private:
 			vector<clock_t> times;
+			vector<stopwatch_rec> stopWatches;
 		};
 
 		TimeManager::TimeManager() {
@@ -31,6 +44,28 @@ namespace wustl_mm {
 			double timeTaken = ((double) (finish - start) / (double) CLOCKS_PER_SEC);
 			printf(textFormat.c_str(), timeTaken);
 			times.pop_back();			
+		}
+
+		int TimeManager::StartStopWatch() {
+			stopwatch_rec rec;
+			rec.startTime = clock();
+			rec.cumulatedTime = 0;		
+			stopWatches.push_back(rec);
+			return stopWatches.size()-1;
+		}
+
+		void TimeManager::PauseStopWatch(int id) {
+			clock_t start = stopWatches[id].startTime;
+			clock_t finish = clock();			
+			stopWatches[id].cumulatedTime += ((double) (finish - start) / (double) CLOCKS_PER_SEC);			
+		}
+
+		void TimeManager::ResumeStopWatch(int id) {
+			stopWatches[id].startTime = clock();
+		}
+
+		void TimeManager::DisplayStopWatch(int id, string textFormat){			
+			printf(textFormat.c_str(), stopWatches[id].cumulatedTime);
 		}
 	}
 }
