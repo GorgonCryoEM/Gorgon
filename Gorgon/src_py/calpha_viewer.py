0b3e7c78fbfaa3a8242eb89753caac23ba332bd8
ssa1 <>
2008-04-16 23:37:18 +0000

    c-alpha atom support

---
+++ Gorgon/src_py/calpha_viewer.py
@@ -0,0 +1,70 @@
+# Author:      Sasakthi S. Abeysinghe (sasakthi@gmail.com)
+# Description: This viewer displays c-alpha atoms
+
+from PyQt4 import QtGui, QtCore, QtOpenGL
+from gorgon_cpp_wrapper import CAlphaRenderer
+from base_viewer import BaseViewer
+
+try:
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
+    from OpenGL.GLUT import *
+except ImportError:
+    app = QtGui.QApplication(sys.argv)
+    QtGui.QMessageBox.critical(None, "Gorgon", "PyOpenGL must be installed to run Gorgon.", QtGui.QMessageBox.Ok | QtGui.QMessageBox.Default, QtGui.QMessageBox.NoButton)
+    sys.exit(1)
+
+class CAlphaViewer(BaseViewer):
+    def __init__(self, main, parent=None):
+        BaseViewer.__init__(self, main, parent)
+        self.title = "C-Alpha"
+        self.isClosedMesh = False
+        self.renderer = CAlphaRenderer()          
+        self.createUI()      
+        self.app.viewers["calpha"] = self;
+        self.modelColor = QtGui.QColor.fromRgba(QtGui.qRgba(170, 170, 0, 255))
+        self.model2Color = QtGui.QColor.fromRgba(QtGui.qRgba(120, 120, 170, 255))
+        self.model2Visible = True
+        self.initVisualizationOptions()
+        self.visualizationOptions.ui.checkBoxModelVisible.setText("Show atoms colored:")
+        self.visualizationOptions.ui.checkBoxModel2Visible.setText("Show backbone colored:")
+        self.visualizationOptions.ui.checkBoxModel2Visible.setVisible(True)
+        self.visualizationOptions.ui.pushButtonModel2Color.setVisible(True)        
+                 
+    def createUI(self):
+        self.createActions()
+        self.createMenus()
+        self.createChildWindows()
+        self.updateActionsAndMenus()
+                  
+    def createChildWindows(self):
+        pass
+        
+    def createActions(self):
+        openAct = QtGui.QAction(self.tr("C-&Alpha Atoms..."), self)
+        openAct.setShortcut(self.tr("Ctrl+A"))
+        openAct.setStatusTip(self.tr("Load a C-Alpha atom file"))
+        self.connect(openAct, QtCore.SIGNAL("triggered()"), self.loadData)
+        self.app.actions.addAction("load_CAlpha", openAct)
+        
+        saveAct = QtGui.QAction(self.tr("C-&Alpha Atoms..."), self)
+        saveAct.setStatusTip(self.tr("Save a C-Alpha atom file"))
+        self.connect(saveAct, QtCore.SIGNAL("triggered()"), self.saveData)
+        self.app.actions.addAction("save_CAlpha", saveAct)                
+        
+        closeAct = QtGui.QAction(self.tr("C-&Alpha Atoms"), self)
+        closeAct.setStatusTip(self.tr("Close the loaded C-Alpha atom file"))
+        self.connect(closeAct, QtCore.SIGNAL("triggered()"), self.unloadData)
+        self.app.actions.addAction("unload_CAlpha", closeAct)
+                                
+    def createMenus(self):
+        self.app.menus.addAction("file-open-calpha", self.app.actions.getAction("load_CAlpha"), "file-open")    
+        self.app.menus.addAction("file-save-calpha", self.app.actions.getAction("save_CAlpha"), "file-save")
+        self.app.menus.addAction("file-close-calpha", self.app.actions.getAction("unload_CAlpha"), "file-close");
+        self.app.menus.addMenu("actions-calpha", self.tr("C-&Alpha Atoms"), "actions");
+                   
+    def updateActionsAndMenus(self):        
+        self.app.actions.getAction("save_CAlpha").setEnabled(self.loaded)
+        self.app.actions.getAction("unload_CAlpha").setEnabled(self.loaded)
+        self.app.menus.getMenu("actions-calpha").setEnabled(self.loaded)        
+             
