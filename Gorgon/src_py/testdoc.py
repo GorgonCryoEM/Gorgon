c8bed2c7c278e75152a44283c79b3e8451788bda
ssa1 <>
2008-06-18 17:50:30 +0000

    comment 3

--- Gorgon/src_py/testdoc.py
+++ Gorgon/src_py/testdoc.py
@@ -1,15 +1,18 @@
 # Copyright (C) 1994-99 Wired Digital Inc.  All rights reserved.
 #
-# File:      $Source $
-# Revision:  $Revision $
-# Date:      $Date $
+# File:      $Source$
+# Revision:  $Revision$
+# Date:      $Date$
 # Author:      $Author$
 # Status:      $State$
 #
 # History Log: 
 #   $Log$
+#   Revision 1.2  2008/06/18 17:48:13  ssa1
+#   adding in test reviesion 2
+#
 #   Revision 1.1  2008/06/18 17:47:12  ssa1
 #   Testing CVS Automatic comments
 #
 
-a b c d e f g adsf as fsadf asfdasdf asdf adf asdf
+a b c d e f g adsf as fsadf asfdasdf asdf adf asdf asdf sad f
