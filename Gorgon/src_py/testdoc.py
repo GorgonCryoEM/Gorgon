137d058bc86d06ad79ebb678d75c8449d34cfd80
ssa1 <>
2008-06-18 17:47:12 +0000

    Testing CVS Automatic comments

---
+++ Gorgon/src_py/testdoc.py
@@ -0,0 +1,12 @@
+# Copyright (C) 1994-99 Wired Digital Inc.  All rights reserved.
+#
+# File:      $Source $
+# Revision:  $Revision $
+# Date:      $Date $
+# Author:      $Author$
+# Status:      $State$
+#
+# History Log: 
+#   $Log$
+
+a b c d e f g
