8318e19dd202fefe0b553d65f48bca12ef79626d
ssa1 <>
2008-06-18 17:48:13 +0000

    adding in test reviesion 2

--- Gorgon/src_py/testdoc.py
+++ Gorgon/src_py/testdoc.py
@@ -8,5 +8,8 @@
 #
 # History Log: 
 #   $Log$
+#   Revision 1.1  2008/06/18 17:47:12  ssa1
+#   Testing CVS Automatic comments
+#
 
-a b c d e f g
+a b c d e f g adsf as fsadf asfdasdf asdf adf asdf
