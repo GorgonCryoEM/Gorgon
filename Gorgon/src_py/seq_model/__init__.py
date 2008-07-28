042799c6c88a43da5a625f010bfa95a05cdcad40
ssa1 <>
2008-07-28 16:23:34 +0000

    Adding __init__.py files into the two new modules.

---
+++ Gorgon/src_py/seq_model/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2005-2008 Washington University in St Louis, Baylor College of Medicine.  All rights reserved
+# Author:        Sasakthi S. Abeysinghe (sasakthi@gmail.com)
+# Description:   An initalization file for the seq_model module.
+
+# CVS Meta Information: 
+#   $Source$
+#   $Revision$
+#   $Date$
+#   $Author$
+#   $State$
+#
+# History Log: 
+#   $Log$
