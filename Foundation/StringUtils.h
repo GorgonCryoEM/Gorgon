042c21c067114c1b1c30049df9c312e7d05bfeb5
ssa1 <>
2008-09-29 15:45:09 +0000

    Adding in CVS meta information

--- Foundation/StringUtils.h
+++ Foundation/StringUtils.h
@@ -1,3 +1,17 @@
+// Copyright (C) 2005-2008 Washington University in St Louis, Baylor College of Medicine.  All rights reserved
+// Author:        Sasakthi S. Abeysinghe (sasakthi@gmail.com)
+// Description:   A class for string manipulation
+
+// CVS Meta Information: 
+//   $Source$
+//   $Revision$
+//   $Date$
+//   $Author$
+//   $State$
+//
+// History Log: 
+//   $Log$
+
 #ifndef FOUNDATION_STRING_UTILS_H
 #define FOUNDATION_STRING_UTILS_H
 
