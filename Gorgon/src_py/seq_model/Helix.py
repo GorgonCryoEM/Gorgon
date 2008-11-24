1f15ea347cb341b0d47e3a6e2b1d2a367784db01
colemanr <>
2008-11-24 21:06:14 +0000

    added method getAngstromLength

--- Gorgon/src_py/seq_model/Helix.py
+++ Gorgon/src_py/seq_model/Helix.py
@@ -15,49 +15,51 @@ except:
 from seq_model.Secel import Secel
 
 class Helix(Secel):
-  serialNo=0
-  def __init__(self, chain, serialNo, label, startIndex, stopIndex, color=None):
-    if qtEnabled and color==None:
-      color=QtGui.QColor(51,208,208)
-    Secel.__init__(self, chain, serialNo, label, startIndex, stopIndex, color)
-    self.type="helix"
+    serialNo=0
+    def __init__(self, chain, serialNo, label, startIndex, stopIndex, color=None):
+        if qtEnabled and color==None:
+          color=QtGui.QColor(51,208,208)
+        Secel.__init__(self, chain, serialNo, label, startIndex, stopIndex, color)
+        self.type="helix"
 
-
-  @classmethod
-  def parsePDB(cls,line,chain):
-    serialNo     =         int(line[7:10].strip())
-    helixID     =     line[11:14].strip()
-    #chainID     =         line[19:20]
-    start     =         int(line[21:25].strip())
-    stop     =         int(line[33:37].strip())
-
-    if helixID in chain.helices.keys():
-      raise ValueError, 'Duplicate Helix entries in PDB'
-    else:
-      #chain.helices[serialNo]=Helix(chain,serialNo,helixID,start,stop)
-      helix=Helix(chain,serialNo,'H' + helixID,start,stop)
-      chain.addHelix(serialNo, helix)
-
-  def toPDB(self):
-    Helix.serialNo=Helix.serialNo+1
-    init_res_name=self.chain.residueList[self.startIndex].symbol3
-    init_chainID=self.chain.chainID
-    init_seq_num=self.startIndex
-    end_res_name=self.chain.residueList[self.stopIndex].symbol3
-    end_seq_num=self.stopIndex
-
-    s= "HELIX  "
-    s=s+ str(Helix.serialNo).rjust(3) +' '
-    s=s+ self.label.lstrip('H').rjust(3) +' '
-    s=s+ init_res_name.rjust(3) +' '
-    s=s+ init_chainID.rjust(1) +' '
-    s=s+ str(init_seq_num).rjust(4) +' '
-
-    s=s+ end_res_name.rjust(4) +' '
-    s=s+ init_chainID.rjust(1) +' '
-    s=s+ str(end_seq_num).rjust(4) +' '
-    s=s+ str(1).rjust(1) +' '
-
-    s=s+ str(self.stopIndex-self.startIndex + 1 ).rjust(36) + "    \n"
-
-    return s
+    @classmethod
+    def parsePDB(cls,line,chain):
+        serialNo     =         int(line[7:10].strip())
+        helixID     =     line[11:14].strip()
+        #chainID     =         line[19:20]
+        start     =         int(line[21:25].strip())
+        stop     =         int(line[33:37].strip())
+    
+        if helixID in chain.helices.keys():
+          raise ValueError, 'Duplicate Helix entries in PDB'
+        else:
+          #chain.helices[serialNo]=Helix(chain,serialNo,helixID,start,stop)
+          helix=Helix(chain,serialNo,'H' + helixID,start,stop)
+          chain.addHelix(serialNo, helix)
+          
+    def getAngstromLength(self):
+        return 1.5*(1+self.stopIndex-self.startIndex)
+    
+    def toPDB(self):
+        Helix.serialNo=Helix.serialNo+1
+        init_res_name=self.chain.residueList[self.startIndex].symbol3
+        init_chainID=self.chain.chainID
+        init_seq_num=self.startIndex
+        end_res_name=self.chain.residueList[self.stopIndex].symbol3
+        end_seq_num=self.stopIndex
+    
+        s= "HELIX  "
+        s=s+ str(Helix.serialNo).rjust(3) +' '
+        s=s+ self.label.lstrip('H').rjust(3) +' '
+        s=s+ init_res_name.rjust(3) +' '
+        s=s+ init_chainID.rjust(1) +' '
+        s=s+ str(init_seq_num).rjust(4) +' '
+    
+        s=s+ end_res_name.rjust(4) +' '
+        s=s+ init_chainID.rjust(1) +' '
+        s=s+ str(end_seq_num).rjust(4) +' '
+        s=s+ str(1).rjust(1) +' '
+    
+        s=s+ str(self.stopIndex-self.startIndex + 1 ).rjust(36) + "    \n"
+    
+        return s
