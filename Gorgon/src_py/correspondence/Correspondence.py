3ba316dacd9de66c13aa1a6b22d52470e3d16a17
colemanr <>
2008-11-25 00:02:26 +0000

    fixed typo self.libary --> self.library

--- Gorgon/src_py/correspondence/Correspondence.py
+++ Gorgon/src_py/correspondence/Correspondence.py
@@ -11,7 +11,7 @@ class Correspondence:
   def __init__(self, library, matchList, score, comments=None, chain=None):
     self.matchList=matchList
     self.score=score
-    self.libary=library
+    self.library=library
     self.comments=None
     self.chain=None
 
