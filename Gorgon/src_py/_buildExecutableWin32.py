ecc7d21db95f599c613cf2b78b39587723abe874
ssa1 <>
2008-11-28 04:41:51 +0000

    Building executable for win32

---
+++ Gorgon/src_py/_buildExecutableWin32.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+      name = "Gorgon",
+      version = "1.0.0",
+      description = "Gorgon: A toolkit for protein structure reconstruction",
+      console=['gorgon.pyw'],
+      options={
+               "py2exe":{
+                         "includes" : ["sip", "PyQt4._qt", "ctypes", "distutils.util", "logging", "ctypes.util", "weakref"],
+                         "excludes" : ["OpenGL"]
+                         }
+               }
+      )
+
+
+#  For PyOpengl 3.0.0b6
+#"includes" : ["sip", "PyQt4._qt", "ctypes", "logging", "ctypes.util", "weakref"], 
