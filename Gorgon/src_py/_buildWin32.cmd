0b14cd105a2ee347ff78b9330343c07ef8949e06
ssa1 <>
2008-11-28 04:41:51 +0000

    Building executable for win32

---
+++ Gorgon/src_py/_buildWin32.cmd
@@ -0,0 +1,16 @@
+@echo off
+copy ..\..\ExternalLibraries\GL\glut32.dll > NUL
+copy "C:\Program Files\Mozilla Firefox\plugins\Microsoft.VC80.CRT\msvcr80.dll" > NUL
+copy "C:\Program Files\Mozilla Firefox\plugins\Microsoft.VC80.CRT\msvcp80.dll" > NUL
+
+python _buildExecutableWin32.py py2exe 
+
+copy msvcr80.dll dist\ > NUL
+copy msvcp80.dll dist\ > NUL
+del msvcr80.dll
+del msvcp80.dll
+del glut32.dll
+copy  C:\Programs\Python25\Lib\site-packages\setuptools-0.6c9-py2.5.egg dist\ > NUL
+md dist\pyopengl-3.0.0b4-py2.5.egg > NUL
+xcopy C:\Programs\Python25\Lib\site-packages\pyopengl-3.0.0b4-py2.5.egg\* dist\pyopengl-3.0.0b4-py2.5.egg\ /E /D > NUL
+
