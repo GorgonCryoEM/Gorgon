7d3767df7a434e2af368f2a1bed2137804a0fa49
colemanr <>
2008-12-20 02:00:39 +0000

    modified to use the new directory structure of Gorgon Linux binaries

--- Gorgon/src_py/gorgon-linux.sh
+++ Gorgon/src_py/gorgon-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=`pwd`:$PATH
-export PYTHONPATH=`pwd`:`pwd`/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
-python gorgon.pyw &
+export PATH=`pwd`/bin:$PATH
+export PYTHONPATH=`pwd`:`pwd`/lib:`pwd`/lib/python2.5:`pwd`/lib/python2.5/site-packages
+export LD_LIBRARY_PATH=`pwd`/lib
+python gorgon.pyw
