8894317a206ca4ac80cf90a712f0671eaebfc353
ssa1 <>
2009-03-31 20:08:45 +0000

    Refactoring: Renaming and moving ChooseChainModel to CAlphaChooseChainModel

--- Gorgon/src_py/calpha_choose_chain_model.py
+++ Gorgon/src_py/calpha_choose_chain_model.py
@@ -1,20 +1,25 @@
 # Copyright (C) 2005-2008 Washington University in St Louis, Baylor College of Medicine.  All rights reserved
 # Author:        Ross A. Coleman (racolema@bcm.edu)
-# Class: ChooseChainModel
-# Class Description: Widget that lets the user see what chain models are in memory and select the one to work with
+# Description:   Widget that lets the user see what chain models are in memory and select the one to work with
+
+# CVS Meta Information: 
+#   $Source$
+#   $Revision$
+#   $Date$
+#   $Author$
+#   $State$
+#
+# History Log: 
+#   $Log$
 
 import sys
 from PyQt4 import QtCore, QtGui
-from seq_model.ui_dialog_choose_chain_model import Ui_DialogChooseChainModel
+from ui_dialog_calpha_choose_chain_model import Ui_DialogCAlphaChooseChainModel
 from seq_model.Chain import Chain
 
-class ChooseChainModel(QtGui.QWidget):
-    """
-This widget displays the loaded chain models and allows the user to
-switch between models.
-    """
+class CAlphaChooseChainModel(QtGui.QWidget):
     def __init__(self, parent=None):
-        super(ChooseChainModel, self).__init__(parent)
+        super(CAlphaChooseChainModel, self).__init__(parent)
         self.app = parent
         self.createUi()
         self.createActions()
@@ -24,10 +29,6 @@ switch between models.
         self.connect(self.ui.chainModelsListWidget, QtCore.SIGNAL("currentTextChanged (const QString&)"), self.modelHighlighted)
     
     def acceptButtonPress(self):
-        """
-This function makes the highlighted model the current model. (Not yet 
-implemented.)
-        """
         pass
         
     def createActions(self):
@@ -43,7 +44,7 @@ implemented.)
         self.app.menus.addAction("actions-calpha-chooseModel", self.app.actions.getAction("perform_chooseModel"), "actions-calpha")
     
     def createUi(self):
-        self.ui = Ui_DialogChooseChainModel()
+        self.ui = Ui_DialogCAlphaChooseChainModel()
         self.ui.setupUi(self)
         self.ui.sequenceTextEdit.setReadOnly(True)
         self.dock = QtGui.QDockWidget(self.tr("Choose Chain Model"), self.app)
@@ -54,17 +55,9 @@ implemented.)
         self.ui.acceptPushButton.setEnabled(False)
     
     def dockVisibilityChanged(self, visible):
-        """
-This sets the checkmark in the menu to indicate whether this widget is 
-visible.
-        """
         self.app.actions.getAction("perform_chooseModel").setChecked(visible)
     
     def loadWidget(self):
-        """
-This refreshes the information in this widget and displays the widget, 
-unless its menu item is unchecked, in which case it hides the widget.
-        """
         if self.app.actions.getAction("perform_chooseModel").isChecked():
             self.app.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.dock)
             self.refresh()
@@ -73,10 +66,6 @@ unless its menu item is unchecked, in which case it hides the widget.
             self.app.removeDockWidget(self.dock)
     
     def modelHighlighted(self, chainQString):
-        """
-This responds to a model being highlighted by displaying the sequence
-of that model.
-        """
         currText = str(chainQString)
         currChainKey = tuple( currText.split(' - ') )
         currChain = Chain.getChain(currChainKey)
@@ -84,24 +73,9 @@ of that model.
         self.ui.sequenceTextEdit.setText(text)
         
     def refresh(self):
-        """
-This updates the list of chain models that are in memory. 
-        """
         self.ui.chainModelsListWidget.clear()
         chainKeys = Chain.getChainKeys()
         for key in chainKeys:
             item = str(key[0]) + ' - ' + str(key[1])
             self.ui.chainModelsListWidget.addItem(item)
-        
-        
-if __name__=='__main__':
-    app = QtGui.QApplication(sys.argv)
-    window = ChooseChainModel()
-    window.ui.chainModelsListWidget.addItem('aaa')
-    window.ui.chainModelsListWidget.addItem('bbb')
-    window.ui.chainModelsListWidget.addItem('ccc')
-    window.ui.chainModelsListWidget.addItem('ddd')
-    window.ui.chainModelsListWidget.addItem('eee')
-    window.ui.sequenceBrowser.setText('RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR')
-    window.show()
-    sys.exit(app.exec_())
+
