45905b4c41620ea80adf5afbe28028d0c6cd41d6
ssa1 <>
2009-04-01 16:01:38 +0000

    Refactoring: Splitting structure_editor into subclasses

---
+++ Gorgon/src_py/calpha_structure_editor_command_atom_placement.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2005-2008 Washington University in St Louis, Baylor College of Medicine.  All rights reserved
+# Author:        Ross A. Coleman (racolema@bcm.edu)
+# Description:   This class creates the QUndoCommand objects for the undo/redo stack.
+
+# CVS Meta Information: 
+#   $Source$
+#   $Revision$
+#   $Date$
+#   $Author$
+#   $State$
+#
+# History Log: 
+#   $Log$
+
+from PyQt4 import QtGui, QtCore
+from libpyGORGON import PDBAtom, PDBBond, Vector3DFloat
+from calpha_interactive_loop_builder import CAlphaInteractiveLoopBuilder
+from seq_model.findHelixCalphas import helixEndpointsToCAlphaPositions
+from seq_model.Helix import Helix
+import math
+from vector_lib import *
+
+class CAlphaStructureEditorCommandAtomPlacement(QtGui.QUndoCommand):
+    def __init__(self, currentChainModel, structureEditor, resSeqNum, chosenCoordinates, viewer, description=None):
+        super(CAlphaStructureEditorCommandAtomPlacement, self).__init__(description)
+        self.currentChainModel = currentChainModel
+        self.structureEditor = structureEditor
+        self.resSeqNum = resSeqNum
+        self.chosenCoordinates = chosenCoordinates
+        self.viewer = viewer
+        
+    def redo(self):
+        """
+In addition to being called to redo an action, this is called the first
+time the action occurs.
+        """
+        print self.chosenCoordinates
+        raw = PDBAtom(self.currentChainModel.getPdbID(), self.currentChainModel.getChainID(), self.resSeqNum, 'CA')
+        raw.setPosition(self.chosenCoordinates)
+        atom = self.viewer.renderer.addAtom(raw) 
+        print atom
+        self.currentChainModel[self.resSeqNum].addAtomObject(atom)
+        self.currentChainModel[self.resSeqNum].setCAlphaColorToDefault() 
+        bondBefore = None
+        bondAfter = None
+        if self.resSeqNum - 1 in self.currentChainModel.residueRange():
+            prevCAlpha = self.currentChainModel[self.resSeqNum - 1].getAtom('CA')
+            if prevCAlpha:
+                print "adding a bond before"
+                bondBefore=PDBBond()
+                bondBefore.setAtom0Ix(prevCAlpha.getHashKey())
+                bondBefore.setAtom1Ix(atom.getHashKey())
+        if self.resSeqNum + 1 in self.currentChainModel.residueRange():
+            nextCAlpha = self.currentChainModel[self.resSeqNum + 1].getAtom('CA')
+            if nextCAlpha:
+                print "adding a bond after"
+                bondAfter = PDBBond()
+                bondAfter.setAtom0Ix(nextCAlpha.getHashKey())
+                bondAfter.setAtom1Ix(atom.getHashKey())
+        
+        if bondBefore:
+            self.viewer.renderer.addBond(bondBefore)
+        if bondAfter:
+            self.viewer.renderer.addBond(bondAfter)
+        
+        self.viewer.emitModelChanged()
+        self.structureEditor.atomJustAdded = atom
+        
+        if self.structureEditor.atomicBackwardRadioButton.isChecked():
+            self.structureEditor.atomPrevButtonPress()
+        elif self.structureEditor.atomicForwardRadioButton.isChecked():
+            self.structureEditor.atomNextButtonPress()
+        
+    def undo(self):
+        print self.structureEditor.atomJustAdded
+        atom = self.currentChainModel[self.resSeqNum].getAtom('CA')
+        
+        try:
+            atomBefore = self.currentChainModel[self.resSeqNum-1].getAtom('CA')
+        except (IndexError, AttributeError, KeyError):
+            atomBefore = None
+        try:
+            atomAfter = self.currentChainModel[self.resSeqNum+1].getAtom('CA')
+        except (IndexError, AttributeError, KeyError):
+            atomAfter = None
+        
+        if atomBefore:
+            bondBeforeIx = self.viewer.renderer.getBondIndex(atomBefore.getHashKey(), atom.getHashKey())
+            self.viewer.renderer.deleteBond(bondBeforeIx)
+            pass
+        if atomAfter:
+            bondAfterIx = self.viewer.renderer.getBondIndex(atomAfter.getHashKey(), atom.getHashKey())
+            self.viewer.renderer.deleteBond(bondAfterIx)
+            pass
+            
+        self.currentChainModel[self.resSeqNum].clearAtom('CA')
+        self.viewer.renderer.deleteAtom(atom.getHashKey())
+        
+        self.viewer.emitModelChanged()
+        
+        if self.structureEditor.atomicBackwardRadioButton.isChecked():
+            self.structureEditor.atomNextButtonPress()
+        elif self.structureEditor.atomicForwardRadioButton.isChecked():
+            self.structureEditor.atomPrevButtonPress()
