7f71ec46339bbeca9430560f58f8cce0f3e37729
schuhs <>
2009-06-09 16:45:06 +0000

    Storing a list of corner cells for every sheet

--- Gorgon/src_py/correspondence/ObservedSheet.py
+++ Gorgon/src_py/correspondence/ObservedSheet.py
@@ -8,8 +8,17 @@
 #
 
 class ObservedSheet:
-  def __init__(self, params):
-    raise NotImplementedError, 'Not Yet Implemented: ObservedSheet contructor'
+  #def __init__(self, params):
+  def __init__(self, label, cornerList):
+    #raise NotImplementedError, 'Not Yet Implemented: ObservedSheet contructor'
+    """Note that the label is currently an integer, not a string."""
+    self.cornerList = cornerList
+    self.label = label #Note: currently an integer, not a string
+
+
+
+
+
 
   def __parseSheet(self, inputString):
     pass
