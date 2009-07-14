a3592529f99e2f852414e7966fb3f45720d6bde4
schuhs <>
2009-07-14 19:50:07 +0000

    Adding the total number of helices to the data structure

--- GraphMatch/SSECorrespondenceResult.h
+++ GraphMatch/SSECorrespondenceResult.h
@@ -11,6 +11,9 @@
 //
 // History Log: 
 //   $Log$
+//   Revision 1.4  2008/09/29 16:19:30  ssa1
+//   Adding in CVS meta information
+//
 
 #ifndef GRAPHMATCH_SSE_CORRESPONDENCE_RESULT_H
 #define GRAPHMATCH_SSE_CORRESPONDENCE_RESULT_H
@@ -27,33 +30,42 @@ namespace wustl_mm {
 		class SSECorrespondenceResult {
 		public:
 			SSECorrespondenceResult();
-			SSECorrespondenceResult(LinkedNode * node);
-			SSECorrespondenceResult(vector<int> correspondence, double cost);
+			SSECorrespondenceResult(LinkedNode * node, int helixCount);
+			SSECorrespondenceResult(vector<int> correspondence, double cost, int helixCount);
 			~SSECorrespondenceResult();
 
 			string GetNodeString();
 			double GetCost();
 			int GetNodeCount();
+			int GetHelixCount();
+			int GetSheetCount();
 			int GetSkeletonNode(int sequenceNode);
 			int NodeToHelix(int nodeId);
 		private:
 			vector<int> correspondence;
 			double cost;
+			int helixCount;
+			int sheetCount;
 		};
 
 		SSECorrespondenceResult::SSECorrespondenceResult(){
 			correspondence.clear();
 			cost = 0;
+			helixCount = 0;
+			sheetCount = 0;
 		}
 
-		SSECorrespondenceResult::SSECorrespondenceResult(LinkedNode * node){
+		//SSECorrespondenceResult::SSECorrespondenceResult(LinkedNode * node){
+		SSECorrespondenceResult::SSECorrespondenceResult(LinkedNode * node, int helixCount){
 			correspondence = node->GetNodeCorrespondence();
 			cost = node->GetCost();
+			this->helixCount = helixCount;
 		}
 
-		SSECorrespondenceResult::SSECorrespondenceResult(vector<int> correspondence, double cost){
+		SSECorrespondenceResult::SSECorrespondenceResult(vector<int> correspondence, double cost, int helixCount){
 			this->correspondence = correspondence;
 			this->cost = cost;
+			this->helixCount = helixCount;
 		}
 
 		SSECorrespondenceResult::~SSECorrespondenceResult(){
@@ -79,13 +91,26 @@ namespace wustl_mm {
 			return correspondence.size();
 		}
 
+		int SSECorrespondenceResult::GetHelixCount() {
+			return helixCount;
+		}
+
+		int SSECorrespondenceResult::GetSheetCount() {
+			// TODO: fix
+			return -1;
+		}
+
 		int SSECorrespondenceResult::GetSkeletonNode(int sequenceNode) {
 			return correspondence[sequenceNode];
 		}
 
 		int SSECorrespondenceResult::NodeToHelix(int nodeId) {
 			if(nodeId >= 0) {
-				return (int)(nodeId / 2);
+				if (nodeId < 2 * helixCount) {
+					return (int)(nodeId / 2);
+				} else {
+					return (int)(nodeId - helixCount);
+				}
 			} else {
 				return nodeId;
 			}
