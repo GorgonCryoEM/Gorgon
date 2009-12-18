703c085ceea721592ccb7a0ad43c09fd410312e4
schuhs <>
2009-12-18 22:06:01 +0000

    handle failed correspondence searches caused by memory error or too many constraints

--- Gorgon/src_py/sse_helix_correspondence_finder_form.py
+++ Gorgon/src_py/sse_helix_correspondence_finder_form.py
@@ -11,6 +11,9 @@
 #
 # History Log: 
 #   $Log$
+#   Revision 1.36.2.38  2009/12/18 19:31:01  schuhs
+#   Successfully catching memory exceptions on search. Getting errors when the search completes but no results found.
+#
 #   Revision 1.36.2.37  2009/12/17 19:10:08  schuhs
 #   Changing what the user sees if a search fails
 #
@@ -806,10 +809,10 @@ class SSEHelixCorrespondenceFinderForm(QtGui.QWidget):
             currentMatch = Match(observed, predicted, Match.FORWARD)
             currentMatch.constrained = False     
             matchList.append(currentMatch)                                              
-            corr = Correspondence(library=library, matchList=matchList, score=0)            
-
-            # add the empty correspondence to the list
-            corrList.append(corr)
+        corr = Correspondence(library=library, matchList=matchList, score=0)            
+        self.lastCorrespondence = corr # used in accept() method
+        # add the empty correspondence to the list
+        corrList.append(corr)
         return corrList
             
 
@@ -1001,14 +1004,12 @@ class SSEHelixCorrespondenceFinderForm(QtGui.QWidget):
     def accept(self):
         print "ok button pushed"
         
-        """
-        # save user settings in case this search fails
-        if len(self.viewer.correspondenceLibrary.correspondenceList) > 0:
-            print "saving set of constraints in case undo is needed"
+        # save the settings used to generate the last result, in case this search fails
+        if self.executed:
+            print "saving the currently selected correspondence, with constraints, in case undo is needed"
             lastCorrespondenceIndex = self.ui.comboBoxCorrespondences.currentIndex()
-            lastCorrespondence = self.viewer.correspondenceLibrary.correspondenceList[lastCorrespondenceIndex]
-            #self.viewer.correspondenceLibrary.correspondenceList = self.populateEmptyResults(self.viewer.correspondenceLibrary)
-        """
+            self.lastCorrespondence = self.viewer.correspondenceLibrary.correspondenceList[lastCorrespondenceIndex]
+        
         
         # read user parameters, read skeleton and sequence files, create correspondence library
         self.createBasicCorrespondence()   
@@ -1020,81 +1021,30 @@ class SSEHelixCorrespondenceFinderForm(QtGui.QWidget):
             self.resultCount = self.viewer.correspondenceEngine.executeQuery()
         except MemoryError:
             print "memory error"
+            # TODO: add popup with message about memory error
             self.resultCount=0
         self.viewer.correspondenceEngine.cleanupMemory()
+        self.viewer.correspondenceEngine.clearAllConstraints()
+
         print "found " + str(self.resultCount) + " results. cleaning up memory."
 
         if self.resultCount > 0:
-        
-            self.viewer.correspondenceEngine.clearAllConstraints()
-            
+            self.executed = True 
             # populate the list of found correspondences        
             print "populating result list"
             self.viewer.correspondenceLibrary.correspondenceList = self.populateResults(self.viewer.correspondenceLibrary)
-            self.populateComboBox(self.viewer.correspondenceLibrary)     
 
         else:
-            print "no results found"
-            #self.resultCount=0
-            # code to handle failed search
-            #self.viewer.correspondenceEngine.cleanupMemory()
-            #self.viewer.correspondenceLibrary.correspondenceList = self.populateEmptyResults(self.viewer.correspondenceLibrary)
-            #self.populateComboBox(self.viewer.correspondenceLibrary)     
-            self.viewer.correspondenceLibrary.correspondenceList = self.populateEmptyResults(self.viewer.correspondenceLibrary)
-            self.populateComboBox(self.viewer.correspondenceLibrary)     
-
-            
-        """
-        if self.resultCount > 0:
-            self.viewer.correspondenceEngine.clearAllConstraints()
-            
-            # populate the list of found correspondences        
-            print "populating result list"
-            self.viewer.correspondenceLibrary.correspondenceList = self.populateResults(self.viewer.correspondenceLibrary)
-            self.populateComboBox(self.viewer.correspondenceLibrary)     
-        else:
-            print "debug a"
+            # TODO: add popup with message about memory error or overconstraint problem
+            self.executed = False 
+            print "no results found. loading the most recent successful correspondence"
             self.viewer.correspondenceLibrary.correspondenceList = []
-            print "debug b, list has length " + str(len(self.viewer.correspondenceLibrary.correspondenceList))
-            self.viewer.correspondenceLibrary.correspondenceList.append(lastCorrespondence)
-            print "debug c, list has length " + str(len(self.viewer.correspondenceLibrary.correspondenceList))
-            #self.viewer.correspondenceLibrary.correspondenceList = self.populateResults(self.viewer.correspondenceLibrary)
-            #self.populateComboBox(self.viewer.correspondenceLibrary)     
-            print "no results found."
-            #self.viewer.correspondenceLibrary.correspondenceList = self.populateResults(self.viewer.correspondenceLibrary)
-            print "debug 1"
-            #self.populateComboBox(self.viewer.correspondenceLibrary)     
-            print "debug 1"
-            #self.ui.tableWidgetCorrespondenceList.clearContents()
-            print "debug 1"
-            #self.ui.tabWidget.setCurrentIndex(0)         
-            print "debug 1"
-            self.ui.comboBoxCorrespondences.setCurrentIndex(-1)
-            print "debug 1"
-            #self.ui.comboBoxCorrespondences.setCurrentIndex(0)
+            self.viewer.correspondenceLibrary.correspondenceList.append(self.lastCorrespondence)
 
-            ##self.createBasicCorrespondence()  
-            ##self.createBasicCorrespondence()  
-            print "debug 1"
-            #self.viewer.correspondenceLibrary.correspondenceList = self.populateEmptyResults(self.viewer.correspondenceLibrary)
-            print "debug 1"
-            #self.populateComboBox(self.viewer.correspondenceLibrary)     
-            self.ui.comboBoxCorrespondences.clear()
-            print "debug 1a"
-            self.ui.comboBoxCorrespondences.addItem("last successful correspondence")
-            print "debug 1b"
-            # add all correspondence to pulldown menu
-            #self.getConstraints()
-
-            self.viewer.correspondenceEngine.clearAllConstraints()
-            print "debug 1c"
-        """
-         
-        self.executed = True 
+        self.populateComboBox(self.viewer.correspondenceLibrary)     
         self.viewer.emitModelChanged()
         self.ui.tabWidget.setCurrentIndex(3)         
-
-        print "done"
+        print "done with search"
                 
     def reject(self):  
         self.executed = False
