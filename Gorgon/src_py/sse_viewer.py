606cb883ccf8a9ec613f23cc900852a264712a5b
schuhs <>
2009-12-20 23:56:06 +0000

    Preparing to merge with trunk: copied in changes from trunk

--- Gorgon/src_py/sse_viewer.py
+++ Gorgon/src_py/sse_viewer.py
@@ -11,6 +11,9 @@
 #
 # History Log: 
 #   $Log$
+#   Revision 1.24.2.7  2009/12/17 02:55:41  schuhs
+#   Add a test to make sure only the sheeting mesh adding method doesn't pick up any helices. Also pass offsets along with meshes to be added.
+#
 #   Revision 1.24.2.6  2009/12/09 20:55:50  schuhs
 #   Removing old console messages and comments.
 #
@@ -80,6 +83,7 @@ from sse_sequence_predictor_form import SSESequencePredictorForm
 from sse_helix_correspondence_finder_form import SSEHelixCorrespondenceFinderForm
 from model_visualization_form import ModelVisualizationForm
 from libpyGORGON import SSECorrespondenceEngine, SSECorrespondenceResult
+from volume_sse_builder_form import VolumeSSEBuilderForm
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -90,6 +94,7 @@ class SSEViewer(BaseViewer):
     def __init__(self, main, parent=None):
         BaseViewer.__init__(self, main, parent)
         self.title = "Secondary Structure Element"
+        self.shortTitle = "SSE"              
         self.app.themes.addDefaultRGB("Secondary Structure Element:Model:0", 0, 180, 0, 255)
         self.app.themes.addDefaultRGB("Secondary Structure Element:Model:1", 120, 185, 255, 255)
         self.app.themes.addDefaultRGB("Secondary Structure Element:Model:2", 120, 185, 255, 255)
@@ -99,11 +104,13 @@ class SSEViewer(BaseViewer):
         self.sheetFileName = ""
         self.currentMatch = None
         self.showBox = False;
+        self.helixLoaded = False
+        self.sheetLoaded = False        
         self.renderer = SSERenderer()
         self.correspondenceEngine = SSECorrespondenceEngine()
         self.createUI()
         self.selectEnabled = True
-        self.app.viewers["sse"] = self;
+        self.app.viewers["sse"] = self
         self.model2Visible = True
         self.model3Visible = True
         self.initVisualizationOptions(ModelVisualizationForm(self.app, self))
@@ -130,40 +137,81 @@ class SSEViewer(BaseViewer):
         self.updateActionsAndMenus()
                   
     def createChildWindows(self):
+        self.sseBuilder = VolumeSSEBuilderForm(self.app, self)
         self.sequencePredictor = SSESequencePredictorForm(self.app, self)
         self.helixCorrespondanceFinder = SSEHelixCorrespondenceFinderForm(self.app, self)
+        
+    def loadHelixDataFromFile(self, fileName):
+        self.setCursor(QtCore.Qt.WaitCursor)
+        self.renderer.loadHelixFile(str(fileName))
+        self.loaded = True
+        self.helixLoaded = True
+        self.setCursor(QtCore.Qt.ArrowCursor)
+        self.emitModelLoaded()
+        self.emitViewerSetCenter()        
     
     def loadHelixData(self):
-        helixFileName = str(QtGui.QFileDialog.getOpenFileName(self, self.tr("Open Data"), "", self.tr(self.renderer.getSupportedLoadFileFormats())))
-        self.loadHelixDataFile(helixFileName)
-               
-    def loadHelixDataFile(self, fileName):
-        self.helixFileName = fileName
-        self.fileName = self.helixFileName
-        if not self.helixFileName == "":  
-            self.setCursor(QtCore.Qt.WaitCursor)
-            self.renderer.loadHelixFile(str(self.helixFileName))
-            self.loaded = True
-            self.setCursor(QtCore.Qt.ArrowCursor)
-            self.emitModelLoaded()
-            self.emitViewerSetCenter()        
-               
+        self.helixFileName = QtGui.QFileDialog.getOpenFileName(self, self.tr("Open Helix Annotations"), "", self.tr(self.renderer.getSupportedHelixLoadFileFormats()))
+        self.fileName = self.helixFileName;
+        if not self.helixFileName.isEmpty():  
+            self.loadHelixDataFromFile(self.helixFileName)
+                    
+    
+    def loadSheetDataFromFile(self, fileName):
+        self.setCursor(QtCore.Qt.WaitCursor)
+        self.renderer.loadSheetFile(str(fileName))
+        self.loaded = True
+        self.sheetLoaded = True
+        self.setCursor(QtCore.Qt.ArrowCursor)
+        self.emitModelLoaded()
+        self.emitViewerSetCenter()        
+    
     def loadSheetData(self):
-        sheetFileName = QtGui.QFileDialog.getOpenFileName(self, self.tr("Open Data"), "", self.tr(self.renderer.getSupportedLoadFileFormats()))
-        self.loadSheetDataFile(sheetFileName)
-                                          
-    def loadSheetDataFile(self, fileName):
-        self.sheetFileName = fileName
-        self.fileName = self.sheetFileName
-        if not self.sheetFileName == "":  
+        self.sheetFileName = QtGui.QFileDialog.getOpenFileName(self, self.tr("Open Sheet Annotations"), "", self.tr(self.renderer.getSupportedSheetLoadFileFormats()))
+        self.fileName = self.sheetFileName;
+        if not self.sheetFileName.isEmpty():  
+            self.loadSheetDataFromFile(self.sheetFileName)
+            
+    def saveHelixData(self):
+        self.fileName = QtGui.QFileDialog.getSaveFileName(self, self.tr("Save Helix Annotations"), "", self.tr(self.renderer.getSupportedHelixSaveFileFormats()))
+        if not self.fileName.isEmpty():  
             self.setCursor(QtCore.Qt.WaitCursor)
-            self.renderer.loadSheetFile(str(self.sheetFileName))
-            self.loaded = True
+            self.renderer.saveHelixFile(str(self.fileName))
+            self.dirty = False
             self.setCursor(QtCore.Qt.ArrowCursor)
-            self.emitModelLoaded()
-            self.emitViewerSetCenter()
-                                          
+
+    def saveSheetData(self):
+        self.fileName = QtGui.QFileDialog.getSaveFileName(self, self.tr("Save Sheet Annotations"), "", self.tr(self.renderer.getSupportedSheetSaveFileFormats()))
+        if not self.fileName.isEmpty():  
+            self.setCursor(QtCore.Qt.WaitCursor)
+            self.renderer.saveSheetFile(str(self.fileName))
+            self.dirty = False
+            self.setCursor(QtCore.Qt.ArrowCursor)
+
+    def getSessionInfo(self, sessionManager):
+        info = BaseViewer.getSessionInfo(self, sessionManager)  
+        info.extend(sessionManager.getRemarkLines(self.shortTitle, "HELIX_LOADED", self.helixLoaded))
+        info.extend(sessionManager.getRemarkLines(self.shortTitle, "HELIX_FILE", self.helixFileName))        
+        info.extend(sessionManager.getRemarkLines(self.shortTitle, "SHEET_LOADED", self.sheetLoaded))
+        info.extend(sessionManager.getRemarkLines(self.shortTitle, "SHEET_FILE", self.sheetFileName))
+        return info
+                       
+    def loadSessionInfo(self, sessionManager, sessionProperties):
+        BaseViewer.loadSessionInfo(self, sessionManager, sessionProperties)        
+        self.helixLoaded = sessionManager.getProperty(sessionProperties, self.shortTitle, "HELIX_LOADED")
+        if self.helixLoaded:
+            self.helixFileName = sessionManager.getProperty(sessionProperties, self.shortTitle, "HELIX_FILE")
+            self.loadHelixDataFromFile(self.helixFileName)
+        self.sheetLoaded = sessionManager.getProperty(sessionProperties, self.shortTitle, "SHEET_LOADED")
+        if self.sheetLoaded:
+            self.sheetFileName = sessionManager.getProperty(sessionProperties, self.shortTitle, "SHEET_FILE")
+            self.loadSheetDataFromFile(self.sheetFileName)
+
+                                                    
     def unloadData(self):
+        self.loaded = False
+        self.helixLoaded = False
+        self.sheetLoaded = False
         self.helixFileName = ""
         self.sheetFileName = ""
         BaseViewer.unloadData(self)
@@ -190,7 +238,17 @@ class SSEViewer(BaseViewer):
         openSheetAct.setStatusTip(self.tr("Load a sheet file"))
         self.connect(openSheetAct, QtCore.SIGNAL("triggered()"), self.loadSheetData)
         self.app.actions.addAction("load_SSE_Sheet", openSheetAct)
-               
+
+        saveHelixAct = QtGui.QAction(self.tr("&Helix Annotations..."), self)
+        saveHelixAct.setStatusTip(self.tr("Save helix annotations"))
+        self.connect(saveHelixAct, QtCore.SIGNAL("triggered()"), self.saveHelixData)
+        self.app.actions.addAction("save_SSE_Helix", saveHelixAct)        
+
+        saveSheetAct = QtGui.QAction(self.tr("&Sheet Annotations..."), self)
+        saveSheetAct.setStatusTip(self.tr("Save sheet annotations"))
+        self.connect(saveSheetAct, QtCore.SIGNAL("triggered()"), self.saveSheetData)
+        self.app.actions.addAction("save_SSE_Sheet", saveSheetAct)    
+                       
         closeAct = QtGui.QAction(self.tr("SSE Annotations"), self)
         closeAct.setStatusTip(self.tr("Close the loaded secondary structure element file"))
         self.connect(closeAct, QtCore.SIGNAL("triggered()"), self.unloadData)
@@ -204,13 +262,17 @@ class SSEViewer(BaseViewer):
                         
     def createMenus(self):
         self.app.menus.addAction("file-open-helix", self.app.actions.getAction("load_SSE_Helix"), "file-open")    
-        self.app.menus.addAction("file-open-sheet", self.app.actions.getAction("load_SSE_Sheet"), "file-open")        
+        self.app.menus.addAction("file-open-sheet", self.app.actions.getAction("load_SSE_Sheet"), "file-open")
+        self.app.menus.addAction("file-save-helix", self.app.actions.getAction("save_SSE_Helix"), "file-save");        
+        self.app.menus.addAction("file-save-sheet", self.app.actions.getAction("save_SSE_Sheet"), "file-save");        
         self.app.menus.addAction("file-close-sse", self.app.actions.getAction("unload_SSE"), "file-close");
         self.app.menus.addMenu("actions-sse", self.tr("Secondary Structure &Element"), "actions");
         self.app.menus.addAction("actions-sse-fit-helix", self.app.actions.getAction("fit_SSE_Helix"), "actions-sse");
                    
     def updateActionsAndMenus(self):
         self.app.actions.getAction("unload_SSE").setEnabled(self.loaded)
+        self.app.actions.getAction("save_SSE_Helix").setEnabled(self.helixLoaded)
+        self.app.actions.getAction("save_SSE_Sheet").setEnabled(self.sheetLoaded)
         self.app.actions.getAction("fit_SSE_Helix").setEnabled(self.loaded and self.app.viewers["volume"].loaded)
     
     def updateCurrentMatch(self, sseType, sseIndex):
