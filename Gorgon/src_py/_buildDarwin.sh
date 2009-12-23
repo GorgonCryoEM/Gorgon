5c5a326904df29668dcd9c0f6f47617509108b50
ssa1 <>
2009-12-23 03:52:25 +0000

    Modified to include FFTW binaries

--- Gorgon/src_py/_buildDarwin.sh
+++ Gorgon/src_py/_buildDarwin.sh
@@ -2,6 +2,7 @@
 
 cp ../../../libpyGORGON.dylib libpyGORGON.dylib
 cp ../../../source/ExternalLibraries/boost_1_35_0/bin.v2/libs/python/build/darwin/release/macosx-version-10.4/threading-multi/libboost_python-mt-1_35.dylib libboost_python-mt-1_35.dylib 
+cp ../../../source/ExternalLibraries/FFTW/MacOS_x86_10.5/lib/libfftw3f.dylib libfftw3f.dylib
 
 rm libpyGORGON.so
 rm -R dist
@@ -9,6 +10,8 @@ rm -R build
 python _buildExecutableDarwin.py py2app
 cp libpyGORGON.dylib dist/gorgon.app/Contents/Frameworks
 cp libboost_python-mt-1_35.dylib dist/gorgon.app/Contents/Resources/ 
+mkdir dist/gorgon.app/Contents/Resources/lib/extlib
+cp libfftw3f.dylib dist/gorgon.app/Contents/Resources/lib/extlib/
 
 
 cd dist/gorgon.app/Contents/Resources/lib/python2.5/lib-dynload/
