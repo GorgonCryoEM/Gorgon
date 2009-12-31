e1b6eb84ecc42b26724ba1ec62d70926810decc9
ssa1 <>
2009-12-31 02:24:47 +0000

    Updating nightly build scripts

--- Gorgon/src_py/_buildWin64.cmd
+++ Gorgon/src_py/_buildWin64.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-%PYTHON_ROOT%\python _buildExecutableWin32.py py2exe 
+%PYTHON_ROOT%\python _buildExecutableWin64.py py2exe 
 
 md dist\OpenGL > NUL
 xcopy %PYTHON_ROOT%\Lib\site-packages\OpenGL\* dist\OpenGL\ /E /Y /Q > NUL
---
+++ _setPathsWin32.bat
@@ -0,0 +1,3 @@
+set VERSION=2.0.0
+set PLATFORM=win32
+set PYTHON_ROOT=C:\Programs\Python25
