296c5f9a2850dc869c01f1e38230994803d856cb
shadow_walker <shadowwalkersb@gmail.com>
2015-05-07 12:17:28 -0500

    replace some paths with variables instead of hardcoded paths

--- Gorgon/src_py/_buildDarwin.sh
+++ Gorgon/src_py/_buildDarwin.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
-cp ../../../libpyGORGON.dylib libpyGORGON.dylib
-cp ../../../source/ExternalLibraries/boost_1_35_0/bin.v2/libs/python/build/darwin/release/macosx-version-10.4/threading-multi/libboost_python-mt-1_35.dylib libboost_python-mt-1_35.dylib 
-cp ../../../source/ExternalLibraries/FFTW/MacOS_x86_10.5/lib/libfftw3f.dylib libfftw3f.dylib
+topdir="/Users/durmaz/Files/eclipse_workspace/workspace_work/gitGorgonBitBucket"
+fboost="libboost_python.dylib"
+
+cp -v ~/Gorgon/lib/libpyGORGON.dylib libpyGORGON.dylib
+cp -v /Users/durmaz/Downloads/boost/boost_1_57_0/stage/lib/libboost_python.dylib $fboost 
+cp -v ${topdir}/ExternalLibraries/FFTW/MacOS_x86_10.5/lib/libfftw3f.dylib libfftw3f.dylib
 
 rm libpyGORGON.so
 rm -R dist
 rm -R build
 python _buildExecutableDarwin.py py2app
-cp libpyGORGON.dylib dist/gorgon.app/Contents/Frameworks
-cp libboost_python-mt-1_35.dylib dist/gorgon.app/Contents/Resources/ 
-cp libfftw3f.dylib dist/gorgon.app/Contents/Resources/
+
+mkdir -p dist/gorgon.app/Contents/Frameworks
+mkdir -p dist/gorgon.app/Contents/Resources/
+mkdir -p dist/gorgon.app/Contents/Resources/
+mkdir -p dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/
+
+cp -v libpyGORGON.dylib dist/gorgon.app/Contents/Frameworks
+cp -v $fboost dist/gorgon.app/Contents/Resources/ 
+cp -v libfftw3f.dylib dist/gorgon.app/Contents/Resources/
 
 cd dist
 mkdir plugins
-cp ../plugins/*.py plugins
+cp -v ../plugins/*.py plugins
 cd ..
 
-cd dist/gorgon.app/Contents/Resources/lib/python2.5/lib-dynload/
-ln -s ../../../../Frameworks/libpyGORGON.dylib libpyGORGON.so
+cd dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/
+cp -av ../../../../Frameworks/libpyGORGON.dylib libpyGORGON.so
 chmod +rx libpyGORGON.so
-ln -s ../OpenGL OpenGL
+ln -sv ../OpenGL OpenGL
 chmod +rx OpenGL
 cd ../../../../../../../
 cd dist/gorgon.app/Contents/Resources
-ln -s ../../../plugins/ plugins
+ln -sv ../../../plugins/ plugins
 cd ../../../..
 
 
-ln -s libpyGORGON.dylib libpyGORGON.so
-
-
+cp -av libpyGORGON.dylib libpyGORGON.so
