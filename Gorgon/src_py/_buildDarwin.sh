e9d5fbca74e9855e6de24a1eb2f92ab3a0a2a511
shadow_walker <shadowwalkersb@gmail.com>
2015-05-08 11:25:16 -0500

    fix paths in Mac build script

--- Gorgon/src_py/_buildDarwin.sh
+++ Gorgon/src_py/_buildDarwin.sh
@@ -4,10 +4,10 @@ topdir="/Users/durmaz/Files/eclipse_workspace/workspace_work/gitGorgonBitBucket"
 fboost="libboost_python.dylib"
 
 cp -v ~/Gorgon/lib/libpyGORGON.dylib libpyGORGON.dylib
-cp -v /Users/durmaz/Downloads/boost/boost_1_57_0/stage/lib/libboost_python.dylib $fboost 
-cp -v ${topdir}/ExternalLibraries/FFTW/MacOS_x86_10.5/lib/libfftw3f.dylib libfftw3f.dylib
+cp -v /Users/durmaz/Documents/boost_1_57_0/stage/lib/libboost_python.dylib $fboost 
+cp -v /opt/local/lib/libfftw3f.dylib .
 
-rm libpyGORGON.so
+rm libpyGORGON.so libpyGORGON.dylib
 rm -R dist
 rm -R build
 python _buildExecutableDarwin.py py2app
@@ -17,7 +17,7 @@ mkdir -p dist/gorgon.app/Contents/Resources/
 mkdir -p dist/gorgon.app/Contents/Resources/
 mkdir -p dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/
 
-cp -v libpyGORGON.dylib dist/gorgon.app/Contents/Frameworks
+cp -v ~/Gorgon/lib/libpyGORGON.dylib dist/gorgon.app/Contents/Frameworks
 cp -v $fboost dist/gorgon.app/Contents/Resources/ 
 cp -v libfftw3f.dylib dist/gorgon.app/Contents/Resources/
 
