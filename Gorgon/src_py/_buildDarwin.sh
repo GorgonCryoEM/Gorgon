02b0584d188ea90f1f3786345a6c61ec2e82eec8
shadow_walker <shadowwalkersb@gmail.com>
2015-05-08 12:39:38 -0500

    rewrite of Mac build script

--- Gorgon/src_py/_buildDarwin.sh
+++ Gorgon/src_py/_buildDarwin.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-topdir="/Users/durmaz/Files/eclipse_workspace/workspace_work/gitGorgonBitBucket"
+topdir="/Users/durmaz/Files/eclipse_workspace/workspace_work/gitGorgonBitBucket/Gorgon/src_py"
 fboost="libboost_python.dylib"
 
-cp -v ~/Gorgon/lib/libpyGORGON.dylib libpyGORGON.dylib
-cp -v /Users/durmaz/Documents/boost_1_57_0/stage/lib/libboost_python.dylib $fboost 
-cp -v /opt/local/lib/libfftw3f.dylib .
+rm -r build dist libpyGORGON.so libpyGORGON.dylib
 
-rm libpyGORGON.so libpyGORGON.dylib
-rm -R dist
-rm -R build
 python _buildExecutableDarwin.py py2app
 
 mkdir -p dist/gorgon.app/Contents/Frameworks
-mkdir -p dist/gorgon.app/Contents/Resources/
-mkdir -p dist/gorgon.app/Contents/Resources/
 mkdir -p dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/
 
-cp -v ~/Gorgon/lib/libpyGORGON.dylib dist/gorgon.app/Contents/Frameworks
-cp -v $fboost dist/gorgon.app/Contents/Resources/ 
-cp -v libfftw3f.dylib dist/gorgon.app/Contents/Resources/
+cp -v ~/Gorgon/lib/libpyGORGON.dylib .
+cp -v /Users/durmaz/Documents/boost_1_57_0/stage/lib/$fboost . 
+cp -v /opt/local/lib/libfftw3f.dylib .
 
-cd dist
-mkdir plugins
-cp -v ../plugins/*.py plugins
-cd ..
+cp -v libpyGORGON.dylib      dist/gorgon.app/Contents/Frameworks
+cp -v $fboost                dist/gorgon.app/Contents/Resources/ 
+cp -v libfftw3f.dylib        dist/gorgon.app/Contents/Resources/
+
+#cd dist
+mkdir dist/plugins
+cp -v plugins/*.py dist/plugins
+#cd ..
+
+#cd dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/
+cp -av     libpyGORGON.dylib                          dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/libpyGORGON.so
+chmod +rx  dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/libpyGORGON.so
 
 cd dist/gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/
-cp -av ../../../../Frameworks/libpyGORGON.dylib libpyGORGON.so
-chmod +rx libpyGORGON.so
-ln -sv ../OpenGL OpenGL
-chmod +rx OpenGL
-cd ../../../../../../../
+ln -sv   ../OpenGL
+chmod +rx  OpenGL
+cd $topdir
+#cd ../../../../../../../
 cd dist/gorgon.app/Contents/Resources
-ln -sv ../../../plugins/ plugins
-cd ../../../..
+ln -sv   ../../../../plugins
 
-
+#cd ../../../..
+cd $topdir
 cp -av libpyGORGON.dylib libpyGORGON.so
