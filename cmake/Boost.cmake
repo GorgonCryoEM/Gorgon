a97f44932d44a3f2b707daf3e7828cf914dbdfc2
shadow_walker <shadowwalkersb@gmail.com>
2015-06-08 19:32:32 -0500

    add more variables

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -18,13 +18,16 @@ set(Boost_USE_MULTITHREADED ON)
 find_package(Boost 1.41 COMPONENTS ${boost_components})
 
 # Set variables
+set(boost_root           ${CMAKE_SOURCE_DIR}/ExternalLibraries/boost )
+set(boost_librarydir     ${boost_root}/lib                           )
+
 set(boost_url            http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz )
 set(boost_url_sha1       a27b010b9d5de0c07df9dddc9c336767725b1e6b                                        )
 set(boost_url_md5        5a5d5614d9a07672e1ab2a250b5defc5                                                )
 set(boost_install_prefix ${CMAKE_SOURCE_DIR}/ExternalLibraries/boost/                                    )
 
 if( NOT Boost_FOUND)
-    set(BOOST_ROOT ${CMAKE_SOURCE_DIR}/ExternalLibraries/boost CACHE PATH "Boost root directory" )
+    set(BOOST_ROOT ${boost_root} CACHE PATH "Boost root directory" )
     set(BOOST_LIBRARYDIR ${BOOST_ROOT}/lib CACHE PATH "Boost library directory")
     find_package(Boost 1.41 COMPONENTS ${boost_components})
 endif()
