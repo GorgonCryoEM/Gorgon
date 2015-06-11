9062cbc6c9826e73d8e1730129080fa18af98356
shadow_walker <shadowwalkersb@gmail.com>
2015-06-10 20:18:34 -0500

    fix glext.h include with conditional macro in cpp code
    closes #56

--- Gorgon/VolumeRenderer.h
+++ Gorgon/VolumeRenderer.h
@@ -12,7 +12,9 @@
 
 #include <iostream>
 #include <glut.h>
-#include <glExt.h>
+#ifndef __APPLE__
+	#include <glExt.h>
+#endif
 #include <string>
 #include <GraphMatch/VectorMath.h>
 #include "Renderer.h"
