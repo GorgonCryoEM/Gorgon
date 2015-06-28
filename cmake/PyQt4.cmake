89b96c3b46d8d26fd3ac45f73b8914f471de2955
shadow_walker <shadowwalkersb@gmail.com>
2015-06-28 05:47:35 -0500

    add sip file location

--- cmake/PyQt4.cmake
+++ cmake/PyQt4.cmake
@@ -25,7 +25,7 @@ set(pyqt4_install_prefix ${GORGON_EXTERNAL_LIBRARIES_DIR}/pyqt4/                
 #        URL_MD5       ${pyqt4_url_md5}
      #--Configure step-------------
 #        CONFIGURE_COMMAND  ./configure --prefix=${pyqt4_install_prefix} --enable-shared
-        CONFIGURE_COMMAND  ${PYTHON_EXECUTABLE} configure-ng.py --confirm-license
+        CONFIGURE_COMMAND  ${PYTHON_EXECUTABLE} configure-ng.py --confirm-license --sip ${CMAKE_BINARY_DIR}/python/Python.framework/Versions/2.7/bin/sip
      #--Build step-----------------
 #        BUILD_COMMAND ${pyqt4_b2_CMD}    ${pyqt4_options}
         BUILD_IN_SOURCE 1
