e1ccf997faa6e620a02dbe36a611e6655396adde
shadow_walker <shadowwalkersb@gmail.com>
2015-06-29 00:20:29 -0500

    update paths to resources in _buildMac.py

--- _buildMac/_buildExecutableDarwin.py
+++ _buildMac/_buildExecutableDarwin.py
@@ -6,11 +6,11 @@ import py2app.recipes
 setup(
     app=['gorgon.pyw'],
     version="2.2.0",
-    data_files=[('', ['splash.png', 'gorgon.icns']), 
+    data_files=[('', ['../design/splash.png', '../design/gorgon.icns']), 
                 ('../../../themes', ['../resources/Dark.thm', '../resources/Light.thm'] )],
     options={
              'py2app': {
-                        'iconfile' : 'gorgon.icns',
+                        'iconfile' : '../design/gorgon.icns',
                         'packages' : [],
                         "includes" : ['sip', 'PyQt4'],
                         "excludes" : [],                        
