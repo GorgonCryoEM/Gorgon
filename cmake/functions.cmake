7b5100d7e52b4d6fe538b3c0a2a6042d34f54af5
shadow_walker <shadowwalkersb@gmail.com>
2015-07-01 21:25:50 -0500

    add root_type options: Gorgon & Custom (+1 squashed commit)

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -2,8 +2,17 @@ include(cmake/Debug.cmake)
 
 function(external_project proj)
 
-    set(CMAKE_PREFIX_PATH ${_root})
-    find_package(${proj} ${_pkg_arg})
+    set(${proj}_root_type "Gorgon" CACHE STRING "${proj} root directory type")
+    set_property(CACHE ${proj}_root_type PROPERTY STRINGS Custom Gorgon)
+    if(${proj}_root_type STREQUAL Gorgon)
+        string(TOLOWER ${proj}_root root_low)
+        set(${proj}_root ${CMAKE_BINARY_DIR}/${root_low})
+        
+        include(${CMAKE_SOURCE_DIR}/cmake/${proj}.cmake)
+    endif()
+
+    set(CMAKE_PREFIX_PATH ${root_low})
+    find_package(${${proj}_find} ${_pkg_arg})
 
 #    string(TOUPPER ${_prefix} prefixup)
 #    string(TOLOWER ${_prefix} prefixlo)
