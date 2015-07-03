babf0822017f2f33c565897e733883235139370f
shadow_walker <shadowwalkersb@gmail.com>
2015-07-03 00:11:17 -0500

    use external_project_vars()

--- cmake/SIP.cmake
+++ cmake/SIP.cmake
@@ -1,11 +1,6 @@
-set(sip_root           ${GORGON_EXTERNAL_LIBRARIES_DIR}/sip  )
-set(sip_librarydir     ${sip_root}/lib     )
-set(sip_includedir     ${sip_root}/include )
-
-set(sip_url            http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.8/sip-4.16.8.tar.gz )
-#set(sip_url_sha1       a27b010b9d5de0c07df9dddc9c336767725b1e6b                                        )
-#set(sip_url_md5        d7547558fd673bd9d38e2108c6b42521                                                )
-set(sip_install_prefix ${GORGON_EXTERNAL_LIBRARIES_DIR}/sip/                                                           )
+external_project_vars( SIP
+    http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.8/sip-4.16.8.tar.gz
+    )
 
 # If not found so far, donwload, build and install sip
 #if( NOT SIP_FOUND)
