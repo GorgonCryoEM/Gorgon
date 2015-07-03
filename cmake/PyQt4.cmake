d8265ac3261f734b0f8c29862ae2a1dade1a1b4a
shadow_walker <shadowwalkersb@gmail.com>
2015-07-03 00:13:46 -0500

    disable root_type (+34 squashed commits)

--- cmake/PyQt4.cmake
+++ cmake/PyQt4.cmake
@@ -1,26 +1,9 @@
 external_project_vars( PyQt4
-    http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
-    )
-
-# If not found so far, donwload, build and install pyqt4
-#if( NOT PyQt4_FOUND)
-  
-    external_project_build( 
-         PyQt4
-         SIP #Qt4
-    	";"
-           ${pyqt4_url}
-#        URL_HASH SHA1=${pyqt4_url_sha1}
-#        URL_MD5       ${pyqt4_url_md5}
-#        CONFIGURE_COMMAND  ./configure --prefix=${pyqt4_install_prefix} --enable-shared
+        SIP #Qt4
+        http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
         "${PYTHON_EXECUTABLE};configure-ng.py;--confirm-license;--sip;${CMAKE_BINARY_DIR}/python/Python.framework/Versions/2.7/bin/sip"
-         "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
-         "${CMAKE_MAKE_PROGRAM};install"
-#         INSTALL_DIR pyqt4
-#        INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install --prefix=${pyqt4_install_prefix}
-     #--Custom targets-------------
-     # [STEP_TARGETS st1 st2 ...]  # Generate custom targets for these steps
+        "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
+        "${CMAKE_MAKE_PROGRAM};install"
     )
 
-ExternalProject_Get_Property(PyQt4 INSTALL_DIR)
-#find_package(PyQt4Libs)
+external_project_find_paths(PyQt4)
--- cmake/SIP.cmake
+++ cmake/SIP.cmake
@@ -1,28 +1,9 @@
 external_project_vars( SIP
-    http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.8/sip-4.16.8.tar.gz
-    )
-
-# If not found so far, donwload, build and install sip
-#if( NOT SIP_FOUND)
-  
-    external_project_build( 
-     SIP
-     Python
-    ";"
-           ${sip_url}
-#        URL_HASH SHA1=${sip_url_sha1}
-#        URL_MD5       ${sip_url_md5}
-#        CONFIGURE_COMMAND  ./configure --prefix=${sip_install_prefix} --enable-shared
+        Python
+        http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.8/sip-4.16.8.tar.gz
         "${PYTHON_EXECUTABLE};configure.py"
-#        BUILD_COMMAND ${sip_b2_CMD}    ${sip_options}
-         "${CMAKE_MAKE_PROGRAM}"
-         "${CMAKE_MAKE_PROGRAM};install"
-     #--Install step---------------
-#         INSTALL_DIR sip
-#        INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install --prefix=${sip_install_prefix}
-     #--Custom targets-------------
-     # [STEP_TARGETS st1 st2 ...]  # Generate custom targets for these steps
+        "${CMAKE_MAKE_PROGRAM}"
+        "${CMAKE_MAKE_PROGRAM};install"
     )
 
-ExternalProject_Get_Property(SIP INSTALL_DIR)
-#find_package(SIPLibs)
+external_project_find_paths(SIP)
