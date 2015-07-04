d070dd9dd5e2e086355945dd36e40d88406acc67
shadow_walker <shadowwalkersb@gmail.com>
2015-07-04 01:09:16 -0500

    use extra_options to pass binder & sip-module to SIP configuration and later refer to them in PyQt4

--- cmake/PyQt4.cmake
+++ cmake/PyQt4.cmake
@@ -1,7 +1,7 @@
 external_project_vars( PyQt4
         SIP #Qt4
         http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
-        "${PYTHON_EXECUTABLE};configure-ng.py;--confirm-license;--sip;${CMAKE_BINARY_DIR}/python/Python.framework/Versions/2.7/bin/sip"
+        "${PYTHON_EXECUTABLE};configure-ng.py;--confirm-license;--sip;${sip_bindir}/${sip_sip-module}"
         "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
         "${CMAKE_MAKE_PROGRAM};install"
     )
--- cmake/SIP.cmake
+++ cmake/SIP.cmake
@@ -1,7 +1,10 @@
 external_project_vars( SIP
         Python
         http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.8/sip-4.16.8.tar.gz
-        "${PYTHON_EXECUTABLE};configure.py"
+        "${PYTHON_EXECUTABLE};configure.py;${sip_extra_options}"
         "${CMAKE_MAKE_PROGRAM}"
         "${CMAKE_MAKE_PROGRAM};install"
+        "bindir;${python_install_prefix}/bin"
+        "sip-module;sip"
     )
+    
