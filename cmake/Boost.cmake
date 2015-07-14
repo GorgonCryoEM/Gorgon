de85f19382140d7e4d2102cbf55464d90337f132
shadow_walker <shadowwalkersb@gmail.com>
2015-07-14 16:00:11 -0500

    SPLIT: Boost

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -7,14 +7,15 @@ set_property(CACHE BOOST_LOCATION_OPTIONS PROPERTY STRINGS Gorgon System)
 # Set variables
 set(boost_version 1.44)
 
-set(boost_root           ${GORGON_EXTERNAL_LIBRARIES_DIR}/boost  )
+set(boost_root           ${CMAKE_CURRENT_BINARY_DIR}/boost  )
 set(boost_librarydir     ${boost_root}/lib     )
 set(boost_includedir     ${boost_root}/include )
 
 set(boost_url            http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz )
 set(boost_url_sha1       a27b010b9d5de0c07df9dddc9c336767725b1e6b                                        )
 set(boost_url_md5        5a5d5614d9a07672e1ab2a250b5defc5                                                )
-set(boost_install_prefix ${GORGON_EXTERNAL_LIBRARIES_DIR}/boost/                                                           )
+#set(boost_install_prefix ${GORGON_EXTERNAL_LIBRARIES_DIR}/boost/                                         )
+set(boost_install_prefix ${boost_root}                                           )
 
 # Windows needs `libbost_serialization` for some reason
 if(WIN32)
@@ -32,16 +33,16 @@ endforeach()
 
 set(Boost_USE_MULTITHREADED ON)
 
-if(BOOST_LOCATION_OPTIONS EQUAL GORGON)
-
+if(BOOST_LOCATION_OPTIONS STREQUAL Gorgon)
     set(BOOST_ROOT       ${boost_root}       CACHE PATH "Boost root directory"   )
     set(BOOST_INCLUDEDIR ${boost_includedir} CACHE PATH "Boost include directory")
     set(BOOST_LIBRARYDIR ${boost_librarydir} CACHE PATH "Boost library directory")
-    
-    find_package(Boost ${boost_version} COMPONENTS ${boost_components})
+ endif()
+
+find_package(Boost ${boost_version} COMPONENTS ${boost_components})
 
 # If not found so far, donwload, build and install Boost
-if( NOT Boost_FOUND)
+#if( NOT Boost_FOUND)
     if( UNIX )
       set( Boost_Bootstrap_CMD ./bootstrap.sh )
       set( Boost_b2_CMD ./b2 )
@@ -53,15 +54,19 @@ if( NOT Boost_FOUND)
     endif()
     
     include(ExternalProject)
-    
+
+#    --with-python=PYTHON      specify the Python executable [python]
+#  --with-python-root=DIR
+  
     ExternalProject_Add( Boost
+    DEPENDS Python
     	PREFIX boost
      #--Download step--------------
         URL           ${boost_url}
         URL_HASH SHA1=${boost_url_sha1}
         URL_MD5       ${boost_url_md5}
      #--Configure step-------------
-        CONFIGURE_COMMAND  ${Boost_Bootstrap_CMD}
+        CONFIGURE_COMMAND  ${Boost_Bootstrap_CMD} --with-python=${PYTHON_EXECUTABLE}
      #--Build step-----------------
         BUILD_COMMAND ${Boost_b2_CMD}    ${boost_options}
         BUILD_IN_SOURCE 1
@@ -77,13 +82,27 @@ if( NOT Boost_FOUND)
      #--Custom targets-------------
      # [STEP_TARGETS st1 st2 ...]  # Generate custom targets for these steps
     )
-endif()
-else()
+#endif()
+#else()
+#    find_package(Boost ${boost_version} COMPONENTS ${boost_components})
+
+#if( NOT Boost_FOUND)
+#        add_custom_target(Rescan ${CMAKE_COMMAND} ${CMAKE_SOURCE_DIR} DEPENDS Boost)
+#else()
+#        add_custom_target(Rescan)
+#endif()
+
+#add_dependencies(pyGORGON Rescan)
+#if(  Boost_FOUND)
+#        TARGET_LINK_LIBRARIES(pyGORGON Boost)
+#endif()
+ExternalProject_Get_Property(Boost INSTALL_DIR)
+
+set(BOOST_ROOT       ${boost_root}       CACHE PATH "Boost root directory"   )
 find_package(Boost ${boost_version} COMPONENTS ${boost_components})
-endif()
 
-include_directories(${Boost_INCLUDE_DIR})
-
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Boost DEFAULT_MSG Boost_LIBRARIES Boost_LIBRARY_DIR Boost_INCLUDE_DIR)
-MARK_AS_ADVANCED(Boost_LIBRARIES Boost_LIBRARY_DIR Boost_INCLUDE_DIR)
+#include_directories(${Boost_INCLUDE_DIR})
+#
+#INCLUDE(FindPackageHandleStandardArgs)
+#FIND_PACKAGE_HANDLE_STANDARD_ARGS(Boost DEFAULT_MSG Boost_LIBRARIES Boost_LIBRARY_DIR Boost_INCLUDE_DIR)
+#MARK_AS_ADVANCED(Boost_LIBRARIES Boost_LIBRARY_DIR Boost_INCLUDE_DIR)
