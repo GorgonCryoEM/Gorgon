c5e9a42184415fe5445ad082383bba54cb813e50
shadow_walker <shadowwalkersb@gmail.com>
2015-07-15 13:48:24 -0500

    install pip during Python installation, get-pip.py is not needed anymore

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -1,7 +1,7 @@
 external_project_vars(
         ""
         https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-        "./configure;--prefix=${python_install_prefix};--with-pydebug;--enable-framework=${python_install_prefix}"
+        "./configure;--prefix=${python_install_prefix};--with-pydebug;--enable-framework=${python_install_prefix};--with-ensurepip=install"
         "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
         "${CMAKE_MAKE_PROGRAM};install"
     )
