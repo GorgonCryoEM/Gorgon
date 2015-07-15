82cc81bcd81736e3f1de1d06c14da6bc76208722
shadow_walker <shadowwalkersb@gmail.com>
2015-07-15 14:43:35 -0500

    add python_lib-dynload & python_site-packages for install step

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -31,3 +31,7 @@ find_package(PythonLibs)
 update_libs_includes(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)
 
 set(python_url_md5_cmd        URL_MD5       d7547558fd673bd9d38e2108c6b42521  CACHE INTERNAL "")
+
+set(python_libs_dir       ${python_install_prefix}/Python.framework/Versions/2.7/lib/python2.7  CACHE INTERNAL "Python directory for lib-dynload & site-settings")
+set(python_lib-dynload    ${python_libs_dir}/lib-dynload                                        CACHE INTERNAL "Python path for lib-dynload"                     )
+set(python_site-packages  ${python_libs_dir}/site-packages                                      CACHE INTERNAL "Python path for site-packages"                   )
