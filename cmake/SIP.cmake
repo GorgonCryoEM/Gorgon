c55b2f794ddecb62c7cf5cf3d06876ecb3653b28
shadow_walker <shadowwalkersb@gmail.com>
2015-07-17 19:47:38 -0500

    empty string for PyQt4 & SIP urls

--- cmake/SIP.cmake
+++ cmake/SIP.cmake
@@ -1,6 +1,7 @@
 external_project_vars(
         Python
-        http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.8/sip-4.16.8.tar.gz
+#        http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.8/sip-4.16.8.tar.gz
+""
         "${PYTHON_EXECUTABLE};configure.py;${sip_extra_options}"
         "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
         "${CMAKE_MAKE_PROGRAM};install"
