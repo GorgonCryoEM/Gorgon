ffc432dac3d72e61394eccce8b31aae36fb96f9a
shadow_walker <shadowwalkersb@gmail.com>
2015-07-17 19:47:38 -0500

    empty string for PyQt4 & SIP urls

--- cmake/PyQt4.cmake
+++ cmake/PyQt4.cmake
@@ -1,6 +1,7 @@
 external_project_vars(
         SIP #Qt4
-        http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
+#        http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
+""
         "${PYTHON_EXECUTABLE};configure-ng.py;--confirm-license;--sip;${sip_bindir}/${sip_sip-module}"
         "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
         "${CMAKE_MAKE_PROGRAM};install"
