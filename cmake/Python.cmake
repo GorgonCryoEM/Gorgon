8be8ec460505270d9edd58f0d76b1473eddbee40
shadow_walker <shadowwalkersb@gmail.com>
2015-07-20 18:04:31 -0500

    fix Python rname

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -1,13 +1,15 @@
 if(APPLE)
     set(enable_shared --enable-framework=${python_install_prefix})
+    set(config_cmd "./configure;--prefix=${python_install_prefix};--with-pydebug;${enable_shared};--with-ensurepip=install;--enable-unicode=ucs4")
 else()
     set(enable_shared --enable-shared)
+    set(config_cmd "./configure;--prefix=${python_install_prefix};--with-pydebug;${enable_shared};--with-ensurepip=install;--enable-unicode=ucs4;LDFLAGS=-Wl,-rpath,${python_install_prefix}/lib")
 endif()
 
 external_project_vars(
         ""
         https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-        "./configure;--prefix=${python_install_prefix};--with-pydebug;${enable_shared};--with-ensurepip=install"
+        "${config_cmd}"
         "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
         "${CMAKE_MAKE_PROGRAM};install"
     )
