ce2ee0dd208a346dadc84327790152076cb4488e
shadow_walker <shadowwalkersb@gmail.com>
2015-07-21 16:49:53 -0500

    update PyQt4 with qmake_exec var

--- cmake/PyQt4.cmake
+++ cmake/PyQt4.cmake
@@ -2,7 +2,7 @@ external_project_vars(
         SIP #Qt4
 #        http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
 ""
-        "${PYTHON_EXECUTABLE};configure-ng.py;--confirm-license;--sip;${sip_bindir}/${sip_sip-module}"
+        "${PYTHON_EXECUTABLE};configure-ng.py;--confirm-license;--sip;${sip_bindir}/${sip_sip-module};--qmake=${QT_QMAKE_EXECUTABLE};--static"
         "${CMAKE_MAKE_PROGRAM};-j${NUMBER_OF_PARALLEL_JOBS}"
         "${CMAKE_MAKE_PROGRAM};install"
     )
