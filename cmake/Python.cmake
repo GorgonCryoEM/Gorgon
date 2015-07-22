b6b07f62d65e55c8f81bf8cb12a52ecffa10e1b4
shadow_walker <shadowwalkersb@gmail.com>
2015-07-22 00:43:36 -0500

    fix python_libs_dir for Linux

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -1,9 +1,11 @@
 if(APPLE)
     set(enable_shared --enable-framework=${python_install_prefix})
     set(config_cmd "./configure;--prefix=${python_install_prefix};--with-pydebug;${enable_shared};--with-ensurepip=install;--enable-unicode=ucs4")
+    set(python_libs_dir       ${python_install_prefix}/Python.framework/Versions/2.7/lib/python2.7  CACHE INTERNAL "Python directory for lib-dynload & site-settings")
 else()
     set(enable_shared --enable-shared)
     set(config_cmd "./configure;--prefix=${python_install_prefix};--with-pydebug;${enable_shared};--with-ensurepip=install;--enable-unicode=ucs4;LDFLAGS=-Wl,-rpath,${python_install_prefix}/lib")
+    set(python_libs_dir       ${python_install_prefix}/lib/python2.7  CACHE INTERNAL "Python directory for lib-dynload & site-settings")
 endif()
 
 external_project_vars(
@@ -32,6 +34,5 @@ update_libs_includes(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)
 
 set(python_url_md5_cmd        URL_MD5       d7547558fd673bd9d38e2108c6b42521  CACHE INTERNAL "")
 
-set(python_libs_dir       ${python_install_prefix}/Python.framework/Versions/2.7/lib/python2.7  CACHE INTERNAL "Python directory for lib-dynload & site-settings")
 set(python_lib-dynload    ${python_libs_dir}/lib-dynload                                        CACHE INTERNAL "Python path for lib-dynload"                     )
 set(python_site-packages  ${python_libs_dir}/site-packages                                      CACHE INTERNAL "Python path for site-packages"                   )
