51232501b32d6bf71ce4ff623ce6d9fc1dad47b3
shadow_walker <shadowwalkersb@gmail.com>
2015-07-30 14:05:22 -0500

    remove url hash keys

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -24,6 +24,3 @@ find_package(Boost ${boost_version} COMPONENTS ${boost_components})
 #list(APPEND GORGON_LIBRARIES    ${Boost_LIBRARIES}  )
 #list(APPEND GORGON_INCLUDE_DIRS ${Boost_INCLUDE_DIR})
 update_libs_includes(Boost_LIBRARIES Boost_INCLUDE_DIR)
-
-set(boost_url_sha1_cmd       URL_HASH SHA1=a27b010b9d5de0c07df9dddc9c336767725b1e6b  CACHE INTERNAL "")
-set(boost_url_md5_cmd        URL_MD5       5a5d5614d9a07672e1ab2a250b5defc5          CACHE INTERNAL "")
