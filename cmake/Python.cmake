c7673f5d75a8e703560e2df7a7eb3140a0e97486
shadow_walker <shadowwalkersb@gmail.com>
2015-07-30 14:05:22 -0500

    remove url hash keys

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -14,8 +14,6 @@ find_package(PythonLibs)
 #list(APPEND GORGON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
 update_libs_includes(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)
 
-set(python_url_md5_cmd        URL_MD5       d7547558fd673bd9d38e2108c6b42521  CACHE INTERNAL "")
-
 set(python_libs_dir       ${python_install_prefix}/${python_framework_extra_path}/lib/python2.7 CACHE INTERNAL "Python directory for lib-dynload & site-settings")
 set(python_lib-dynload    ${python_libs_dir}/lib-dynload                                        CACHE INTERNAL "Python path for lib-dynload"                     )
 set(python_site-packages  ${python_libs_dir}/site-packages                                      CACHE INTERNAL "Python path for site-packages"                   )
