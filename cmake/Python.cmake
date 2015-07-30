5d5de641a9664f068d3a5e60b67642b0703f1d76
shadow_walker <shadowwalkersb@gmail.com>
2015-07-30 14:23:59 -0500

    fix Python internal vars for lib-dynload and site-settings

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -5,6 +5,5 @@ find_package(PythonLibs)
 #list(APPEND GORGON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
 update_libs_includes(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)
 
-set(python_libs_dir       ${python_install_prefix}/${python_framework_extra_path}/lib/python2.7 CACHE INTERNAL "Python directory for lib-dynload & site-settings")
-set(python_lib-dynload    ${python_libs_dir}/lib-dynload                                        CACHE INTERNAL "Python path for lib-dynload"                     )
-set(python_site-packages  ${python_libs_dir}/site-packages                                      CACHE INTERNAL "Python path for site-packages"                   )
+set(Python_lib-dynload    "FIND-ME"  CACHE PATH "Python path for lib-dynload"  )
+set(Python_site-packages  "FIND-ME"  CACHE PATH "Python path for site-packages, needed for pip")
