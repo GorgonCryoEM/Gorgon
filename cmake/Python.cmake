0a4fe4056495aa31513715a800bbcea34fcdc381
shadow_walker <shadowwalkersb@gmail.com>
2015-07-30 14:26:04 -0500

    remove installing Python modules
    It requires root permissions. Better not modify the system through CMake

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -4,6 +4,3 @@ find_package(PythonLibs)
 #list(APPEND GORGON_LIBRARIES    ${PYTHON_LIBRARY}    )
 #list(APPEND GORGON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
 update_libs_includes(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)
-
-set(Python_lib-dynload    "FIND-ME"  CACHE PATH "Python path for lib-dynload"  )
-set(Python_site-packages  "FIND-ME"  CACHE PATH "Python path for site-packages, needed for pip")
