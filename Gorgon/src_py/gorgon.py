3d4f7723b44df54f401374c81a2c32f98d390fd7
shadow_walker <shadowwalkersb@gmail.com>
2015-08-06 17:12:06 -0500

    working standalone script, needs one or two more tweaks
    Conflicts:
    	Gorgon/src_py/playground.py

--- Gorgon/src_py/gorgon.py
+++ Gorgon/src_py/gorgon.py
@@ -25,27 +25,27 @@ from main_window_form import MainWindowForm
 import time
 
 
-if __name__ == '__main__':
-    gorgonVersion = '2.2.2'
-    app = QtGui.QApplication(sys.argv)    
-    #setting different font sizes for different OSs
-    if(sys.platform == 'win32'):
-        pass
-    elif(sys.platform == 'darwin'):
-        app.setStyleSheet("* { font-size: 10px }")
-    else :
-        app.setStyleSheet("* { font-size: 10px }")
-        
-    pixmap = QtGui.QPixmap(pathname + "/splash.png")
-    splash = QtGui.QSplashScreen(pixmap, QtCore.Qt.WindowStaysOnTopHint)
-    splash.setMask(pixmap.mask())
-    splash.show()
-    app.processEvents()
+# if __name__ == '__main__':
+gorgonVersion = '2.2.2'
+app = QtGui.QApplication(sys.argv)    
+#setting different font sizes for different OSs
+if(sys.platform == 'win32'):
+    pass
+elif(sys.platform == 'darwin'):
+    app.setStyleSheet("* { font-size: 10px }")
+else :
+    app.setStyleSheet("* { font-size: 10px }")
     
-    window = MainWindowForm(gorgonVersion)
-    window.isNightlyBuild = True
-    window.showMaximized()
-    splash.finish(window)
-    window.loadPlugins()
-    app.exec_()
+pixmap = QtGui.QPixmap(pathname + "/splash.png")
+splash = QtGui.QSplashScreen(pixmap, QtCore.Qt.WindowStaysOnTopHint)
+splash.setMask(pixmap.mask())
+splash.show()
+app.processEvents()
+
+window = MainWindowForm(gorgonVersion)
+window.isNightlyBuild = True
+window.showMaximized()
+splash.finish(window)
+window.loadPlugins()
+#     app.exec_()
     
