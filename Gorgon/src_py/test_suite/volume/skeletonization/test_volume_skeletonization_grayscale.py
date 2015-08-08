2460b3dea741c3db30b2d80f0e3cd1889a3976e7
shadow_walker <shadowwalkersb@gmail.com>
2015-08-07 22:37:30 -0500

    remove call of skeletonization and just load the form

--- Gorgon/src_py/test_suite/volume/skeletonization/test_volume_skeletonization_grayscale.py
+++ Gorgon/src_py/test_suite/volume/skeletonization/test_volume_skeletonization_grayscale.py
@@ -6,22 +6,12 @@ from gorgon import *
 
 volumeViewer             = window.windowManager.volumeViewer
 grayscaleSkeletonizer    = volumeViewer.grayscaleSkeletonizer
+action = grayscaleSkeletonizer.app.actions.getAction("perform_VolumeGrayscaleSkeletonization")
 
 volumeViewer.loadDataFromFile('densityMap.mrc')
 
-skeletonRenderer = grayscaleSkeletonizer.app.viewers["skeleton"].renderer
-skeleton = grayscaleSkeletonizer.viewer.renderer.performPreservingGrayscaleSkeletonizationAbeysinghe2008(
-                    skeletonRenderer.getMesh(),
-                    grayscaleSkeletonizer.getStartingDensity(),
-                    grayscaleSkeletonizer.getStepCount(),
-                    grayscaleSkeletonizer.getMinCurveLength(),
-                    grayscaleSkeletonizer.getMinSurfaceSize(),
-                    grayscaleSkeletonizer.getCurveRadius(),
-                    grayscaleSkeletonizer.getSurfaceRadius(),
-                    grayscaleSkeletonizer.getSkeletonRadius()
-                )
+action.trigger()
+grayscaleSkeletonizer.accept()
 
 viewer   = grayscaleSkeletonizer.app.viewers["skeleton"]
-viewer.loadVolume(skeleton)
-
 viewer.renderer.saveFile('grayscale_skeletonized_test.mrc')
