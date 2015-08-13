b86b7ede71f43caca6a4084c48cbbe8fb3e63d1c
shadow_walker <shadowwalkersb@gmail.com>
2015-08-13 15:53:49 -0500

    add source subdirectory: hello & core

---
+++ src/hello/hello.cpp
@@ -0,0 +1,12 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: Aug 13, 2015
+ *      Author: shadow_walker
+ */
+
+#include "hello.h"
+
+std::string hello(){
+	return "Hello World!";
+}
---
+++ src/hello/hello.h
@@ -0,0 +1,8 @@
+#ifndef SRC_CORE_HELLO_H_
+#define SRC_CORE_HELLO_H_
+
+#include <string>
+
+std::string hello();
+
+#endif /* SRC_CORE_HELLO_H_ */
