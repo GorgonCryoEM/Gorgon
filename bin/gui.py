cb4f8654399788097e12f8e14a2cea8b9b8897bb
shadow_walker <shadowwalkersb@gmail.com>
2015-08-14 20:18:34 -0500

    load and show volume attempt

--- bin/gui.py
+++ bin/gui.py
@@ -2,3 +2,13 @@
 
 # import libpygorgon
 import gorg
+
+import os, sys
+
+pathname = os.path.abspath(gorg.__path__[0]+'../../../../groel-demo')
+print pathname
+
+gorg.window.show()
+
+volumeViewer = gorg.window.windowManager.volumeViewer
+volumeViewer.loadDataFromFile(pathname +'/densityMap.mrc')
