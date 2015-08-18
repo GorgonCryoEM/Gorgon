eb4053e669495bd56e26779f3c623e937eb3307f
shadow_walker <shadowwalkersb@gmail.com>
2015-08-18 11:31:34 -0500

    remove createActions and createMenus: seem to have only session related

--- gorg/window_manager.py
+++ gorg/window_manager.py
@@ -12,7 +12,6 @@ from volume_viewer import VolumeViewer
 # from calpha_viewer import CAlphaViewer
 # from calpha_reference_viewer import CAlphaReferenceViewer
 import datetime
-# from session_manager import SessionManager
 
 class WindowManager(QtGui.QWidget):
     
@@ -37,31 +36,6 @@ class WindowManager(QtGui.QWidget):
         self.mainCamera = Camera([self.volumeViewer], self.app)
         self.app.mainCamera = self.mainCamera   
         self.app.setCentralWidget(self.mainCamera)
-        
-    def createActions(self):
-        self.saveSessionAct = QtGui.QAction(self.tr("Sessio&n..."), self)
-        self.saveSessionAct.setStatusTip(self.tr("Save the current session"))
-        self.connect(self.saveSessionAct, QtCore.SIGNAL("triggered()"), self.saveSession)
-        self.app.actions.addAction("save_Session", self.saveSessionAct)  
-        
-        self.loadSessionAct = QtGui.QAction(self.tr("Sessio&n..."), self)
-        self.loadSessionAct.setStatusTip(self.tr("Load a session from file"))
-        self.connect(self.loadSessionAct, QtCore.SIGNAL("triggered()"), self.loadSession)
-        self.app.actions.addAction("load_Session", self.loadSessionAct)          
-
-        self.closeSessionAct = QtGui.QAction(self.tr("Sessio&n..."), self)
-        self.closeSessionAct.setStatusTip(self.tr("Close session"))
-        self.connect(self.closeSessionAct, QtCore.SIGNAL("triggered()"), self.closeSession)
-        self.app.actions.addAction("close_Session", self.closeSessionAct)          
-            
-    def createMenus(self):
-        self.app.menus.addAction("file-save-session", self.app.actions.getAction("save_Session"), "file-save")
-        self.app.menus.getMenu("file-save").addSeparator()
-        self.app.menus.addAction("file-open-session", self.app.actions.getAction("load_Session"), "file-open")
-        self.app.menus.getMenu("file-open").addSeparator()
-        self.app.menus.addAction("file-close-session", self.app.actions.getAction("close_Session"), "file-close")
-        self.app.menus.getMenu("file-close").addSeparator()
-
         
     def closeSession(self):
         if (QtGui.QMessageBox.warning(self, self.tr("Are you sure?"), self.tr("You are about to close the session.  All unsaved data will be lost!"), QtGui.QMessageBox.Yes, QtGui.QMessageBox.Cancel) == QtGui.QMessageBox.Yes):
