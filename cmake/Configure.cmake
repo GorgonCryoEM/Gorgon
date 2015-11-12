6016c7e6edee5068fe4b513bada6beae22c7bf9c
shadow_walker <shadowwalkersb@gmail.com>
2015-11-12 13:37:04 -0600

    config_file write setup_win.py

--- cmake/Configure.cmake
+++ cmake/Configure.cmake
@@ -1,3 +1,14 @@
 configure_file(${CMAKE_SOURCE_DIR}/Gorgon/src_py/gorgon.pyw.in
                 ${CMAKE_BINARY_DIR}/run/gorgon.pyw
                )
+
+
+if(WIN32)
+    set(package_setup_file ${CMAKE_SOURCE_DIR}/setup_win.py.in)
+endif()
+
+if(WIN32)
+    configure_file(${package_setup_file}
+                    ${CMAKE_BINARY_DIR}/run/setup.py
+                   )
+endif()
