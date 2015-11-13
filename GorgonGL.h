ded78582e9fd6f5eb7fc3cabd9747bfe261f20a0
shadow_walker <shadowwalkersb@gmail.com>
2015-11-13 13:28:54 -0600

    new include to handle glut.h

---
+++ GorgonGL.h
@@ -0,0 +1,20 @@
+/*
+ * GorgonGL.h
+ *
+ *  Created on: Nov 13, 2015
+ *      Author: shadow_walker
+ */
+
+#ifndef GORGONGL_H_
+#define GORGONGL_H_
+
+
+#ifndef __APPLE__
+    #include <GL/glut.h>
+#else
+    #include <GLUT/glut.h>
+#endif
+
+
+
+#endif /* GORGONGL_H_ */
