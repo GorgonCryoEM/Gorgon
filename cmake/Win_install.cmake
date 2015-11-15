c82ad6e625d9d0f0218621f7bd33a3e8e540a01a
shadow_walker <shadowwalkersb@gmail.com>
2015-11-14 21:46:00 -0600

    install boost_python and FFTW3F dlls on Windows

--- cmake/Win_install.cmake
+++ cmake/Win_install.cmake
@@ -4,4 +4,27 @@ if(WIN32)
                      PREFIX lib
                      SUFFIX .pyd
                      )
+# --------------------------------------------------------------------
+    function(install_dll lib)
+        set(dll ${lib})
+        
+#        message(STATUS "dll: ${dll}")
+        
+        string(REGEX REPLACE "[.]lib"
+               ".dll"
+               dll
+               ${dll}
+              )
+        
+#        message(STATUS "dll: ${dll}")
+        
+        install(FILES ${dll}
+                DESTINATION ${CMAKE_BINARY_DIR}/run
+                COMPONENT "Gorgon"
+                )
+        
+    endfunction()
+# --------------------------------------------------------------------
+    install_dll(${Boost_PYTHON_LIBRARY_RELEASE})
+    install_dll(${FFTW3F_LIBRARIES})
 endif()
