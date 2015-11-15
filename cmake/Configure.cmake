5a7f758c8184d44cafd33192f14ce112534866d8
shadow_walker <shadowwalkersb@gmail.com>
2015-11-14 23:12:43 -0600

    remove gorgon.pyw and write it to binary tree via config_file()

--- cmake/Configure.cmake
+++ cmake/Configure.cmake
@@ -1,3 +1,3 @@
 configure_file(${CMAKE_SOURCE_DIR}/Gorgon/src_py/gorgon.pyw.in
-                ${CMAKE_SOURCE_DIR}/Gorgon/src_py/gorgon.pyw
+                ${CMAKE_BINARY_DIR}/run/gorgon.pyw
                )
