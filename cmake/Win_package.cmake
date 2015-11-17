7bafda5cfb81f8cb22c02dd2d578355f3293f333
shadow_walker <shadowwalkersb@gmail.com>
2015-11-17 14:14:27 -0600

    website for list of options

--- cmake/Win_package.cmake
+++ cmake/Win_package.cmake
@@ -1,3 +1,6 @@
+# For list of options:
+# http://www.py2exe.org/index.cgi/ListOfOptions
+
 install(CODE "execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_BINARY_DIR}/run/setup.py py2exe
                                                     --dist-dir=${CMAKE_BINARY_DIR}/package/dist
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/run/)"
