e688ca902f9bfefad1a4800d705dc2400bad7279
shadow_walker <shadowwalkersb@gmail.com>
2015-11-17 15:01:52 -0600

    use var: package_dir

--- cmake/Win_package.cmake
+++ cmake/Win_package.cmake
@@ -2,7 +2,7 @@
 # http://www.py2exe.org/index.cgi/ListOfOptions
 
 install(CODE "execute_process(COMMAND ${PYTHON_EXECUTABLE} ${run_dir}/setup.py py2exe
-                                                    --dist-dir=${CMAKE_BINARY_DIR}/dist
+                                                    --dist-dir=${package_dir}
     WORKING_DIRECTORY ${run_dir})"
     COMPONENT "Package"
     )
