77b3d3cdfd3b7bed552c484889d6e3d4d2cac6b5
shadow_walker <shadowwalkersb@gmail.com>
2015-11-17 14:02:06 -0600

    remove package/ from build tree and move dist/ into build tree

--- cmake/Mac_package.cmake
+++ cmake/Mac_package.cmake
@@ -1,13 +1,13 @@
 install(CODE "execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_BINARY_DIR}/run/setup.py py2app
-                                                    --bdist-base=${CMAKE_BINARY_DIR}/package/build
-                                                    --dist-dir=${CMAKE_BINARY_DIR}/package/dist
+                                                    --bdist-base=${CMAKE_BINARY_DIR}/build
+                                                    --dist-dir=${CMAKE_BINARY_DIR}/dist
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/run/)"
     COMPONENT "Package"
     )
     
 install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E rename
-                                        package/dist/Gorgon.app/Contents/Frameworks/libpyGORGON.so
-                                        package/dist/Gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/libpyGORGON.so
+                                        dist/Gorgon.app/Contents/Frameworks/libpyGORGON.so
+                                        dist/Gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/libpyGORGON.so
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR})"
     COMPONENT "Package"
     )
--- cmake/Win_package.cmake
+++ cmake/Win_package.cmake
@@ -2,7 +2,7 @@
 # http://www.py2exe.org/index.cgi/ListOfOptions
 
 install(CODE "execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_BINARY_DIR}/run/setup.py py2exe
-                                                    --dist-dir=${CMAKE_BINARY_DIR}/package/dist
+                                                    --dist-dir=${CMAKE_BINARY_DIR}/dist
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/run/)"
     COMPONENT "Package"
     )
