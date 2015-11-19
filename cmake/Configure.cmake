3f9949f5513d813cbc81876e47e88ba0e4f81376
shadow_walker <shadowwalkersb@gmail.com>
2015-11-18 23:18:19 -0600

    configure_file: Mac and Windows

--- cmake/Configure.cmake
+++ cmake/Configure.cmake
@@ -2,12 +2,15 @@ configure_file(${CMAKE_SOURCE_DIR}/Gorgon/src_py/gorgon.pyw.in
                 ${CMAKE_BINARY_DIR}/run/gorgon.pyw
                )
 
+if(APPLE)
+    set(package_setup_file ${CMAKE_SOURCE_DIR}/setup_mac.py.in)
+endif()
 
 if(WIN32)
     set(package_setup_file ${CMAKE_SOURCE_DIR}/setup_win.py.in)
 endif()
 
-if(WIN32)
+if(APPLE OR WIN32)
     configure_file(${package_setup_file}
                     ${CMAKE_BINARY_DIR}/run/setup.py
                    )
