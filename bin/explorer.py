61f22fc86722b7cab6172dc5b80ebef0f976ed23
shadow_walker <shadowwalkersb@gmail.com>
2015-12-01 13:40:06 -0600

    add explorer.py, __init__: Explorer

---
+++ bin/explorer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from PyQt4 import QtGui, QtCore, QtOpenGL
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+import sys
+
+
+class GLWidget(QtOpenGL.QGLWidget):
+
+    def __init__(self):
+        super(GLWidget, self).__init__()
+        
+        self.setMinimumSize(300, 300)
+        self.setWindowTitle('Gorgon Explorer - alpha')
+        
+    def initializeGL(self):
+        glEnable(GL_DEPTH_TEST)
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClearColor(0,0,0,1)
+        
+    def resizeGL(self, w, h):
+        glViewport(0,0,w,h)
+        
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    window = GLWidget()
+    window.showNormal()
+    window.show()
+    window.raise_()
+    window.resize(800, 800)
+    window.move(300,50)
+    
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
