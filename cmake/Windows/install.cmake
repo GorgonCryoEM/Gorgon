579689612be07f06826da084587da06e7bf1b37e
shadow_walker <shadowwalkersb@gmail.com>
2015-12-14 16:39:09 -0600

    remove function: install_dll

--- cmake/Windows/install.cmake
+++ cmake/Windows/install.cmake
@@ -4,27 +4,4 @@ if(WIN32)
                      PREFIX lib
                      SUFFIX .pyd
                      )
-# --------------------------------------------------------------------
-    function(install_dll lib)
-        set(dll ${lib})
-        
-#        message(STATUS "dll: ${dll}")
-        
-        string(REGEX REPLACE "[.]lib"
-               ".dll"
-               dll
-               ${dll}
-              )
-        
-#        message(STATUS "dll: ${dll}")
-        
-        install(FILES ${dll}
-                DESTINATION ${run_dir}
-                COMPONENT "Gorgon"
-                )
-        
-    endfunction()
-# --------------------------------------------------------------------
-    install_dll(${Boost_PYTHON_LIBRARY_RELEASE})
-    install_dll(${FFTW3F_LIBRARIES})
 endif()
