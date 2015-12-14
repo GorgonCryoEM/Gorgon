4c05abf3f4019109d91aa9385ffe7b1e39f72e2a
shadow_walker <shadowwalkersb@gmail.com>
2015-12-14 16:40:04 -0600

    use function: add_dll

--- cmake/Windows/init.cmake
+++ cmake/Windows/init.cmake
@@ -1,4 +1,7 @@
 if(WIN32)
+    add_dll(${Boost_PYTHON_LIBRARY_RELEASE})
+    add_dll(${FFTW3F_LIBRARIES})
+    
     if(MSVC AND GORGON_TARGET_ARCH EQUAL 64)
     	ADD_DEFINITIONS(/bigobj)
     endif()
