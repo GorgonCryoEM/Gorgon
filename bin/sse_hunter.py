349dc292edb708ebbcbff9001d605c9e20850d64
shadow_walker <shadowwalkersb@gmail.com>
2016-01-01 22:16:44 -0600

    add parser

--- bin/sse_hunter.py
+++ bin/sse_hunter.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
+import argparse
+
 from Toolkit.sse import VolumeSSEBuilderForm
 
 
 def main():
     sseh = VolumeSSEBuilderForm()
+    parser = argparse.ArgumentParser(description='Gorgon SSEHunter')
+    
+    parser.add_argument('volume', action="store")
+    parser.add_argument('skeleton', action="store")
+    parser.add_argument('output', action="store")
+    
+    args = parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
