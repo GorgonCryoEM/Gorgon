ebfe30e1f8d505ad35bd743df97f85830a8a0eee
shadow_walker <shadowwalkersb@gmail.com>
2016-01-10 01:57:25 -0600

    remove bool from init()

--- src/core/SkeletonMaker/VolumeData.h
+++ src/core/SkeletonMaker/VolumeData.h
@@ -42,7 +42,7 @@ namespace SkeletonMaker {
         void setDataAt(int index, float value);
         void pad(int padBy, double padValue);
     private:
-        void init(int sizeX, int sizeY, int sizeZ, float spacingX, float spacingY, float spacingZ, float originX, float originY, float originZ, bool initializeData, float val=0.0);
+        void init(int sizeX, int sizeY, int sizeZ, float spacingX, float spacingY, float spacingZ, float originX, float originY, float originZ, float val=0.0);
         void setSize(int sizeX, int sizeY, int sizeZ);
     protected:
         Dim3D<int> size;
@@ -52,22 +52,20 @@ namespace SkeletonMaker {
     };
 
     VolumeData::VolumeData() {
-      init(0, 0, 0, 1, 1, 1, 0, 0, 0, true);
+      init(0, 0, 0, 1, 1, 1, 0, 0, 0);
     }
 
     VolumeData::VolumeData(int sizeX, int sizeY, int sizeZ, float val) {
-        init(sizeX, sizeY, sizeZ, 1, 1, 1, 0, 0, 0, true, val);
+        init(sizeX, sizeY, sizeZ, 1, 1, 1, 0, 0, 0, val);
     }
 
     VolumeData::~VolumeData() {}
 
-    void VolumeData::init(int sizeX, int sizeY, int sizeZ, float spacingX, float spacingY, float spacingZ, float originX, float originY, float originZ, bool initializeData, float val) {
+    void VolumeData::init(int sizeX, int sizeY, int sizeZ, float spacingX, float spacingY, float spacingZ, float originX, float originY, float originZ, float val) {
         setSize(sizeX, sizeY, sizeZ);
         setSpacing(spacingX, spacingY, spacingZ);
         setOrigin(originX, originY, originZ);
-        if(initializeData) {
-          data.assign(data.size(), val);
-        }
+        data.assign(data.size(), val);
     }
 
     int VolumeData::getSizeX() const {
