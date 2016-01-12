3bf559c0d92913304bbc88f15cbdca05f758f433
shadow_walker <shadowwalkersb@gmail.com>
2016-01-11 22:33:36 -0600

    test_skeletonization.py

---
+++ tests/TestSuite/skeletonization/test_skeletonization.py
@@ -0,0 +1,20 @@
+from ..toolkit_tests import ToolkitTestCases
+
+
+class Skeletonization(ToolkitTestCases.ToolkitTests):
+
+	def __init__(self):
+		self.py = 'skeletonization.py'
+		self.fprefix = 'skeleton_'
+		ToolkitTestCases.ToolkitTests.__init__(self)
+
+	def test_binary(self):
+		self.run('binary')
+	
+	def test_grayscale(self):
+		self.run('grayscale')
+		
+	
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
