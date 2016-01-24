042da3faf3ce4ca458157b1c1220278e59f148a8
shadow_walker <shadowwalkersb@gmail.com>
2016-01-24 00:53:35 -0600

    remove gui related and unnecessary

--- bin/Toolkit/sse/volume_sse_builder_form.py
+++ bin/Toolkit/sse/volume_sse_builder_form.py
@@ -39,40 +39,6 @@ class VolumeSSEBuilderForm(object):
     def savePseudoatoms(self):
         self.calphaRenderer.saveSSEHunterFile(self.output)
     
-    def removeHelices(self):
-        self.viewer.renderer.removeHelices()
-        
-    def removeSheets(self):
-        self.viewer.renderer.removeSheets()
-
-    def browseAtomScoreFile(self, result):
-        pdbFile = QtGui.QFileDialog.getOpenFileName(self, self.tr("Load SSEHunter Results"), "", self.tr("PDB Files (*.pdb)"))
-        if not pdbFile.isEmpty():
-            self.calphaViewer = self.app.viewers["calpha"]
-            self.calphaViewer.loadSSEHunterData(pdbFile)
-            self.sseViewer = self.app.viewers["sse"]
-            self.lineEditAtomScore.setText(pdbFile)
-            self.connect(self.app.viewers["calpha"],  QtCore.SIGNAL("modelUnloaded()"), self.disableSavePseudoatoms)
-            self.pushButtonSavePseudoatoms.setEnabled(True)
-        
-    def autoBuildHelices(self):
-        print "VolumeSSEBuilderForm.autoBuildHelices()"
-        self.calphaViewer = self.app.viewers["calpha"]
-        patom_hashkeys = self.calphaViewer.renderer.getAtomHashes();
-        patoms = [self.calphaViewer.renderer.getAtom(hashkey) for hashkey in patom_hashkeys]
-        
-        score_thresh = self.doubleSpinBoxScoreThresh.value()
-        pt_line_dist_thresh = self.horizontalSliderLinearityThresh.value() / 4.0
-        auto_helix_builder = AutoHelixBuilderEngine(patoms, score_thresh, pt_line_dist_thresh)
-        helix_list = auto_helix_builder.get_helix_list()
-        print helix_list
-        for helix in helix_list:
-            for atom in helix:
-                atom.setSelected(True)
-            self.selectionToHelix(None)
-            for atom in helix:
-                atom.setSelected(False)
-        
     def runSSEHunter(self):
         self.logger.debug(self.volume)
         self.logger.debug("self.volume.getSize(): %d" % self.volume.getSize())
@@ -93,21 +59,3 @@ class VolumeSSEBuilderForm(object):
         
         for pseudoatom in patoms:
             self.calphaRenderer.addAtom(pseudoatom)
-        
-    def updateTotalScoreSSEHunterAtoms(self):
-        self.calphaViewer.updateTotalScoreSSEHunterAtoms( self.doubleSpinBoxCorrelation.value(), self.doubleSpinBoxSkeleton.value(),
-            self.doubleSpinBoxGeometry.value() )
-        
-    def getHelixEnds(self, atoms):
-        distance = 0
-        ends = []
-        for pt1 in atoms:
-            pos1 = pt1.getPosition()
-            for pt2 in atoms:
-                pos2 = pt2.getPosition()
-                atomDist = (pos2 - pos1).length()
-                if(atomDist > distance):
-                    distance = atomDist
-                    ends = [pos1, pos2]
-        return ends
-                
