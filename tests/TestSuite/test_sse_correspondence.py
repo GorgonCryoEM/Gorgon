1f5c668ef3947c1d5cee8dbf63c58f90fc8bc502
shadow_walker <shadowwalkersb@gmail.com>
2016-01-26 22:12:31 -0600

    test script & ref file: SSE Correspondence

---
+++ tests/TestSuite/test_sse_correspondence.py
@@ -0,0 +1,25 @@
+from .toolkit_tests import ToolkitTestCases
+
+
+class TestSSECorrespondence(ToolkitTestCases.ToolkitTests):
+
+	def __init__(self):
+		self.py = 'sse_correspondence.py'
+		ToolkitTestCases.ToolkitTests.__init__(self)
+
+	def test_sse_correspondence(self):
+		input1 = join(self.indir, 'densityMap-skeleton.mrc')
+		input2 = join(self.indir, 'groel-segment.seq')
+		input3 = join(self.indir, 'helices-densityMap.wrl')
+		output = join(self.outdir, 'sse_correspondences.txt')
+		ref    = join(self.refdir, 'sse_correspondences.txt')
+
+		cmd = '%s %s %s %s %s' % (self.exe, input1, input2, input3, output)
+		
+		check_call([cmd], shell=True)
+		assert cmp(output, ref), "\nFiles differ:\n   1: %s\n   2: %s" % (output, ref)
+		
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
