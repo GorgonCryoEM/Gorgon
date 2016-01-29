fc7acc698655721a8f4f0e4a7ef9abc20e15f5ab
shadow_walker <shadowwalkersb@gmail.com>
2016-01-29 16:56:39 -0600

    get_inputs()

--- tests/TestSuite/test_sse_correspondence.py
+++ tests/TestSuite/test_sse_correspondence.py
@@ -5,6 +5,9 @@ class TestSSECorrespondence(ToolkitTestCases.ToolkitTests):
 
 	def __init__(self):
 		ToolkitTestCases.ToolkitTests.__init__(self, 'corr', prog_name='sse_correspondence.py')
+		
+	def get_inputs(self):
+		return [self.skeleton, self.segment, self.helices]
 
 	def test_sse_correspondence(self):
 		self.run()
