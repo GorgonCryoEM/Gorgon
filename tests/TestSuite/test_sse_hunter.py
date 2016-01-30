18f9dc18610dba05472a7d1c0753736a71701658
shadow_walker <shadowwalkersb@gmail.com>
2016-01-29 23:09:54 -0600

    class name: TestSSEHunter, cleanup, get_inputs(), rename ref file

--- tests/TestSuite/test_sse_hunter.py
+++ tests/TestSuite/test_sse_hunter.py
@@ -1,27 +1,17 @@
-from os.path import join
-
-from subprocess import check_call
-from filecmp import cmp
-
 from .toolkit_tests import ToolkitTestCases
 
 
-class TestFilter(ToolkitTestCases.ToolkitTests):
+class TestSSEHunter(ToolkitTestCases.ToolkitTests):
 
 	def __init__(self):
-		self.py = 'sse_hunter.py'
-		ToolkitTestCases.ToolkitTests.__init__(self)
+		ToolkitTestCases.ToolkitTests.__init__(self, 'pdb', prog_name='sse_hunter.py')
 
+	def get_inputs(self):
+		return [self.volume, self.skeleton]
+	
 	def test_sse_hunter(self):
-		input2 = join(self.indir, 'densityMap-skeleton.mrc')
-		output = join(self.outdir, 'pseudoatoms.pdb')
-		ref    = join(self.refdir, 'scored_pseudoatoms.pdb')
+		self.run()
 
-		cmd = '%s %s %s %s' % (self.exe, self.input, input2, output)
-		
-		check_call([cmd], shell=True)
-		assert cmp(output, ref), "\nFiles differ:\n   1: %s\n   2: %s" % (output, ref)
-		
 	test_sse_hunter.long = True
 	
 if __name__ == '__main__':
