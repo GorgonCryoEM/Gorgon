098cbb18789813418fb3cf3fe852c894f2b855cc
shadow_walker <shadowwalkersb@gmail.com>
2016-01-31 21:52:57 -0600

    remove related calls

--- src/core/GraySkeletonCPP/VolumeFormatConverter.h
+++ src/core/GraySkeletonCPP/VolumeFormatConverter.h
@@ -3,11 +3,6 @@
 
 //#include <string>
 #include <Foundation/StringUtils.h>
-#include "VolumeReaderRAW.h"
-#include "VolumeReaderATOM.h"
-#include "VolumeReaderTXT.h"
-#include "VolumeReaderPTS.h"
-#include "VolumeReaderTNS.h"
 #include <SkeletonMaker/reader.h>
 #include "GraySkeletonCPP/ImageReaderMRC.h"
 #include "GraySkeletonCPP/ImageReaderBMP.h"
@@ -47,28 +42,6 @@ namespace GraySkeletonCPP {
         Volume * vol = NULL;
         if(strcmp(inputFormat.c_str(), "MRC") == 0) {
             vol = MRCReaderPicker::pick((char *)inputFile.c_str())->getVolume();
-        } else if(strcmp(inputFormat.c_str(), "CCP4") == 0) {
-            vol = MRCReaderPicker::pick((char *)inputFile.c_str())->getVolume();
-        } else if(strcmp(inputFormat.c_str(), "MAP") == 0) {
-            vol = MRCReaderPicker::pick((char *)inputFile.c_str())->getVolume();
-        } else if(strcmp(inputFormat.c_str(), "PTS") == 0) {
-            vol = VolumeReaderPTS::LoadVolume(inputFile);
-        } else if(strcmp(inputFormat.c_str(), "RAW") == 0) {
-            switch(bitsPerCell) {
-                case(8):
-                    vol = VolumeReaderRAW::LoadVolume8bit(inputFile, sizeX, sizeY, sizeZ, 1, 1, 1);
-                    break;
-                case(16):
-                    vol = VolumeReaderRAW::LoadVolume16bit(inputFile, sizeX, sizeY, sizeZ, 1, 1, 1);
-                    break;
-                default:
-                    printf("Input format [%s] (%d bits) not supported!\n", (char *)inputFormat.c_str(), bitsPerCell);
-                    break;
-            }
-        } else if (strcmp(inputFormat.c_str(), "ATOM") == 0) {
-            vol = VolumeReaderATOM::LoadVolume(inputFile);
-        } else if (strcmp(inputFormat.c_str(), "TXT") == 0) {
-            vol = VolumeReaderTXT::LoadVolume(inputFile);
         } else {
             printf("Input format [%s] not supported!\n", (char *)inputFormat.c_str());
         }
@@ -89,22 +62,6 @@ namespace GraySkeletonCPP {
 
         if(strcmp(inputFormat.c_str(), "MRC") == 0) {
             vol = MRCReaderPicker::pick((char *)inputFile.c_str())->getVolume();
-        } else if(strcmp(inputFormat.c_str(), "CCP4") == 0) {
-            vol = MRCReaderPicker::pick((char *)inputFile.c_str())->getVolume();
-        } else if(strcmp(inputFormat.c_str(), "MAP") == 0) {
-            vol = MRCReaderPicker::pick((char *)inputFile.c_str())->getVolume();
-        } else if(strcmp(inputFormat.c_str(), "PTS") == 0) {
-            vol = VolumeReaderPTS::LoadVolume(inputFile);
-        } else if (strcmp(inputFormat.c_str(), "RAW8") == 0) {
-            vol = VolumeReaderRAW::LoadVolume8bit(inputFile, sizeX, sizeY, sizeZ, 1, 1, 1);
-        } else if (strcmp(inputFormat.c_str(), "RAW16") == 0) {
-            vol = VolumeReaderRAW::LoadVolume16bit(inputFile, sizeX, sizeY, sizeZ, 1, 1, 1);
-        } else if (strcmp(inputFormat.c_str(), "ATOM") == 0) {
-            vol = VolumeReaderATOM::LoadVolume(inputFile);
-        } else if (strcmp(inputFormat.c_str(), "TXT") == 0) {
-            vol = VolumeReaderTXT::LoadVolume(inputFile);
-        } else if(strcmp(inputFormat.c_str(), "BMP") == 0) {
-            vol = ImageReaderBMP::LoadVolumeAsImageSet(inputFile, sizeZ);
         } else {
             printf("Input format [%s] not supported!\n", inputFormat.c_str());
             return;
@@ -114,24 +71,6 @@ namespace GraySkeletonCPP {
 
         if(strcmp(outputFormat.c_str(), "MRC") == 0) {
             vol->toMRCFile((char *)outputFile.c_str());
-        } else if(strcmp(outputFormat.c_str(), "CCP4") == 0) {
-            vol->toMRCFile((char *)outputFile.c_str());
-        } else if(strcmp(outputFormat.c_str(), "MAP") == 0) {
-            vol->toMRCFile((char *)outputFile.c_str());
-        } else if(strcmp(outputFormat.c_str(), "BMP") == 0) {
-            ImageReaderBMP::SaveVolumeAsImageSet(vol, outputFile);
-        } else if(strcmp(outputFormat.c_str(), "BMPPROJ") == 0) {
-            ImageReaderMRC::SaveGrayscaleImage(vol, outputFile);
-        } else if (strcmp(outputFormat.c_str(), "NB") == 0) {
-            vol->toMathematicaFile((char *)outputFile.c_str());
-        } else if (strcmp(outputFormat.c_str(), "OFF0") == 0) {
-            vol->toOFFCells((char *)outputFile.c_str());
-        } else if (strcmp(outputFormat.c_str(), "OFF") == 0) {
-            vol->toOFFCells2((char *)outputFile.c_str());
-        } else if (strcmp(outputFormat.c_str(), "RAW16") == 0) {
-            VolumeReaderRAW::SaveVolume16bit(vol, outputFile);
-        } else if (strcmp(outputFormat.c_str(), "TNS") == 0) {
-            VolumeReaderTNS::SaveVolume(vol, outputFile);
         } else {
             printf("Output format %s not supported!\n", outputFormat.c_str());
         }
