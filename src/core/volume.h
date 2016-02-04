43c393ef4d605607a21b94beb3982bf57d3e5d9b
shadow_walker <shadowwalkersb@gmail.com>
2016-02-04 17:24:05 -0600

    volume

--- src/core/volume.h
+++ src/core/volume.h
@@ -1,93 +1,12 @@
 #ifndef CORE_SKELETON_MAKER_VOLUME_H
 #define CORE_SKELETON_MAKER_VOLUME_H
 
-#define MAX_SHEETS 100000
-#define MAX_QUEUELEN 5000000
-#define MAX_ERODE 1000
-
 #include "VolumeData.h"
-#include "ThinningTemplate.h"
-#include "GridQueue2.h"
-////#include <cstdio>
-////#include <cstdlib>
-////#include <cmath>
-#include "PriorityQueue.h"
-////#include <vector>
-#include <MathTools/Vector3D.h>
-#include <MathTools/MathLib.h>
-//
-////---- VolumeRenderer.h -----------
-////#include <iostream>
-//#include <string>
-////#include <GorgonGL.h>
-////#include <GraphMatch/VectorMath.h>
-#include "GlobalConstants.h"
-//#include <GraySkeletonCPP/GlobalDefinitions.h>
-//#include <GraySkeletonCPP/VolumeSkeletonizer.h>
-//#include <GraySkeletonCPP/VolumeFormatConverter.h>
-////#include "MeshRenderer.h"
-////#include <ProteinMorph/NonManifoldMesh.h>
-////#include <ProteinMorph/TriangleMesh.h>
-////#include <MathTools/Vector3D.h>
-//#include <MathTools/MathLib.h>
-////#include <Foundation/Octree.h>
-////#include <queue>
-//
-//using namespace GraySkeletonCPP;
-////using namespace Protein_Morph;
-////using namespace MathTools;
-////using namespace GraphMatch;
-//
-////---- VolumeRenderer.h -----------
-//
-////using namespace std;
+
+//using namespace std;
 using namespace MathTools;
-using namespace Visualization;
 
-namespace SkeletonMaker {
-
-    const int neighbor6[6][3]={{0,0,1},{0,0,-1},{0,1,0},{0,-1,0},{1,0,0},{-1,0,0}} ;
-    const int neighbor4[4][2]={{0,1},{0,-1},{1,0},{-1,0}} ;
-    const int neighbor64[6][4][3] = {
-        {{0,1,0},{0,-1,0},{1,0,0},{-1,0,0}},
-        {{0,1,0},{0,-1,0},{1,0,0},{-1,0,0}},
-        {{0,0,1},{0,0,-1},{1,0,0},{-1,0,0}},
-        {{0,0,1},{0,0,-1},{1,0,0},{-1,0,0}},
-        {{0,0,1},{0,0,-1},{0,1,0},{0,-1,0}},
-        {{0,0,1},{0,0,-1},{0,1,0},{0,-1,0}}} ;
-
-    const int sheetNeighbor[12][4][3] = {
-        {{0,-1,-1},{0,-1,0},{0,0,-1},{0,0,0}},
-        {{0,-1,0},{0,-1,1},{0,0,0},{0,0,1}},
-        {{0,0,-1},{0,0,0},{0,1,-1},{0,1,0}},
-        {{0,0,0},{0,0,1},{0,1,0},{0,1,1}},
-
-        {{-1,0,-1},{-1,0,0},{0,0,-1},{0,0,0}},
-        {{-1,0,0},{-1,0,1},{0,0,0},{0,0,1}},
-        {{0,0,-1},{0,0,0},{1,0,-1},{1,0,0}},
-        {{0,0,0},{0,0,1},{1,0,0},{1,0,1}},
-
-        {{-1,-1,0},{-1,0,0},{0,-1,0},{0,0,0}},
-        {{-1,0,0},{-1,1,0},{0,0,0},{0,1,0}},
-        {{0,-1,0},{0,0,0},{1,-1,0},{1,0,0}},
-        {{0,0,0},{0,1,0},{1,0,0},{1,1,0}}
-        };
-
-    const int faceCells[12][2]={{0,4},{1,5},{2,6},{3,7},{0,2},{1,3},{4,6},{5,7},{0,1},{2,3},{4,5},{6,7}};
-
-    const int cubeFaces[6][4] =
-    { {1,5,7,3},{0,2,6,4},{2,3,7,6},{0,4,5,1},{5,4,6,7},{0,1,3,2}};
-
-    const int faceEdges[12][2] = {{3,1},{3,0},{2,1},{2,0},
-    {5,1},{5,0},{4,1},{4,0},
-    {5,3},{5,2},{4,3},{4,2}};
-
-    const int edgeFaces[6][4] = {{1,3,5,7},{0,2,4,6},{2,3,9,11},{0,1,8,10},{6,7,10,11},{4,5,8,9}} ;
-
-    struct gridPoint
-    {
-        int x, y, z;
-    };
+namespace Core {
 
     class Volume : public VolumeData {
     public:
