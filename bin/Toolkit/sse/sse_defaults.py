0be48443020636b7e247bb5dc36662bfcc7c488a
shadow_walker <shadowwalkersb@gmail.com>
2016-02-03 21:14:56 -0600

    rename

--- bin/Toolkit/sse/sse_defaults.py
+++ bin/Toolkit/sse/sse_defaults.py
@@ -18,25 +18,25 @@ class ComboBox(object):
 class SSEDefaults(object):
 
     def __init__(self):
-        self.doubleSpinBoxHelixImportance           = 1.0
-        self.doubleSpinBoxHelixMissingPenalty       = 5.0
-        self.doubleSpinBoxHelixMissingPenaltyScaled = 0.0
-        self.doubleSpinBoxEndHelixMissingPenalty    = 8.0
-        self.doubleSpinBoxSheetImportance           = 1.0
-        self.doubleSpinBoxSheetMissingPenalty       = 5.0
-        self.doubleSpinBoxSheetMissingPenaltyScaled = 0.0
-        self.spinBoxBorderMarginThreshold           = 3
-        self.doubleSpinBoxEuclideanDistance         = 0.0
-        self.spinBoxMinSheetSize                    = 10
-        self.doubleSpinBoxMaxSheetDistance          = 5.0
-        self.doubleSpinBoxSheetSelfLoopLength       = 5.0
-        self.doubleSpinBoxSheetMergeThreshold       = 3.0
-        self.doubleSpinBoxEuclideanToPDBRatio       = 10.0
-        self.radioButtonAbsoluteDifference          = True
-        self.doubleSpinBoxLoopImportance            = 0.2
-        self.doubleSpinBoxEuclideanLoopUsedPenalty  = 5.0
+        self.HelixImportance           = 1.0
+        self.HelixMissingPenalty       = 5.0
+        self.HelixMissingPenaltyScaled = 0.0
+        self.EndHelixMissingPenalty    = 8.0
+        self.SheetImportance           = 1.0
+        self.SheetMissingPenalty       = 5.0
+        self.SheetMissingPenaltyScaled = 0.0
+        self.BorderMarginThreshold           = 3
+        self.EuclideanDistance         = 0.0
+        self.MinSheetSize                    = 10
+        self.MaxSheetDistance          = 5.0
+        self.SheetSelfLoopLength       = 5.0
+        self.SheetMergeThreshold       = 3.0
+        self.EuclideanToPDBRatio       = 10.0
+        self.AbsoluteDifference          = True
+        self.LoopImportance            = 0.2
+        self.EuclideanLoopUsedPenalty  = 5.0
         
-        self.checkBoxMissingHelices = bool()
-        self.checkBoxMissingSheets = bool()
+        self.MissingHelices = bool()
+        self.MissingSheets = bool()
         
-        self.comboBoxCorrespondences = ComboBox()
+        self.correspondences = ComboBox()
