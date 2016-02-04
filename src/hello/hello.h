96105a217fb516eaa9ebfcaa87d1e5a205a1c1e2
shadow_walker <shadowwalkersb@gmail.com>
2016-02-04 14:35:56 -0600

    include guards: Hello

--- src/hello/hello.h
+++ src/hello/hello.h
@@ -1,9 +1,9 @@
-#ifndef SRC_CORE_HELLO_H_
-#define SRC_CORE_HELLO_H_
+#ifndef SRC_HELLO_HELLO_H_
+#define SRC_HELLO_HELLO_H_
 
 #include <string>
 
 std::string hello();
 std::string platform();
 
-#endif /* SRC_CORE_HELLO_H_ */
+#endif /* SRC_HELLO_HELLO_H_ */
