a08ba12c1899f0c026e2fe7936842f8bcf213be9
shadow_walker <shadowwalkersb@gmail.com>
2016-02-04 14:28:22 -0600

    rename install component: Toolkit -> Programs

--- src/cmake/Windows/install.cmake
+++ src/cmake/Windows/install.cmake
@@ -1,6 +1,6 @@
 if(WIN32)
     install_wrapper(FILES ${win_dlls}
             DESTINATIONS ${target_installation_locations}
-            COMPONENT Toolkit
+            COMPONENT Programs
             )
 endif()
