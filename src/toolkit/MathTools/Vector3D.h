cb197c80ac17db4e006269c0c1d1b50eb18d7ad2
shadow_walker <shadowwalkersb@gmail.com>
2016-02-09 22:52:12 -0600

    rename template

--- src/toolkit/MathTools/Vector3D.h
+++ src/toolkit/MathTools/Vector3D.h
@@ -8,13 +8,13 @@
 
 namespace MathTools {
     template <class T> class
-    Vector3DTemplate {
+    Vector3D {
     public:
-        Vector3DTemplate();
-        Vector3DTemplate(T x, T y, T z);
-        Vector3DTemplate(const vector<T>& vec);
-        Vector3DTemplate(const Vector3DTemplate<T>& rhs);
-        ~Vector3DTemplate();
+        Vector3D();
+        Vector3D(T x, T y, T z);
+        Vector3D(const vector<T>& vec);
+        Vector3D(const Vector3D<T>& rhs);
+        ~Vector3D();
 
         T X() const;
         T Y() const;
@@ -22,29 +22,29 @@ namespace MathTools {
         T& operator[](int subscript);
         T operator[](int subscript) const;
         double Length();
-        T operator*(const Vector3DTemplate<T> &d ); // Dot Product
+        T operator*(const Vector3D<T> &d ); // Dot Product
         int XInt();
         int YInt();
         int ZInt();
 
-        bool operator!=(Vector3DTemplate<T> &d);
-        bool operator==(Vector3DTemplate<T> &d);
-        bool operator>(Vector3DTemplate<T> &d);
-        bool operator<(Vector3DTemplate<T> &d);
-        bool operator>=(Vector3DTemplate<T> &d);
-        bool operator<=(Vector3DTemplate<T> &d);
+        bool operator!=(Vector3D<T> &d);
+        bool operator==(Vector3D<T> &d);
+        bool operator>(Vector3D<T> &d);
+        bool operator<(Vector3D<T> &d);
+        bool operator>=(Vector3D<T> &d);
+        bool operator<=(Vector3D<T> &d);
 
-        Vector3DTemplate<T> operator+(const Vector3DTemplate<T> &d);
-        Vector3DTemplate<T> operator-();
-        Vector3DTemplate<T> operator-(const Vector3DTemplate<T> &d );
-        Vector3DTemplate<T> operator^(const Vector3DTemplate<T> &d );		 // Cross Product
-        Vector3DTemplate<T> operator*(double s);
-        Vector3DTemplate<T>& operator=(const Vector3DTemplate<T>& d);
-        Vector3DTemplate<T>& operator+=(const Vector3DTemplate<T>& d);
-        Vector3DTemplate<T>& operator-=(const Vector3DTemplate<T>& d);
-        Vector3DTemplate<T> GetOrthogonal();
-        Vector3DTemplate<T> Rotate(Vector3DTemplate<T> axis, double angle);
-        Vector3DTemplate<T> Transform(Matrix<T> transformation);
+        Vector3D<T> operator+(const Vector3D<T> &d);
+        Vector3D<T> operator-();
+        Vector3D<T> operator-(const Vector3D<T> &d );
+        Vector3D<T> operator^(const Vector3D<T> &d );		 // Cross Product
+        Vector3D<T> operator*(double s);
+        Vector3D<T>& operator=(const Vector3D<T>& d);
+        Vector3D<T>& operator+=(const Vector3D<T>& d);
+        Vector3D<T>& operator-=(const Vector3D<T>& d);
+        Vector3D<T> GetOrthogonal();
+        Vector3D<T> Rotate(Vector3D<T> axis, double angle);
+        Vector3D<T> Transform(Matrix<T> transformation);
         T * begin();
         T * end();
 
@@ -52,104 +52,104 @@ namespace MathTools {
         bool IsBadNormal();
         void Print();
 
-        static Vector3DTemplate<T> Normalize(Vector3DTemplate<T> d);
-        static Vector3DTemplate<T> Project3Dto2D(Vector3DTemplate<T> point, Vector3DTemplate<T> planePt, Vector3DTemplate<T> planeVec1, Vector3DTemplate<T> planeVec2);
+        static Vector3D<T> Normalize(Vector3D<T> d);
+        static Vector3D<T> Project3Dto2D(Vector3D<T> point, Vector3D<T> planePt, Vector3D<T> planeVec1, Vector3D<T> planeVec2);
 
         T values[3];
     };
 
-    typedef Vector3DTemplate<int>    Vector3DInt;
-    typedef Vector3DTemplate<float>  Vector3DFloat;
-    typedef Vector3DTemplate<double> Vector3DDouble;
+    typedef Vector3D<int>    Vector3DInt;
+    typedef Vector3D<float>  Vector3DFloat;
+    typedef Vector3D<double> Vector3DDouble;
 
     template <class T>
-    Vector3DTemplate<T>::Vector3DTemplate() {
+    Vector3D<T>::Vector3D() {
         values[0] = (T)0.0;
         values[1] = (T)0.0;
         values[2] = (T)0.0;
     }
 
     template <class T>
-    Vector3DTemplate<T>::Vector3DTemplate(T x, T y, T z) {
+    Vector3D<T>::Vector3D(T x, T y, T z) {
         values[0] = x;
         values[1] = y;
         values[2] = z;
     }
 
     template <class T>
-    Vector3DTemplate<T>::Vector3DTemplate(const vector<T>& vec) {
+    Vector3D<T>::Vector3D(const vector<T>& vec) {
         values[0] = vec[0];
         values[1] = vec[1];
         values[2] = vec[2];
     }
 
     template <class T>
-    Vector3DTemplate<T>::Vector3DTemplate(const Vector3DTemplate<T>& rhs) {
+    Vector3D<T>::Vector3D(const Vector3D<T>& rhs) {
         values[0] = rhs.values[0];
         values[1] = rhs.values[1];
         values[2] = rhs.values[2];
     }
 
     template <class T>
-    Vector3DTemplate<T>::~Vector3DTemplate() {
+    Vector3D<T>::~Vector3D() {
     }
 
 
     template <class T>
-    bool Vector3DTemplate<T>::operator!=(Vector3DTemplate<T> &d) {
+    bool Vector3D<T>::operator!=(Vector3D<T> &d) {
         return (X() != d.X()) || (Y() != d.Y()) || (Z() != d.Z());
     }
 
     template <class T>
-    bool Vector3DTemplate<T>::operator==(Vector3DTemplate<T> &d) {
+    bool Vector3D<T>::operator==(Vector3D<T> &d) {
         return (X() == d.X()) && (Y() == d.Y()) && (Z() == d.Z());
     }
 
     template <class T>
-    bool Vector3DTemplate<T>::operator>(Vector3DTemplate<T> &d) {
+    bool Vector3D<T>::operator>(Vector3D<T> &d) {
         return (X() > d.X()) ||
             ((X() == d.X()) && (Y() > d.Y())) ||
             ((X() == d.X()) && (Y() == d.Y()) && (Z() > d.Y()));
     }
 
     template <class T>
-    bool Vector3DTemplate<T>::operator<(Vector3DTemplate<T> &d) {
+    bool Vector3D<T>::operator<(Vector3D<T> &d) {
         return d > *this;
     }
 
     template <class T>
-    bool Vector3DTemplate<T>::operator>=(Vector3DTemplate<T> &d) {
+    bool Vector3D<T>::operator>=(Vector3D<T> &d) {
         return (*this > d) || (*this == d);
     }
 
     template <class T>
-    bool Vector3DTemplate<T>::operator<=(Vector3DTemplate<T> &d) {
+    bool Vector3D<T>::operator<=(Vector3D<T> &d) {
         return (*this < d) || (*this == d);
     }
 
     template <class T>
-    T Vector3DTemplate<T>::X() const {
+    T Vector3D<T>::X() const {
         return values[0];
     }
 
     template <class T>
-    T Vector3DTemplate<T>::Y() const {
+    T Vector3D<T>::Y() const {
         return values[1];
     }
 
     template <class T>
-    T Vector3DTemplate<T>::Z() const {
+    T Vector3D<T>::Z() const {
         return values[2];
     }
     template <class T>
-    T& Vector3DTemplate<T>::operator[](int subscript) {
+    T& Vector3D<T>::operator[](int subscript) {
         if (subscript < 0 || subscript > 2)
             throw "Vector3D [] operator BoundsError!\n";
         else
             return values[subscript];
     }
     template <class T>
-    T Vector3DTemplate<T>::operator[](int subscript) const {
+    T Vector3D<T>::operator[](int subscript) const {
         if (subscript < 0 || subscript > 2)
             throw "Vector3D [] operator BoundsError!\n";
         else
@@ -157,60 +157,60 @@ namespace MathTools {
     }
 
     template <class T>
-    double Vector3DTemplate<T>::Length() {
+    double Vector3D<T>::Length() {
         return (double)sqrt((double)(values[0] * values[0] + values[1] * values[1] + values[2] * values[2]));
     }
 
     template <class T>
-    int Vector3DTemplate<T>::XInt() {
+    int Vector3D<T>::XInt() {
         return (int)round(values[0]);
     }
 
     template <class T>
-    int Vector3DTemplate<T>::YInt() {
+    int Vector3D<T>::YInt() {
         return (int)round(values[1]);
     }
 
     template <class T>
-    int Vector3DTemplate<T>::ZInt() {
+    int Vector3D<T>::ZInt() {
         return (int)round(values[2]);
     }
 
     template <class T>
-    T Vector3DTemplate<T>::operator*(const Vector3DTemplate<T> &d ) {		// Dot Product
+    T Vector3D<T>::operator*(const Vector3D<T> &d ) {		// Dot Product
         return X() * d.X() + Y() * d.Y() + Z() * d.Z();
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::operator+(const Vector3DTemplate<T> &d) {
-        return Vector3DTemplate<T>(X() + d.X(), Y() + d.Y(), Z() + d.Z());
+    Vector3D<T> Vector3D<T>::operator+(const Vector3D<T> &d) {
+        return Vector3D<T>(X() + d.X(), Y() + d.Y(), Z() + d.Z());
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::operator-() {
-        return Vector3DTemplate<T>(-X(), -Y(), -Z());
+    Vector3D<T> Vector3D<T>::operator-() {
+        return Vector3D<T>(-X(), -Y(), -Z());
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::operator-(const Vector3DTemplate<T> &d ) {
-        return Vector3DTemplate<T>(X() - d.X(), Y() - d.Y(), Z() - d.Z());
+    Vector3D<T> Vector3D<T>::operator-(const Vector3D<T> &d ) {
+        return Vector3D<T>(X() - d.X(), Y() - d.Y(), Z() - d.Z());
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::operator^(const Vector3DTemplate<T> &d ) { // Cross Product
-        return Vector3DTemplate<T>(
+    Vector3D<T> Vector3D<T>::operator^(const Vector3D<T> &d ) { // Cross Product
+        return Vector3D<T>(
             values[1] * d.values[2] - values[2] * d.values[1],
             values[2] * d.values[0] - values[0] * d.values[2],
             values[0] * d.values[1] - values[1] * d.values[0]);
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::operator*(double s) {
-        return Vector3DTemplate<T>((T)(X()*s), (T)(Y()*s), (T)(Z()*s));
+    Vector3D<T> Vector3D<T>::operator*(double s) {
+        return Vector3D<T>((T)(X()*s), (T)(Y()*s), (T)(Z()*s));
     }
 
     template <class T>
-    Vector3DTemplate<T>& Vector3DTemplate<T>::operator+=(const Vector3DTemplate<T>& d) {
+    Vector3D<T>& Vector3D<T>::operator+=(const Vector3D<T>& d) {
         for(int i = 0; i < 3; i++) {
             values[i] += d.values[i];
         }
@@ -218,14 +218,14 @@ namespace MathTools {
     }
 
     template <class T>
-    Vector3DTemplate<T>& Vector3DTemplate<T>::operator-=(const Vector3DTemplate<T>& d) {
+    Vector3D<T>& Vector3D<T>::operator-=(const Vector3D<T>& d) {
         for(int i = 0; i < 3; i++) {
             values[i] -= d.values[i];
         }
         return *this;
     }
     template <class T>
-    Vector3DTemplate<T>& Vector3DTemplate<T>::operator=(const Vector3DTemplate<T>& d) {
+    Vector3D<T>& Vector3D<T>::operator=(const Vector3D<T>& d) {
         for(int i = 0; i < 3; i++) {
             values[i] = d.values[i];
         }
@@ -238,7 +238,7 @@ namespace MathTools {
      * @return the iterator (here is the pointer) of the first element
      * */
     template <class T>
-    T * Vector3DTemplate<T>::begin() {
+    T * Vector3D<T>::begin() {
         return &values[0];
     }
 
@@ -248,12 +248,12 @@ namespace MathTools {
      * @return the iterator (here is the pointer) of the one beyond the last element.
      * */
     template <class T>
-    T * Vector3DTemplate<T>::end() {
+    T * Vector3D<T>::end() {
         return &values[2];
     }
 
     template <class T>
-    void Vector3DTemplate<T>::Normalize() {
+    void Vector3D<T>::Normalize() {
         double base = Length();
         if(base == 0) {
             values[0] = 0;
@@ -267,25 +267,25 @@ namespace MathTools {
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::Normalize(Vector3DTemplate<T> d) {
-        Vector3DTemplate<T> ret = d;
+    Vector3D<T> Vector3D<T>::Normalize(Vector3D<T> d) {
+        Vector3D<T> ret = d;
         ret.Normalize();
         return ret;
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::GetOrthogonal() {
-        Vector3DTemplate<T> orthVec = Vector3DTemplate<T>(1, 1, 1);
-        orthVec = Vector3DTemplate<T>(this->X(), this->Y(), this->Z()) ^ orthVec;
+    Vector3D<T> Vector3D<T>::GetOrthogonal() {
+        Vector3D<T> orthVec = Vector3D<T>(1, 1, 1);
+        orthVec = Vector3D<T>(this->X(), this->Y(), this->Z()) ^ orthVec;
         if(isZero(orthVec.Length())) {
-            orthVec = Vector3DTemplate<T>(1, -1, -1);
-            orthVec = Vector3DTemplate<T>(this->X(), this->Y(), this->Z()) ^ orthVec;
+            orthVec = Vector3D<T>(1, -1, -1);
+            orthVec = Vector3D<T>(this->X(), this->Y(), this->Z()) ^ orthVec;
         }
         return orthVec;
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::Rotate(Vector3DTemplate<T> axis, double angle) {
+    Vector3D<T> Vector3D<T>::Rotate(Vector3D<T> axis, double angle) {
         double r = angle;
         T a = axis.values[0];
         T b = axis.values[1];
@@ -302,7 +302,7 @@ namespace MathTools {
                           {2*(q3*q1 - q0*q2),					2*(q3*q2 + q0*q1),					q0*q0 - q1*q1 - q2*q2 + q3*q3}};
 
 
-        Vector3DTemplate<T> v = Vector3DTemplate(0.0, 0.0, 0.0);
+        Vector3D<T> v = Vector3D(0.0, 0.0, 0.0);
         for(int x = 0; x < 3; x++) {
             for(int y = 0; y < 3; y++) {
                 v.values[x] = v.values[x] + R[y][x] * values[y];
@@ -312,25 +312,25 @@ namespace MathTools {
     }
 
     template <class T>
-    bool Vector3DTemplate<T>::IsBadNormal() {
+    bool Vector3D<T>::IsBadNormal() {
         return !isZero(Length() - 1.0, 0.00001);
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::Project3Dto2D(Vector3DTemplate<T> point, Vector3DTemplate<T> planePt, Vector3DTemplate<T> planeVec1, Vector3DTemplate<T> planeVec2) {
-        Vector3DTemplate<T> ray = point - planePt;
-        return Vector3DTemplate<T>(ray * planeVec1, ray * planeVec2, 0);
+    Vector3D<T> Vector3D<T>::Project3Dto2D(Vector3D<T> point, Vector3D<T> planePt, Vector3D<T> planeVec1, Vector3D<T> planeVec2) {
+        Vector3D<T> ray = point - planePt;
+        return Vector3D<T>(ray * planeVec1, ray * planeVec2, 0);
         //return Vector3DTemplate<T>(point * planeVec1, point * planeVec2, 0);
 
     }
 
     template <class T>
-    void Vector3DTemplate<T>::Print() {
+    void Vector3D<T>::Print() {
         printf("{%f, %f, %f} \n", X(), Y(), Z());
     }
 
     template <class T>
-    Vector3DTemplate<T> Vector3DTemplate<T>::Transform(Matrix<T> transformation) {
+    Vector3D<T> Vector3D<T>::Transform(Matrix<T> transformation) {
         Matrix<T> p = Matrix<T>(4, 1);
         p.SetValue(values[0], 0, 0);
         p.SetValue(values[1], 1, 0);
@@ -338,7 +338,7 @@ namespace MathTools {
         p.SetValue((T)1, 3, 0);
 
         p = transformation * p;
-        return Vector3DTemplate<T>(p.GetValue(0,0), p.GetValue(1,0), p.GetValue(2,0));
+        return Vector3D<T>(p.GetValue(0,0), p.GetValue(1,0), p.GetValue(2,0));
     }
 }
 
