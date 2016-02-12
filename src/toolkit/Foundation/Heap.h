67d573eb73832a7bb14df86735c2456e07d3142e
shadow_walker <shadowwalkersb@gmail.com>
2016-02-12 10:28:46 -0600

    Revert "IsEmpty"
    This reverts commit 92e6aa33dc2c0aed0bfc9c72ab1bc8703359b994.

--- src/toolkit/Foundation/Heap.h
+++ src/toolkit/Foundation/Heap.h
@@ -14,6 +14,7 @@ namespace Foundation {
 
         void AddValue(T value);
         T PopRoot();
+        bool IsEmpty();
 
     private:
         unsigned int GetParent(unsigned int index);
@@ -96,6 +97,11 @@ namespace Foundation {
         }
     }
 
+
+    template <class T>
+    bool Heap<T>::IsEmpty() {
+        return (values.size() == 0);
+    }
 
     template <class T>
     T Heap<T>::PopRoot() {
