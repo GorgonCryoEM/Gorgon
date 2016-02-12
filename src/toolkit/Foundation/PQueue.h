a7eb83b0954f1e04539e8d22b13017d0939f0480
shadow_walker <shadowwalkersb@gmail.com>
2016-02-12 10:10:21 -0600

    merge PQueue and its element

--- src/toolkit/Foundation/PQueue.h
+++ src/toolkit/Foundation/PQueue.h
@@ -3,9 +3,73 @@
 
 
 #include "Heap.h"
-#include "PQueueElem.h"
 
 namespace Foundation {
+    template <class TKey, class TValue>
+    class PQueueElem {
+    public:
+        PQueueElem(TKey key, TValue value);
+        bool operator==(const PQueueElem<TKey, TValue> &other) const;
+        bool operator!=(const PQueueElem<TKey, TValue> &other) const;
+        bool operator>(const PQueueElem<TKey, TValue> &other) const;
+        bool operator<(const PQueueElem<TKey, TValue> &other) const;
+        bool operator>=(const PQueueElem<TKey, TValue> &other) const;
+        bool operator<=(const PQueueElem<TKey, TValue> &other) const;
+
+        TValue GetValue();
+        TKey GetKey();
+    private:
+        TKey key;
+        TValue value;
+    };
+
+    template <class TKey, class TValue>
+    PQueueElem<TKey, TValue>::PQueueElem(TKey key, TValue value) {
+        this->key = key;
+        this->value = value;
+    }
+
+    template <class TKey, class TValue>
+    inline bool PQueueElem<TKey, TValue>::operator==(const PQueueElem<TKey, TValue> &other) const {
+        return (this->key == other.key);
+    }
+
+    template <class TKey, class TValue>
+    inline bool PQueueElem<TKey, TValue>::operator!=(const PQueueElem<TKey, TValue> &other) const {
+        return (this->key != other.key);
+    }
+
+    template <class TKey, class TValue>
+    inline bool PQueueElem<TKey, TValue>::operator>(const PQueueElem<TKey, TValue> &other) const {
+        return (this->key > other.key);
+    }
+
+    template <class TKey, class TValue>
+    inline bool PQueueElem<TKey, TValue>::operator<(const PQueueElem<TKey, TValue> &other) const {
+        return (this->key < other.key);
+    }
+
+    template <class TKey, class TValue>
+    inline bool PQueueElem<TKey, TValue>::operator>=(const PQueueElem<TKey, TValue> &other) const {
+        return (this->key >= other.key);
+    }
+
+    template <class TKey, class TValue>
+    inline bool PQueueElem<TKey, TValue>::operator<=(const PQueueElem<TKey, TValue> &other) const {
+        return (this->key <= other.key);
+    }
+
+    template <class TKey, class TValue>
+    inline TValue PQueueElem<TKey, TValue>::GetValue() {
+        return value;
+    }
+
+    template <class TKey, class TValue>
+    inline TKey PQueueElem<TKey, TValue>::GetKey() {
+        return key;
+    }
+
+
     template <class TKey, class TValue>
     class PQueue {
     public:
