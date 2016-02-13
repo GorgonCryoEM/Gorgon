70a9216e68c91a74499c11f19e91261c5e6d8149
shadow_walker <shadowwalkersb@gmail.com>
2016-02-12 22:49:05 -0600

    refactor completely

--- src/toolkit/Foundation/Queue.h
+++ src/toolkit/Foundation/Queue.h
@@ -7,7 +7,6 @@ namespace SkeletonMaker {
 
     struct QueueNode {
             int x, y, z;
-            QueueNode* next;
 
             QueueNode(int xx, int yy, int zz)
                     : x(xx), y(yy), z(zz)
@@ -19,7 +18,6 @@ namespace SkeletonMaker {
     class Queue {
         public:
             Queue();
-            ~Queue();
             QueueNode* getNext();
             void reset();
             int getNumElements();
@@ -27,42 +25,16 @@ namespace SkeletonMaker {
             QueueNode * remove();
 
         private:
-            QueueNode* head;
-            QueueNode* pre;
-            QueueNode* cur;
-            int numEles;
-
             typedef list<QueueNode> Cont;
             Cont q;
             Cont::iterator it;
     };
 
     Queue::Queue() {
-        head = NULL;
-        cur = NULL;
-        pre = NULL;
-        numEles = 0;
         it=q.end();
     }
 
-    Queue::~Queue() {
-        QueueNode* ele;
-        reset();
-        ele = getNext();
-        while( (ele = remove()) != NULL) {
-        };
-    }
-
     QueueNode* Queue::getNext() {
-        if(cur == NULL) {
-            pre = NULL;
-            cur = head;
-        }
-        else {
-            pre = cur;
-            cur = cur->next;
-        }
-
         if(it==q.end()){
             it=q.begin();
             if(it==q.end())
@@ -75,50 +47,23 @@ namespace SkeletonMaker {
         }
 
         return &(*it);
-//        return cur;
     }
 
     void Queue::reset() {
-        pre = NULL;
-        cur = NULL;
         it = q.end();
     }
 
     int Queue::getNumElements() {
         return q.size();
-//        return numEles;
     }
 
     void Queue::prepend(int xx, int yy, int zz) {
-        QueueNode* ele = new QueueNode;
-        ele->x = xx;
-        ele->y = yy;
-        ele->z = zz;
-        ele->next = head;
-        head = ele;
-        numEles++;
-
         q.push_front(QueueNode(xx, yy, zz));
-
         reset();
     }
 
     /* Remove current element pointed by cur */
     QueueNode * Queue::remove() {
-        QueueNode* temp = cur;
-        if(cur != NULL) {
-            cur = cur->next;
-            delete temp;
-
-            if(pre != NULL) {
-                pre->next = cur;
-            }
-            else {
-                head = cur;
-            }
-            numEles--;
-        }
-
         if(it != q.end()){
             it = q.erase(it);
             if(it==q.end())
@@ -128,7 +73,6 @@ namespace SkeletonMaker {
         }
         else
             return NULL;
-//        return cur;
     }
 }
 
