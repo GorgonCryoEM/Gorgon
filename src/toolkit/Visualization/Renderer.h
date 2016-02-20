8a28e6512a24c91269a1a12a254ada9c6c7b9e52
shadow_walker <shadowwalkersb@gmail.com>
2016-02-19 22:33:31 -0600

    GetMin and GetMax

--- src/toolkit/Visualization/Renderer.h
+++ src/toolkit/Visualization/Renderer.h
@@ -44,8 +44,8 @@ namespace Visualization {
         virtual void SetObjectSpecificColoring(bool objectSpecific);
         virtual void UpdateBoundingBox();
 
-        float GetMin(int dimension);
-        float GetMax(int dimension);
+        float getMin(int dimension);
+        float getMax(int dimension);
 
     protected:
         Dim3D<float> minPts;
@@ -65,11 +65,11 @@ namespace Visualization {
     Renderer::~Renderer() {
     }
 
-    float Renderer::GetMin(int dimension) {
+    float Renderer::getMin(int dimension) {
         return minPts[dimension];
     }
 
-    float Renderer::GetMax(int dimension) {
+    float Renderer::getMax(int dimension) {
         return maxPts[dimension];
     }
 
