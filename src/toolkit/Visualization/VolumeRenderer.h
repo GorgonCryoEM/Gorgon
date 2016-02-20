b3919fa14dc34af7cbdbd1fb77186805a9e3f69e
shadow_walker <shadowwalkersb@gmail.com>
2016-02-19 23:25:52 -0600

    fix volData loading from file

--- src/toolkit/Visualization/VolumeRenderer.h
+++ src/toolkit/Visualization/VolumeRenderer.h
@@ -127,7 +127,6 @@ namespace Visualization {
         useDisplayRadius = false;
         viewingType = VIEWING_TYPE_ISO_SURFACE;
         surfaceMesh = new VolumeSurfaceMeshType();
-        volData = NULL;
         octree = NULL;
         surfaceValue = 1.5;
         displayRadius = 1;
@@ -142,9 +141,6 @@ namespace Visualization {
             textureLoaded = false;
         }
         delete surfaceMesh;
-        if(volData != NULL) {
-            delete volData;
-        }
         if(octree != NULL) {
             delete octree;
         }
@@ -559,10 +555,7 @@ namespace Visualization {
     }
 
     void VolumeRenderer::LoadFile(string fileName) {
-        if(volData != NULL) {
-            delete volData;
-        }
-        volData = MRCReaderPicker::pick(fileName.c_str())->getVolume();
+        Volume::loadFile(fileName);
         InitializeOctree();
         UpdateBoundingBox();
 
