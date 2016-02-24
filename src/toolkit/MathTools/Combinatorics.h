e688816e9dbfad45b64fedcd4441369ce8151720
shadow_walker <shadowwalkersb@gmail.com>
2016-02-24 11:26:56 -0600

    shorten vars

--- src/toolkit/MathTools/Combinatorics.h
+++ src/toolkit/MathTools/Combinatorics.h
@@ -10,11 +10,11 @@ namespace MathTools {
     static unsigned long long Combinations(int n, int r);
     static unsigned long long Permutations(int n, int r);
 
-    static void GetBinomialDistribution(ProbabilityDistribution1D & distributionInfo);
-    static void GetBinomialDistribution(ProbabilityDistribution2D & distributionInfo);
-    static void GetBinomialDistribution(ProbabilityDistribution3D & distributionInfo);
-    static void GetUniformDistribution(ProbabilityDistribution3D & distributionInfo);
-    static void GetAnisotropicDistributionAxisAligned(ProbabilityDistribution3D & distributionInfo, int xRadius, int yRadius, int zRadius);
+    static void GetBinomialDistribution(ProbabilityDistribution1D & distro);
+    static void GetBinomialDistribution(ProbabilityDistribution2D & distro);
+    static void GetBinomialDistribution(ProbabilityDistribution3D & distro);
+    static void GetUniformDistribution(ProbabilityDistribution3D & distro);
+    static void GetAnisotropicDistributionAxisAligned(ProbabilityDistribution3D & distro, int xR, int yR, int zR);
 
     unsigned long long Combinations(int n, int r) {
         long long c = 1;
@@ -35,76 +35,76 @@ namespace MathTools {
         return p;
     }
 
-    void GetBinomialDistribution(ProbabilityDistribution1D & distributionInfo) {
+    void GetBinomialDistribution(ProbabilityDistribution1D & distro) {
         double total = 0;
 
-        for(int x = -distributionInfo.radius, i=0; x <= distributionInfo.radius; x++, i++) {
-            distributionInfo.values[i] = (float)Combinations(2 * distributionInfo.radius, x+distributionInfo.radius);
-            total += distributionInfo.values[i];
+        for(int x = -distro.radius, i=0; x <= distro.radius; x++, i++) {
+            distro.values[i] = (float)Combinations(2 * distro.radius, x+distro.radius);
+            total += distro.values[i];
         }
 
-        for(int x = -distributionInfo.radius, i=0; x <= distributionInfo.radius; x++, i++) {
-            distributionInfo.values[i] = (float)(distributionInfo.values[i] / total);
+        for(int x = -distro.radius, i=0; x <= distro.radius; x++, i++) {
+            distro.values[i] = (float)(distro.values[i] / total);
         }
     }
 
-    void GetBinomialDistribution(ProbabilityDistribution2D & distributionInfo) {
+    void GetBinomialDistribution(ProbabilityDistribution2D & distro) {
         ProbabilityDistribution1D dist1D;
-        dist1D.radius = distributionInfo.radius;
+        dist1D.radius = distro.radius;
         GetBinomialDistribution(dist1D);
         for(int x = 0; x < dist1D.radius * 2 +1; x++) {
             for(int y = 0; y < dist1D.radius * 2 +1; y++) {
-                distributionInfo.values[x][y] = dist1D.values[x] * dist1D.values[y];
+                distro.values[x][y] = dist1D.values[x] * dist1D.values[y];
             }
         }
     }
 
-    void GetBinomialDistribution(ProbabilityDistribution3D & distributionInfo) {
+    void GetBinomialDistribution(ProbabilityDistribution3D & distro) {
         ProbabilityDistribution1D dist1D;
-        dist1D.radius = distributionInfo.radius;
+        dist1D.radius = distro.radius;
         GetBinomialDistribution(dist1D);
         for(int x = 0; x < dist1D.radius * 2 +1; x++) {
             for(int y = 0; y < dist1D.radius * 2 +1; y++) {
                 for(int z = 0; z < dist1D.radius * 2 +1; z++) {
-                    distributionInfo.values[x][y][z] = dist1D.values[x] * dist1D.values[y] * dist1D.values[z];
+                    distro.values[x][y][z] = dist1D.values[x] * dist1D.values[y] * dist1D.values[z];
                 }
             }
         }
     }
 
-    void GetUniformDistribution(ProbabilityDistribution3D & distributionInfo) {
-        double probability = 1.0 / (double)(distributionInfo.radius * 2 +1) * (distributionInfo.radius * 2 +1) * (distributionInfo.radius * 2 +1);
-        for(int x = 0; x < distributionInfo.radius * 2 +1; x++) {
-            for(int y = 0; y < distributionInfo.radius * 2 +1; y++) {
-                for(int z = 0; z < distributionInfo.radius * 2 +1; z++) {
-                    distributionInfo.values[x][y][z] = (float)probability;
+    void GetUniformDistribution(ProbabilityDistribution3D & distro) {
+        double probability = 1.0 / (double)(distro.radius * 2 +1) * (distro.radius * 2 +1) * (distro.radius * 2 +1);
+        for(int x = 0; x < distro.radius * 2 +1; x++) {
+            for(int y = 0; y < distro.radius * 2 +1; y++) {
+                for(int z = 0; z < distro.radius * 2 +1; z++) {
+                    distro.values[x][y][z] = (float)probability;
                 }
             }
         }
     }
 
-    void GetAnisotropicDistributionAxisAligned(ProbabilityDistribution3D & distributionInfo, int xRadius, int yRadius, int zRadius) {
-        distributionInfo.radius = max (xRadius, max(yRadius, zRadius));
+    void GetAnisotropicDistributionAxisAligned(ProbabilityDistribution3D & distro, int xR, int yR, int zR) {
+        distro.radius = max (xR, max(yR, zR));
         ProbabilityDistribution1D xBinomialDist, yBinomialDist, zBinomialDist;
 
-        xBinomialDist.radius = xRadius;
+        xBinomialDist.radius = xR;
         GetBinomialDistribution(xBinomialDist);
-        yBinomialDist.radius = yRadius;
+        yBinomialDist.radius = yR;
         GetBinomialDistribution(yBinomialDist);
-        zBinomialDist.radius = zRadius;
+        zBinomialDist.radius = zR;
         GetBinomialDistribution(zBinomialDist);
 
-        for(int x = 0; x < distributionInfo.radius * 2 +1; x++) {
-            for(int y = 0; y < distributionInfo.radius * 2 +1; y++) {
-                for(int z = 0; z < distributionInfo.radius * 2 +1; z++) {
-                    if((x < distributionInfo.radius-xRadius) || (x > distributionInfo.radius+xRadius) ||
-                        (y < distributionInfo.radius-yRadius) || (y > distributionInfo.radius+yRadius) ||
-                        (z < distributionInfo.radius-zRadius) || (z > distributionInfo.radius+zRadius)) {
-                        distributionInfo.values[x][y][z] = 0;
+        for(int x = 0; x < distro.radius * 2 +1; x++) {
+            for(int y = 0; y < distro.radius * 2 +1; y++) {
+                for(int z = 0; z < distro.radius * 2 +1; z++) {
+                    if((x < distro.radius-xR) || (x > distro.radius+xR) ||
+                        (y < distro.radius-yR) || (y > distro.radius+yR) ||
+                        (z < distro.radius-zR) || (z > distro.radius+zR)) {
+                        distro.values[x][y][z] = 0;
                     } else {
-                        distributionInfo.values[x][y][z] = xBinomialDist.values[x - distributionInfo.radius + xRadius] *
-                            yBinomialDist.values[y - distributionInfo.radius + yRadius] *
-                            zBinomialDist.values[z - distributionInfo.radius + zRadius];
+                        distro.values[x][y][z] = xBinomialDist.values[x - distro.radius + xR] *
+                            yBinomialDist.values[y - distro.radius + yR] *
+                            zBinomialDist.values[z - distro.radius + zR];
                     }
                 }
             }
