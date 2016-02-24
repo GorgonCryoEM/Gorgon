9aec4be231cd735bc0f4618b4b1d28513a42f4db
shadow_walker <shadowwalkersb@gmail.com>
2016-02-24 11:51:28 -0600

    refactor: use Dim3D

--- src/toolkit/MathTools/Combinatorics.h
+++ src/toolkit/MathTools/Combinatorics.h
@@ -2,6 +2,7 @@
 #define TOOLKIT_MATHTOOLS_COMBINATORICS_H
 
 #include "DataStructures.h"
+#include "Dim3D.h"
 #include <algorithm>
 
 using namespace std;
@@ -85,26 +86,25 @@ namespace MathTools {
 
     void AnisoDistrAxisAligned(ProbDistr3D & distro, int xR, int yR, int zR) {
         distro.radius = max (xR, max(yR, zR));
-        ProbDistr1D xBinomialDist, yBinomialDist, zBinomialDist;
 
-        xBinomialDist.radius = xR;
-        BinomDistr(xBinomialDist);
-        yBinomialDist.radius = yR;
-        BinomDistr(yBinomialDist);
-        zBinomialDist.radius = zR;
-        BinomDistr(zBinomialDist);
+        Dim3D<int> R(xR, yR, zR);
+        Dim3D<ProbDistr1D> BinomialDist;
+
+        BinomialDist[0].radius = R[0];        BinomDistr(BinomialDist[0]);
+        BinomialDist[1].radius = R[1];        BinomDistr(BinomialDist[1]);
+        BinomialDist[2].radius = R[2];        BinomDistr(BinomialDist[2]);
 
         for(int x = 0; x < distro.radius * 2 +1; x++) {
             for(int y = 0; y < distro.radius * 2 +1; y++) {
                 for(int z = 0; z < distro.radius * 2 +1; z++) {
-                    if((x < distro.radius-xR) || (x > distro.radius+xR) ||
-                        (y < distro.radius-yR) || (y > distro.radius+yR) ||
-                        (z < distro.radius-zR) || (z > distro.radius+zR)) {
-                        distro.values[x][y][z] = 0;
+                    if((x < distro.radius-R.X()) || (x > distro.radius+R.X()) ||
+                       (y < distro.radius-R.Y()) || (y > distro.radius+R.Y()) ||
+                       (z < distro.radius-R.Z()) || (z > distro.radius+R.Z())) {
+                       distro.values[x][y][z] = 0;
                     } else {
-                        distro.values[x][y][z] = xBinomialDist.values[x - distro.radius + xR] *
-                            yBinomialDist.values[y - distro.radius + yR] *
-                            zBinomialDist.values[z - distro.radius + zR];
+                        distro.values[x][y][z] = BinomialDist[0].values[x - distro.radius + R.X()] *
+                                                 BinomialDist[1].values[y - distro.radius + R.Y()] *
+                                                 BinomialDist[2].values[z - distro.radius + R.Z()];
                     }
                 }
             }
