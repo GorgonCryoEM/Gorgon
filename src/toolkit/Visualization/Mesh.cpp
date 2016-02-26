86b4a561135471b7b7bc13783cfd6a8d314bde1f
shadow_walker <shadowwalkersb@gmail.com>
2016-02-26 12:46:55 -0600

    Mesh: file and class

---
+++ src/toolkit/Visualization/Mesh.cpp
@@ -0,0 +1,21 @@
+/*
+ * Mesh.cpp
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#include "Mesh.h"
+
+namespace Core {
+
+//    Mesh::Mesh() {
+//        // TODO Auto-generated constructor stub
+//
+//    }
+//
+//    Mesh::~Mesh() {
+//        // TODO Auto-generated destructor stub
+//    }
+
+} /* namespace Core */
---
+++ src/toolkit/Visualization/Mesh.h
@@ -0,0 +1,29 @@
+/*
+ * Mesh.h
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#ifndef SRC_TOOLKIT_VISUALIZATION_MESH_H_
+#define SRC_TOOLKIT_VISUALIZATION_MESH_H_
+
+namespace Core {
+
+    /*
+     *
+     */
+    class Mesh {
+        public:
+//            Mesh();
+//            virtual ~Mesh();
+            virtual int AddMarchingVertex(Vec3F location, int hashKey) =0;
+            virtual unsigned long long AddMarchingFace(unsigned long long vertexHash0,
+                                               unsigned long long vertexHash1,
+                                               unsigned long long vertexHash2) =0;
+
+    };
+
+} /* namespace Core */
+#endif /* SRC_TOOLKIT_VISUALIZATION_MESH_H_ */
+
