e4f1c4780a189f424001a4c72cab9ae5afc58234
shadow_walker <shadowwalkersb@gmail.com>
2016-02-29 00:10:54 -0600

    add members: IsoSurface, CrossSection, Solid

--- src/toolkit/Visualization/Display.h
+++ src/toolkit/Visualization/Display.h
@@ -9,11 +9,28 @@
 #define SRC_TOOLKIT_VISUALIZATION_DISPLAY_H_
 
 #include "DisplayBase.h"
+#include "IsoSurface.h"
+#include "CrossSection.h"
+#include "Solid.h"
 
 namespace Visualization {
 
     class Display : public DisplayBase {
+        public:
+            Display();
+
+        private:
+            vector<DisplayBase *> displays;
     };
+
+    Display::Display()
+        : DisplayBase()
+{
+        displays.push_back(new IsoSurface);
+        displays.push_back(new CrossSection);
+        displays.push_back(new Solid);
+}
+
 }
 
 
