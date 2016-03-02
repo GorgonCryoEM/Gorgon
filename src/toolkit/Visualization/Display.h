e0cbf57b5a94a98c9ae2def3c0c8e1df033d5654
shadow_walker <shadowwalkersb@gmail.com>
2016-03-01 21:06:37 -0600

    start from scratch

--- src/toolkit/Visualization/Display.h
+++ src/toolkit/Visualization/Display.h
@@ -8,25 +8,16 @@
 #ifndef SRC_TOOLKIT_VISUALIZATION_DISPLAY_H_
 #define SRC_TOOLKIT_VISUALIZATION_DISPLAY_H_
 
-#include "DisplayBase.h"
+#include "IsoSurface.h"
 
 namespace Visualization {
 
-    class Display : public DisplayBase {
+    class Display : public IsoSurface {
         public:
             Display();
 
-            bool calculateDisplay();
-            void setViewingType(const int type);
-
-            void draw(int subSceneIndex, bool selectEnabled);
-            void setSampleInterval(const int size);
-            void setSurfaceValue(const float value);
-            void setMaxSurfaceValue(const float value);
-            bool setCuttingPlane(float position, float vecX, float vecY, float vecZ);
         private:
             vector<DisplayBase *> displays;
-            DisplayBase * cur;
     };
 }
 
