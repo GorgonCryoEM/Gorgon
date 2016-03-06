9a7d73796453d7228d5fb9417fc1d4cfcfb798cc
shadow_walker <shadowwalkersb@gmail.com>
2016-03-06 16:14:44 -0600

    new class Vox

---
+++ src/toolkit/Core/Vox.cpp
@@ -0,0 +1,21 @@
+/*
+ * Vox.cpp
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#include "Vox.h"
+
+namespace Core {
+
+    Vox::Vox() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+    Vox::~Vox() {
+        // TODO Auto-generated destructor stub
+    }
+
+} /* namespace Core */
---
+++ src/toolkit/Core/Vox.h
@@ -0,0 +1,24 @@
+/*
+ * Vox.h
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#ifndef SRC_TOOLKIT_CORE_VOX_H_
+#define SRC_TOOLKIT_CORE_VOX_H_
+
+namespace Core {
+
+    /*
+     *
+     */
+    class Vox {
+        public:
+            Vox();
+            virtual ~Vox();
+    };
+
+} /* namespace Core */
+
+#endif /* SRC_TOOLKIT_CORE_VOX_H_ */
