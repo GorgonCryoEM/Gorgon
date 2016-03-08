53515809bac10a39a0c115e0562d92ad3b2dc41e
shadow_walker <shadowwalkersb@gmail.com>
2016-03-08 13:39:09 -0600

    draw a simple sphere

---
+++ bin/Explorer/sphere.py
@@ -0,0 +1,26 @@
+from PyQt4 import QtGui, QtCore, QtOpenGL
+from base_viewer import BaseViewer
+from libpytoolkit import Display
+from libpytoolkit import RendererBase
+from libpytoolkit import Vec3F
+from .libs import Vec3
+
+# from volume_surface_editor_form import VolumeSurfaceEditorForm
+# from model_visualization_form import ModelVisualizationForm
+# from string import split, upper
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+
+class Sphere(BaseViewer):
+    
+    def __init__(self, main, parent=None):
+        super(Sphere, self).__init__(main, parent)
+        
+        self.renderer = Display()
+        self.s = RendererBase()
+        
+    def draw(self):
+        self.s.drawSphere(Vec3(10., 10., 10.), 20.)
