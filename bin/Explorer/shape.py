91cde136f566e7fa05ffccfc556cf3752324dd20
shadow_walker <shadowwalkersb@gmail.com>
2016-03-20 21:50:05 -0500

    cleanup imports

--- bin/Explorer/shape.py
+++ bin/Explorer/shape.py
@@ -2,7 +2,6 @@ from PyQt4 import QtGui, QtCore, QtOpenGL
 from base_viewer import BaseViewer
 from libpytoolkit import Display
 from libpytoolkit import RendererBase
-from libpytoolkit import Vec3F
 from .libs import Vec3
 
 from OpenGL.GL import *
