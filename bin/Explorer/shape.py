9084c8c71e8a322eb1beb9f7ee50b1217ac7c794
shadow_walker <shadowwalkersb@gmail.com>
2016-03-20 19:50:38 -0500

    new class: Shape

---
+++ bin/Explorer/shape.py
@@ -0,0 +1,18 @@
+from PyQt4 import QtGui, QtCore, QtOpenGL
+from base_viewer import BaseViewer
+from libpytoolkit import Display
+from libpytoolkit import RendererBase
+from libpytoolkit import Vec3F
+from .libs import Vec3
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+
+class Shape(BaseViewer):
+
+    def __init__(self, main):
+        super(Shape, self).__init__(main)
+        
+        self.renderer = Display()
