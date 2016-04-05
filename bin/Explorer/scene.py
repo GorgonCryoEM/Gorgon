226b71edc973ab723958d5af19cd795232a90aa9
shadow_walker <shadowwalkersb@gmail.com>
2016-04-05 15:23:33 -0500

    comment out primitives

--- bin/Explorer/scene.py
+++ bin/Explorer/scene.py
@@ -1,7 +1,7 @@
 from .libs import Vec3
-from line import Line
-from line0 import Line0
-from dot import Dot
+# from line import Line
+# from line0 import Line0
+# from dot import Dot
 from volume_viewer import VolumeViewer
 
 
@@ -14,17 +14,17 @@ class Scene:
 
         self.shapes = [self.volumeViewer]
         
-        self.linecom = Line0(Vec3(100,100,100))
-        self.linecom.setColor(40, 70, 50, 150)
-        self.lineaxis = Line0(Vec3(100,100,100))
-        self.lineaxis.setColor(80, 40, 50, 150)
-        self.lineaxis.depthEnabled = True
-        self.dotcenter = Dot()
-        self.dotcom = Dot()
-        self.dotcom.setColor(0, 130, 0, 150)
-        self.shapes.append(self.lineaxis)
-        self.shapes.append(self.dotcenter)
-        self.shapes.append(self.dotcom)
+#         self.linecom = Line0(Vec3(100,100,100))
+#         self.linecom.setColor(40, 70, 50, 150)
+#         self.lineaxis = Line0(Vec3(100,100,100))
+#         self.lineaxis.setColor(80, 40, 50, 150)
+#         self.lineaxis.depthEnabled = True
+#         self.dotcenter = Dot()
+#         self.dotcom = Dot()
+#         self.dotcom.setColor(0, 130, 0, 150)
+#         self.shapes.append(self.lineaxis)
+#         self.shapes.append(self.dotcenter)
+#         self.shapes.append(self.dotcom)
 
     def add(self, shape):
         self.shapes.append(shape)
