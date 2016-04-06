d7c930bc0586c2391597eff703abff48cbdb7d74
shadow_walker <shadowwalkersb@gmail.com>
2016-04-06 13:20:30 -0500

    remove createActions

--- bin/Explorer/volume_surface_editor_form.py
+++ bin/Explorer/volume_surface_editor_form.py
@@ -22,7 +22,6 @@ class VolumeSurfaceEditorForm(BaseDockWidget):
         self.viewer = volumeViewer
         self.connect(self.viewer, QtCore.SIGNAL("modelLoadedPreDraw()"), self.modelLoadedPreDraw)
         self.createUI()
-        self.createActions()
 
     def createUI(self):
         self.ui = Ui_DialogVolumeSurfaceEditor()
@@ -42,8 +41,3 @@ class VolumeSurfaceEditorForm(BaseDockWidget):
         self.displayAct.setEnabled(True)
         self.showWidget(True)
         self.viewer.renderer.enableDraw(True)
-        
-    def createActions(self):
-        self.displayAct.setEnabled(False)
-    
-        
