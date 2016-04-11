d3b593e51f05587f959f13428fd1b0e32b70f88a
shadow_walker <shadowwalkersb@gmail.com>
2016-04-11 00:08:42 -0500

    refactor var declarations

--- src/toolkit/Visualization/DisplayBase.cpp
+++ src/toolkit/Visualization/DisplayBase.cpp
@@ -148,13 +148,10 @@ namespace Visualization {
         //        extern int aiCubeEdgeFlags[256];
         //        extern int a2iTriangleConnectionTable[256][16];
 
-        int iVertex, iFlagIndex, iEdgeFlags;
-        float fOffset;
-        float afCubeValue[8];
-        Vec3D asEdgeVertex[12];
-        int vertexIds[12];
 
         //Make a local copy of the values at the cube's corners
+        float afCubeValue[8];
+
         for(int iVertex = 0; iVertex < 8; iVertex++) {
             afCubeValue[iVertex] = vol.getVoxelData(iX + a2iVertexOffset[iVertex][0]*iScale,
                     iY + a2iVertexOffset[iVertex][1]*iScale,
@@ -162,7 +159,7 @@ namespace Visualization {
         }
 
         //Find which vertices are inside of the surface and which are outside
-        iFlagIndex = 0;
+        int iFlagIndex = 0;
         for(int i = 0; i < 8; i++)
         {
             if(afCubeValue[i] <= iso_level)
@@ -170,7 +167,7 @@ namespace Visualization {
         }
 
         //Find which edges are intersected by the surface
-        iEdgeFlags = aiCubeEdgeFlags[iFlagIndex];
+        int iEdgeFlags = aiCubeEdgeFlags[iFlagIndex];
 
         //If the cube is entirely inside or outside of the surface, then there will be no intersections
         if(iEdgeFlags == 0)
@@ -180,12 +177,15 @@ namespace Visualization {
 
         //Find the point of intersection of the surface with each edge
         //Then find the normal to the surface at those points
+        Vec3D asEdgeVertex[12];
+        int vertexIds[12];
+
         for(int i = 0; i < 12; i++)
         {
             //if there is an intersection on this edge
             if(iEdgeFlags & (1<<i))
             {
-                fOffset = vol.getOffset(afCubeValue[ a2iEdgeConnection[i][0] ], afCubeValue[ a2iEdgeConnection[i][1] ], iso_level);
+                float fOffset = vol.getOffset(afCubeValue[ a2iEdgeConnection[i][0] ], afCubeValue[ a2iEdgeConnection[i][1] ], iso_level);
 
                 asEdgeVertex[i][0] = (float)iX + ((float)a2iVertexOffset[ a2iEdgeConnection[i][0] ][0] +  fOffset * (float)a2iEdgeDirection[i][0]) * (float)iScale;
                 asEdgeVertex[i][1] = (float)iY + ((float)a2iVertexOffset[ a2iEdgeConnection[i][0] ][1] +  fOffset * (float)a2iEdgeDirection[i][1]) * (float)iScale;
@@ -204,7 +204,7 @@ namespace Visualization {
             TriangleMeshFace triangleVertices;
             for(int j = 0; j < 3; j++)
             {
-                iVertex = a2iTriangleConnectionTable[iFlagIndex][3*i+j];
+                int iVertex = a2iTriangleConnectionTable[iFlagIndex][3*i+j];
                 triangleVertices[j] = vertexIds[iVertex];
             }
 
