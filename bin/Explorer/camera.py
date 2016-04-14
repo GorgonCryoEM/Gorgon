fbfafd8e2bfc4ee9aea8c095e3d6bfb738183856
shadow_walker <shadowwalkersb@gmail.com>
2016-04-14 00:10:07 -0500

    remove signal: modelUpdated in Camera

--- bin/Explorer/camera.py
+++ bin/Explorer/camera.py
@@ -61,7 +61,6 @@ class Camera(QtOpenGL.QGLWidget):
             s.visualizationUpdated.connect(self.updateGL)
             s.centerRequested.connect(self.sceneSetCenterLocal)
             s.centerAllRequested.connect(self.sceneSetCenter)
-            self.connect(s, QtCore.SIGNAL("modelChanged()"), self.modelChanged)
             self.connect(s, QtCore.SIGNAL("modelLoaded()"), self.modelChanged)
             self.connect(s, QtCore.SIGNAL("modelUnloaded()"), self.modelChanged)
             self.connect(s, QtCore.SIGNAL("modelVisualizationChanged()"), self.modelChanged)
