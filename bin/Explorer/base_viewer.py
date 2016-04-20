d254f64606db9cce4f3d145503139442335a827f
shadow_walker <shadowwalkersb@gmail.com>
2016-04-20 13:08:17 -0500

    draw with 3 subindex

--- bin/Explorer/base_viewer.py
+++ bin/Explorer/base_viewer.py
@@ -132,16 +132,17 @@ class BaseViewer(BaseDockWidget):
         glEndList()
 
     def drawGL(self):
-        ss=0
-        if(self.color.alpha() < 255):
-            glDepthFunc(GL_LESS)
-            glColorMask(False, False, False, False)
-            self.renderer.draw(ss, False)
-            glDepthFunc(GL_LEQUAL)
-            glColorMask(True, True, True, True)
-            self.renderer.draw(ss, self.selectEnabled or self.mouseMoveEnabled)
-        else:
-            self.renderer.draw(ss, self.selectEnabled or self.mouseMoveEnabled)
+        ss=[0,1,2]
+        for s in ss:
+            if(self.color.alpha() < 255):
+                glDepthFunc(GL_LESS)
+                glColorMask(False, False, False, False)
+                self.renderer.draw(s, False)
+                glDepthFunc(GL_LEQUAL)
+                glColorMask(True, True, True, True)
+                self.renderer.draw(s, self.selectEnabled or self.mouseMoveEnabled)
+            else:
+                self.renderer.draw(s, self.selectEnabled or self.mouseMoveEnabled)
 
     def setMaterials(self):
         color = self.color
