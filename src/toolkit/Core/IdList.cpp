213d2bea122c35c16aefb800e336da01e7ec6e02
shadow_walker <shadowwalkersb@gmail.com>
2016-04-27 20:21:21 -0500

    update IdList to accommodate Edge

--- src/toolkit/Core/IdList.cpp
+++ src/toolkit/Core/IdList.cpp
@@ -21,6 +21,18 @@ namespace Core {
         ids.push_back(i);
     }
 
+    IdList::IdList(TKey i1, TKey i2)
+            : ids(2)
+    {
+        ids[0] = i1;
+        ids[1] = i2;
+    }
+
+
+    TKey IdList::id(int i) const {
+        return ids[i];
+    }
+
     ostream& operator<<(ostream& out, const IdList& obj) {
         return out //<<"\033[34m"
                     << "\n\nvertexIds.size(): " << obj.ids.size()
