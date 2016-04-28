2652b40646a231de6c93b95706813d8bd83f3ac4
shadow_walker <shadowwalkersb@gmail.com>
2016-04-28 15:55:00 -0500

    cmake

--- src/cmake/explorer.cmake
+++ src/cmake/explorer.cmake
@@ -1,6 +1,6 @@
 add_custom_target_wrapper(TARGET Explorer
         PROGRAMS ${CMAKE_SOURCE_DIR}/bin/explorer.py
-        DIRECTORY ${CMAKE_SOURCE_DIR}/bin/Explorer
+        DIRECTORY ${CMAKE_SOURCE_DIR}/bin/Explorer ${CMAKE_SOURCE_DIR}/bin/ToolkitGui
         DESTINATIONS ${CMAKE_BINARY_DIR}/bin
         COMPONENT "Explorer"
         DEPENDS Hello Toolkit Programs
