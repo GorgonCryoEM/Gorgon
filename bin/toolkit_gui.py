2c70d149a33bae1c3edadd83594765e4610d489e
shadow_walker <shadowwalkersb@gmail.com>
2016-04-29 21:59:58 -0500

    STASH

--- bin/toolkit_gui.py
+++ bin/toolkit_gui.py
@@ -24,7 +24,8 @@ def main():
 
     app = QtGui.QApplication(sys.argv)
 
-    windows = [MainWindowForm('2.2.2', args), MainWindowForm2('2.2.3', args)]
+#     windows = [MainWindowForm('2.2.2', args), MainWindowForm2('2.2.3', args)]
+    windows = []
     windows.append(MainWindowForm3('2.2.4', args))
 #     window = MainWindowForm('2.2.2', args)
     
