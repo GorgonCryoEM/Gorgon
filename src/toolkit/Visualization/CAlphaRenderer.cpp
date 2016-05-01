eac40a543b00d028babf678b9d62bc1fe50d05de
shadow_walker <shadowwalkersb@gmail.com>
2016-04-30 20:42:17 -0500

    deactivate missing

--- src/toolkit/Visualization/CAlphaRenderer.cpp
+++ src/toolkit/Visualization/CAlphaRenderer.cpp
@@ -1078,7 +1078,7 @@ namespace Visualization {
             }
         }
         if(count == 0) {
-            centerOfMass = Display::SelectionCenterOfMass();
+//            centerOfMass = Display::SelectionCenterOfMass();
         } else {
             centerOfMass = centerOfMass * (1.0f/(float)count);
         }
@@ -1142,7 +1142,7 @@ namespace Visualization {
     }
 
     void CAlphaRenderer::SelectionToggle(int subsceneIndex, bool forceTrue, int ix0, int ix1, int ix2, int ix3, int ix4) {
-        Display::SelectionToggle(subsceneIndex, forceTrue, ix0, ix1, ix2, ix3, ix4);
+//        Display::SelectionToggle(subsceneIndex, forceTrue, ix0, ix1, ix2, ix3, ix4);
         AtomMapType::iterator it;
         PDBAtom * a;
         if (subsceneIndex == 0) {
