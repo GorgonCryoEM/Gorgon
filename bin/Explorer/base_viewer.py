6fa010dff3db0bb8fc725446f2000edbd515f582
shadow_walker <shadowwalkersb@gmail.com>
2016-05-03 23:39:36 -0500

    revert: world object conversions

--- bin/Explorer/base_viewer.py
+++ bin/Explorer/base_viewer.py
@@ -272,6 +272,28 @@ class BaseViewer(BaseDockWidget):
             self.mouseMoveEnabledRay = value
             self.emitMouseTrackingChanged()
 
+    def objectToWorldCoordinates(self, objectCoords):
+        #Need to apply rotations
+        origin = [self.renderer.getOriginX(), self.renderer.getOriginY(), self.renderer.getOriginZ()]
+        scale = [self.renderer.getSpacingX(), self.renderer.getSpacingY(), self.renderer.getSpacingZ()]
+
+        return Vec3([objectCoords[i] * scale[i] + origin[i] for i in range(3)])
+    
+    def worldToObjectCoordinates(self, worldCoords):
+        origin = [self.renderer.getOriginX(), self.renderer.getOriginY(), self.renderer.getOriginZ()]
+        scale = [self.renderer.getSpacingX(), self.renderer.getSpacingY(), self.renderer.getSpacingZ()]
+        
+        return Vec3([(worldCoords[i] - origin[i]) / scale[i] for i in range(3)])
+
+    def objectVectorToWorldCoordinates(self, objectCoords):
+        #Need to apply rotations
+        scale = [self.renderer.getSpacingX(), self.renderer.getSpacingY(), self.renderer.getSpacingZ()]
+        return Vec3([objectCoords[i] * scale[i] for i in range(3)])
+    
+    def worldVectorToObjectCoordinates(self, worldCoords):
+        scale = [self.renderer.getSpacingX(), self.renderer.getSpacingY(), self.renderer.getSpacingZ()]
+        return Vec3([worldCoords[i] / scale[i] for i in range(3)])
+        
     def objectToWorldCoordinatesVector(self, objectCoords):
         coords = self.objectToWorldCoordinates([objectCoords.x(), objectCoords.y(), objectCoords.z()])
         return Vector3Float(coords[0], coords[1], coords[2])
