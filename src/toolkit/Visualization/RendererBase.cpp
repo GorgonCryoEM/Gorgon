8082d622ddbaf1f9f07be8f4df7bcc0a1b6c7e79
shadow_walker <shadowwalkersb@gmail.com>
2016-05-09 14:16:49 -0500

    1

--- src/toolkit/Visualization/RendererBase.cpp
+++ src/toolkit/Visualization/RendererBase.cpp
@@ -10,5 +10,27 @@
 using namespace Visualization;
 
 RendererBase::RendererBase()
+        : IsoSurface((Volume)(*this))
 {
+}
+
+float RendererBase::getMinPos(int i) const {
+    return 0.0;
+}
+
+float RendererBase::getMaxPos(int i) const {
+    float result;
+    switch(i) {
+        case 0:
+            result = float(getSizeX()-1);
+            break;
+        case 1:
+            result = float(getSizeY()-1);
+            break;
+        case 2:
+            result = float(getSizeZ()-1);
+            break;
+    }
+
+    return result;
 }
--- src/toolkit/Visualization/RendererBase.h
+++ src/toolkit/Visualization/RendererBase.h
@@ -9,12 +9,18 @@
 #define SRC_TOOLKIT_VISUALIZATION_RENDERERBASE_H_
 
 #include "IsoSurface.h"
+#include <Core/volume.h>
+
+using namespace Core;
 
 namespace Visualization {
 
-    class RendererBase : public IsoSurface {
+    class RendererBase : public Volume, public IsoSurface {
         public:
             RendererBase();
+
+            virtual float getMinPos(int i) const;
+            virtual float getMaxPos(int i) const;
     };
 }
 
