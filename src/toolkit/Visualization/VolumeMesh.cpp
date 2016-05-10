c862df78717a855df64981f463673c7d455c3e6b
shadow_walker <shadowwalkersb@gmail.com>
2016-05-10 18:47:54 -0500

    VolumeMesh

---
+++ src/toolkit/Visualization/VolumeMesh.cpp
@@ -0,0 +1,21 @@
+/*
+ * VolumeMesh.cpp
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#include "VolumeMesh.h"
+
+namespace Core {
+
+    VolumeMesh::VolumeMesh() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+    VolumeMesh::~VolumeMesh() {
+        // TODO Auto-generated destructor stub
+    }
+
+} /* namespace Core */
---
+++ src/toolkit/Visualization/VolumeMesh.h
@@ -0,0 +1,23 @@
+/*
+ * VolumeMesh.h
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#ifndef SRC_TOOLKIT_VISUALIZATION_VOLUMEMESH_H_
+#define SRC_TOOLKIT_VISUALIZATION_VOLUMEMESH_H_
+
+#include "Mesh.h"
+
+namespace Core {
+
+    class VolumeMesh : public Volume, public Mesh {
+        public:
+            VolumeMesh();
+            virtual ~VolumeMesh();
+    };
+
+} /* namespace Core */
+
+#endif /* SRC_TOOLKIT_VISUALIZATION_VOLUMEMESH_H_ */
