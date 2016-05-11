7be9c80a9cbc4fa601c0efdfb41c634dcd8e83aa
shadow_walker <shadowwalkersb@gmail.com>
2016-05-10 19:07:54 -0500

    VolumeRenderer

---
+++ src/toolkit/Visualization/VolumeRenderer.cpp
@@ -0,0 +1,21 @@
+/*
+ * VolumeRenderer.cpp
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#include "VolumeRenderer.h"
+
+namespace Visualization {
+
+    VolumeRenderer::VolumeRenderer() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+    VolumeRenderer::~VolumeRenderer() {
+        // TODO Auto-generated destructor stub
+    }
+
+} /* namespace Visualization */
---
+++ src/toolkit/Visualization/VolumeRenderer.h
@@ -0,0 +1,24 @@
+/*
+ * VolumeRenderer.h
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
+
+#ifndef SRC_TOOLKIT_VISUALIZATION_VOLUMERENDERER_H_
+#define SRC_TOOLKIT_VISUALIZATION_VOLUMERENDERER_H_
+
+#include "RendererBase.h"
+#include "VolumeMesh.h"
+
+namespace Visualization {
+
+    class VolumeRenderer : public RendererBase {
+        public:
+            VolumeRenderer();
+            virtual ~VolumeRenderer();
+    };
+
+} /* namespace Visualization */
+
+#endif /* SRC_TOOLKIT_VISUALIZATION_VOLUMERENDERER_H_ */
