1d2cdaa308cf0d35c3a4b90dbc2f391f3bdcf68d
shadow_walker <shadowwalkersb@gmail.com>
2016-05-11 21:48:31 -0500

    use VolumeRenderer

--- bin/Explorer/base_viewer.py
+++ bin/Explorer/base_viewer.py
@@ -4,7 +4,6 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-from libpytoolkit import RendererBase
 from libpytoolkit import *
 from .libs import Vec3
 from .ui_common import Ui_Common
