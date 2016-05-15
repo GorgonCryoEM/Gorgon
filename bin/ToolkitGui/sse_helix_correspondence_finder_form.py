a672fe08da53ed2484417c39efbcb156e73aedfe
shadow_walker <shadowwalkersb@gmail.com>
2016-05-15 00:19:15 -0500

    fix members: correspondenceEngine and correspondenceLibrary

--- bin/ToolkitGui/sse_helix_correspondence_finder_form.py
+++ bin/ToolkitGui/sse_helix_correspondence_finder_form.py
@@ -31,6 +31,8 @@ class SSEHelixCorrespondenceFinderForm(QtGui.QDialog, SSEHelixCorrespondence):
                 
         args = self.app.args
         SSEHelixCorrespondence.__init__(self, args.skeleton, args.sequence, args.helix, args.output, False)
+        self.viewer.correspondenceEngine = self.correspondenceEngine
+        self.viewer.correspondenceLibrary = self.correspondenceLibrary
         
         dock = QtGui.QDockWidget("SSEHelixCorrespondenceFinder", self.app)
         dock.setWidget(self)
