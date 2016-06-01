7569ca6e8d661ed99ec9c3b413291748fcced0a4
shadow_walker <shadowwalkersb@gmail.com>
2016-05-31 21:39:40 -0500

    debug print

--- src/toolkit/Visualization/CAlphaRenderer.cpp
+++ src/toolkit/Visualization/CAlphaRenderer.cpp
@@ -55,6 +55,17 @@ namespace Visualization {
     void CAlphaRenderer::drawBackboneModel(int subSceneIndex, bool selectEnabled) {
         GLfloat emissionColor[4] = {1.0, 1.0, 1.0, 1.0};
 
+        selectEnabled = true;
+
+//        #ifdef GORGON_DEBUG
+              cout<<"\033[32mDEBUG: File:   CAlphaRenderer.cpp"<<endl;
+              cout<<"DEBUG: Method: CAlphaRenderer::drawBackboneModel(int, bool)\033[0m"<<endl;
+              cout<<"...subSceneIndex, selectEnabled...: "<<subSceneIndex<<", "<<selectEnabled<<endl;
+              cout<<"......atoms.size(): "<<atoms.size()<<endl;
+              cout<<"......bonds.size(): "<<bonds.size()<<endl;
+//        #endif
+
+
         if(subSceneIndex == 0) { // Drawing Atoms
             if(selectEnabled) {
                 atomHashKeys.clear();
@@ -77,6 +88,7 @@ namespace Visualization {
                         glLoadName(static_cast<GLuint>( atomHashKeys.size() - 1)); // the index of the element just added
                     }
                     if(1) {
+                        cout<<".........drawSphere: "<<it->second.GetPosition()<<"\t"<<it->second.GetAtomRadius()<<endl;
                         drawSphere(it->second.GetPosition(), it->second.GetAtomRadius() * 0.3);
                     }
 
@@ -113,6 +125,7 @@ namespace Visualization {
                 }
 
                 if(1) {
+                    cout<<".........drawCylinder: "<<atoms[bonds[i].GetAtom0Ix()].GetPosition()<<"\t"<<atoms[bonds[i].GetAtom1Ix()].GetPosition()<<endl;
                     drawCylinder(atoms[bonds[i].GetAtom0Ix()].GetPosition(), atoms[bonds[i].GetAtom1Ix()].GetPosition(), 0.1, 10, 2);
                 }
                 glPopAttrib();
@@ -124,6 +137,7 @@ namespace Visualization {
         } else if(subSceneIndex == 2) { // Drawing spheres to cover up the cylinder edges
             for(AtomMapType::iterator i = atoms.begin(); i != atoms.end(); i++) {
                 if(1) {
+                    cout<<".........drawSphere: "<<i->second.GetPosition()<<endl;
                     drawSphere(i->second.GetPosition(), 0.1);
                 }
             }
