31f9fa3b391e0668d5e0a572eb0a0ef561e3aabd
shadow_walker <shadowwalkersb@gmail.com>
2016-06-04 16:35:40 -0400

    format

--- src/toolkit/Readers/SkeletonReader.h
+++ src/toolkit/Readers/SkeletonReader.h
@@ -16,31 +16,20 @@ using namespace SkeletonMaker;
 
 namespace GraphMatch {
 
-            int getGraphIndex(vector<Shape*> & helixes, int helixNum,
-                                     int cornerNum);
-            int getGraphIndex(vector<Shape*> & helixes, int helixNum,
-                                     Point3Pair * point);
-            Graph * readFile(string volumeFile, string helixFile,
-                                    string sseFile, string sheetFile);
-            Volume* getSheetsNoThreshold(Volume * vol, int minSize);
-            void readSheetFile(string sheetFile,
-                                      vector<Shape*> & helixes);
-            void readHelixFile(string helixFile, string sseFile,
-                                      vector<Shape*> & helixes);
-            void findSizes(int startHelix, int startCell,
-                                  vector<Shape*> & helixList, Volume * vol,
-                                  Volume * coloredVol, Graph * graph);
-            void findPaths(Graph * graph);
-            void findPath(int startIx, int endIx,
-                                 vector<vector<Vec3I> > nodes,
-                                 Volume * maskVol, Graph * graph,
-                                 bool eraseMask);
-            void findCornerCellsInSheet(Volume * vol,
-                                               Volume * paintedVol,
-                                               vector<Shape*> & helixes,
-                                               int sheetId);
-            int isSkeletonSheet(const Volume &vol, int ox, int oy,
-                                       int oz);
+    int getGraphIndex(vector<Shape*> & helixes, int helixNum, int cornerNum);
+    int getGraphIndex(vector<Shape*> & helixes, int helixNum, Point3Pair * point);
+    Graph * readFile(string volumeFile, string helixFile, string sseFile, string sheetFile);
+    Volume* getSheetsNoThreshold(Volume * vol, int minSize);
+    void readSheetFile(string sheetFile, vector<Shape*> & helixes);
+    void readHelixFile(string helixFile, string sseFile, vector<Shape*> & helixes);
+    void findSizes(int startHelix, int startCell, vector<Shape*> & helixList,
+                   Volume * vol, Volume * coloredVol, Graph * graph);
+    void findPaths(Graph * graph);
+    void findPath(int startIx, int endIx, vector<vector<Vec3I> > nodes,
+                  Volume * maskVol, Graph * graph, bool eraseMask);
+    void findCornerCellsInSheet(Volume * vol, Volume * paintedVol,
+                                vector<Shape*> & helixes, int sheetId);
+    int isSkeletonSheet(const Volume &vol, int ox, int oy, int oz);
 
     // returns the graph node index for corner cornerNum of helix/sheet helixNum
     inline int getGraphIndex(vector<Shape*> & helixes, int helixNum, int cornerNum) {
