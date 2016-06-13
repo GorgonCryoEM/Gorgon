8ba4ff211483e85884a5649276b3d60f6976592c
shadow_walker <shadowwalkersb@gmail.com>
2016-06-13 12:15:58 -0500

    new import

--- bin/ToolkitGui/calpha_structure_editor.py
+++ bin/ToolkitGui/calpha_structure_editor.py
@@ -6,7 +6,7 @@ from libpytoolkit import PDBAtom, PDBBond
 # import math
 from Explorer.libs import *
 from .calpha_structure_editor_command_place_helix import CAlphaStructureEditorCommandPlaceHelix
-# from calpha_structure_editor_command_atom_placement import CAlphaStructureEditorCommandAtomPlacement
+from .calpha_structure_editor_command_atom_placement import CAlphaStructureEditorCommandAtomPlacement
 from .calpha_structure_editor_command_change_position import CAlphaStructureEditorCommandChangePosition
 
 
