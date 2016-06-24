a3250210802380e418b26e08053d6c1c1f4c9b24
shadow_walker <shadowwalkersb@gmail.com>
2016-06-24 14:26:59 -0500

    use in toolkit_gui

--- bin/toolkit_gui.py
+++ bin/toolkit_gui.py
@@ -13,6 +13,7 @@ from ToolkitGui import MainWindowForm
 from ToolkitGui import MainWindowForm2
 from ToolkitGui import MainWindowForm3
 from ToolkitGui import MainWindowForm4
+from ToolkitGui import MainWindowForm5
 
 
 def main():
@@ -22,6 +23,7 @@ def main():
     parser.add_argument('skeleton')
     parser.add_argument('sequence')
     parser.add_argument('helix')
+    parser.add_argument('calpha')
     parser.add_argument('output')
     args = parser.parse_args()
 
@@ -32,6 +34,7 @@ def main():
 #     windows.append(MainWindowForm2('2.2.3', args))
 #     windows.append(MainWindowForm3('2.2.4', args))
     windows.append(MainWindowForm4('2.2.5', args))
+    windows.append(MainWindowForm5('2.2.6', args))
 #     exit()
     
     for window in windows:
