292aef44b430ba0dcc18e48350886d2c1357545b
shadow_walker <shadowwalkersb@gmail.com>
2016-06-24 14:09:25 -0500

    new file

--- bin/ToolkitGui/__init__.py
+++ bin/ToolkitGui/__init__.py
@@ -2,3 +2,4 @@ from .main_window_form import MainWindowForm
 from .main_window_form2 import MainWindowForm2
 from .main_window_form3 import MainWindowForm3
 from .main_window_form4 import MainWindowForm4
+from .main_window_form5 import MainWindowForm5
---
+++ bin/ToolkitGui/main_window_form5.py
@@ -0,0 +1,77 @@
+from PyQt4 import QtCore, QtGui
+import sys, os
+
+from Explorer import Camera
+from Explorer.volume_viewer import VolumeViewer
+from Explorer.skeleton_viewer import SkeletonViewer
+from .sse_helix_correspondence_finder_form import SSEHelixCorrespondenceFinderForm
+from .calpha_viewer import CAlphaViewer
+from .sse_viewer import SSEViewer
+from Toolkit.sse.correspondence.StructurePrediction import StructurePrediction
+
+
+class MainWindowForm5(QtGui.QMainWindow):
+
+    def __init__(self, version, args):
+        super(MainWindowForm5, self).__init__()
+        
+        self.args = args
+        self.menubar = self.menuBar()
+        self.docksMenu = self.menubar.addMenu('&Docks')
+
+        self.structPred = StructurePrediction.load(self.args.sequence, self)
+        
+        self.viewers = {}
+        
+#         self.volumeViewer = VolumeViewer(self)
+        self.skeletonViewer = SkeletonViewer(self)
+        self.sseViewer    = SSEViewer(self)
+        self.calphaViewer = CAlphaViewer(self)
+#         self.viewers['volume'] = self.volumeViewer
+        self.viewers['skeleton'] = self.skeletonViewer
+        self.viewers['sse'] = self.sseViewer
+        self.viewers['calpha'] = self.calphaViewer
+        
+        self.scene = []
+#         self.scene.append(self.volumeViewer)
+        self.scene.append(self.skeletonViewer)
+        self.scene.append(self.sseViewer)
+        self.scene.append(self.calphaViewer)
+        
+        self.mainCamera = Camera(self.scene, self)
+        self.setCentralWidget(self.mainCamera)
+        
+        self.setWindowTitle(self.tr("Gorgon Toolkit - v" + version))
+        pathname = os.path.abspath(os.path.dirname(sys.argv[0]))
+        self.setWindowIcon(QtGui.QIcon(pathname + '/gorgon.ico'))
+#         exit()
+        
+    def load(self):
+        self.form = self.sseViewer.helixCorrespondanceFinder
+#         self.form.show()
+#
+#         self.volumeViewer.load(self.args.volume)
+        self.skeletonViewer.load(self.args.skeleton)
+        self.sseViewer.loadHelixDataFromFile(self.args.helix)
+        # self.calphaViewer.loadSeq(self.args.sequence)
+
+        self.form.ui.lineEditSkeletonFile.setText(self.args.skeleton)
+        self.form.ui.lineEditSequenceFile.setText(self.args.sequence)
+        self.form.ui.lineEditHelixLocationFile.setText(self.args.helix)
+#         self.form.loadDefaults()
+#         self.form.setConstants()
+# #         self.form.lineEditSheetLocationFile.setText()
+#         self.form.drawOverlay()
+        self.form.checkOk()
+        self.form.ui.pushButtonOk.setEnabled(True)
+        self.form.ui.tabWidget.setCurrentIndex(4)
+        self.form.accept()
+
+        # self.form.create_all_helices()
+
+#         self.form.viewer.sheetFileName    = QtCore.QString('groel-segment.seq')
+#         self.volumeViewer.load(self.args.volume)
+#         self.form.modelLoaded()
+        
+    def exitApplication(self):
+        QtGui.qApp.closeAllWindows()
