602723ad0addf2f011d9d92e9bf0f8788c1dbe56
shadow_walker <shadowwalkersb@gmail.com>
2016-07-03 09:08:27 -0500

    format space

--- bin/ToolkitGui/calpha/sequence/scrollable_view.py
+++ bin/ToolkitGui/calpha/sequence/scrollable_view.py
@@ -4,14 +4,14 @@ from view import CAlphaSequenceView
 
 class CAlphaScrollableSequenceView(QtGui.QScrollArea):
 
-    def __init__(self,structurePrediction, currentChainModel, parent=None):
-        super(CAlphaScrollableSequenceView,self).__init__()
-        self.seqView=CAlphaSequenceView(structurePrediction, currentChainModel,parent=self)
+    def __init__(self, structurePrediction, currentChainModel, parent=None):
+        super(CAlphaScrollableSequenceView, self).__init__()
+        self.seqView = CAlphaSequenceView(structurePrediction, currentChainModel, parent=self)
         seqView = self.seqView
-        seqView.updatePanelHeight() #This is needed to get all residues to show up in this widget.
-        #Note: updatePanelHeight also adjusts width - I'm guessing that is the part that fixes things.
-        seqView.scrollbar=self.horizontalScrollBar()
-        self.seqView=seqView
+        seqView.updatePanelHeight()  # This is needed to get all residues to show up in this widget.
+        # Note: updatePanelHeight also adjusts width - I'm guessing that is the part that fixes things.
+        seqView.scrollbar = self.horizontalScrollBar()
+        self.seqView = seqView
         self.setWidget(self.seqView)
 
         self.setWidgetResizable(False)
@@ -24,10 +24,10 @@ class CAlphaScrollableSequenceView(QtGui.QScrollArea):
         This resizes the CAlphaScrollableSequenceView's height based on the size of
         the CAlphaSequenceView.
         """
-        scrollbar=self.horizontalScrollBar()
-        scrollbarHeight=scrollbar.height()
-        widgetHeight=self.widget().height#()
-        #self.resize(QtCore.QSize(self.width(), scrollbarHeight+widgetHeight))
+        scrollbar = self.horizontalScrollBar()
+        scrollbarHeight = scrollbar.height()
+        widgetHeight = self.widget().height  # ()
+        # self.resize(QtCore.QSize(self.width(), scrollbarHeight+widgetHeight))
         self.resize(QtCore.QSize(self.width(), widgetHeight))
 
     def getSeqview(self):
