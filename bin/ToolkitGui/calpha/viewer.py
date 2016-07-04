425bdcd9a9b28ac4b79f2ca21e4a9aaf1c1357d1
shadow_walker <shadowwalkersb@gmail.com>
2016-07-04 01:05:53 -0500

    move 2

--- bin/ToolkitGui/calpha/viewer.py
+++ bin/ToolkitGui/calpha/viewer.py
@@ -88,31 +88,6 @@ class CAlphaViewer(BaseViewer):
             self.modelChanged()
 
     # Overridden
-    def getDrawColors(self):
-        if (self.displayStyle == self.DisplayStyleBackbone):
-            colors = [self.getAtomColor(), self.getBondColor(), self.getBondColor()]
-        elif (self.displayStyle == self.DisplayStyleRibbon):
-            colors = [self.getHelixColor(), self.getStrandColor(), self.getLoopColor()]
-        elif (self.displayStyle == self.DisplayStyleSideChain):
-            colors = [self.getAtomColor(), self.getBondColor(), self.getAtomColor()]
-        else:
-            colors = [None, None, None]
-        return colors
-    
-    # Overridden
-    def getDrawVisibility(self):
-        if (self.displayStyle == self.DisplayStyleBackbone):
-            visibility = [self.atomsVisible, self.bondsVisible, self.bondsVisible and (not self.atomsVisible)]
-        elif (self.displayStyle == self.DisplayStyleRibbon):
-            visibility = [self.helicesVisible, self.strandsVisible, self.loopsVisible]
-        elif (self.displayStyle == self.DisplayStyleSideChain):
-            visibility = [self.atomsVisible, self.bondsVisible, self.bondsVisible and (not self.atomsVisible)]
-        else:
-            visibility = [False, False, False]
-        visibility = [True, True, True]
-        return visibility
-
-    # Overridden
     def emitElementClicked(self, hitStack, event):
         print "emitElementClicked overridden: ", self.title, hitStack
         print "self.displayStyle: ", self.displayStyle
@@ -401,6 +376,31 @@ class CAlphaViewer(BaseViewer):
                     print self.currentMatch
                     self.emit(QtCore.SIGNAL("SSE selected"))
                     break
+
+    # Overridden
+    def getDrawColors(self):
+        if (self.displayStyle == self.DisplayStyleBackbone):
+            colors = [self.getAtomColor(), self.getBondColor(), self.getBondColor()]
+        elif (self.displayStyle == self.DisplayStyleRibbon):
+            colors = [self.getHelixColor(), self.getStrandColor(), self.getLoopColor()]
+        elif (self.displayStyle == self.DisplayStyleSideChain):
+            colors = [self.getAtomColor(), self.getBondColor(), self.getAtomColor()]
+        else:
+            colors = [None, None, None]
+        return colors
+
+    # Overridden
+    def getDrawVisibility(self):
+        if (self.displayStyle == self.DisplayStyleBackbone):
+            visibility = [self.atomsVisible, self.bondsVisible, self.bondsVisible and (not self.atomsVisible)]
+        elif (self.displayStyle == self.DisplayStyleRibbon):
+            visibility = [self.helicesVisible, self.strandsVisible, self.loopsVisible]
+        elif (self.displayStyle == self.DisplayStyleSideChain):
+            visibility = [self.atomsVisible, self.bondsVisible, self.bondsVisible and (not self.atomsVisible)]
+        else:
+            visibility = [False, False, False]
+        visibility = [True, True, True]
+        return visibility
 
     def setAtomColorsAndVisibility(self, displayStyle):
         if displayStyle == self.DisplayStyleBackbone:
