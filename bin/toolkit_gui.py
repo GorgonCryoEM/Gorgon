a844674efb9cd457a9aea322b758bdfa9dcbd1d5
shadow_walker <shadowwalkersb@gmail.com>
2016-07-08 11:52:53 -0500

    toolkit_gui

--- bin/toolkit_gui.py
+++ bin/toolkit_gui.py
@@ -32,8 +32,8 @@ def main():
     windows = []
 #     windows.append(MainWindowForm('2.2.2', args))
 #     windows.append(MainWindowForm2('2.2.3', args))
-#     windows.append(MainWindowForm3('2.2.4', args))
-    windows.append(MainWindowForm4('2.2.5', args))
+    windows.append(MainWindowForm3('2.2.4', args))
+    # windows.append(MainWindowForm4('2.2.5', args))
     # windows.append(MainWindowForm5('2.2.6', args))
 #     exit()
     
