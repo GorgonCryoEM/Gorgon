77e4a25cb556b13a170494d25b4e804b7de885e7
shadow_walker <shadowwalkersb@gmail.com>
2016-07-11 01:04:14 -0500

    STASH: load calphas

--- bin/ToolkitGui/main_window_form3.py
+++ bin/ToolkitGui/main_window_form3.py
@@ -56,7 +56,7 @@ class MainWindowForm3(QtGui.QMainWindow):
     def load(self):
         self.volumeViewer.load(self.args.volume)
         self.skeletonViewer.load(self.args.skeleton)
-        # self.calphaViewer.loadSSEHunterData('pseudoatoms.pdb')
+        self.calphaViewer.loadSSEHunterData('pseudoatoms_thr_20.pdb')
         self.form.modelLoaded()
 #         self.volumeViewer.renderer.printVertices()
         minDensity = self.volumeViewer.renderer.getMinDensity()
