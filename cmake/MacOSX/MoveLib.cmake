111ce51c235887e183db1f5fa3bd0dd0608579d2
shadow_walker <shadowwalkersb@gmail.com>
2016-08-20 01:21:51 -0500

    remove cmake/MacOSX/MoveLib.cmake

--- cmake/MacOSX/MoveLib.cmake
+++
@@ -1,6 +0,0 @@
-get_filename_component(trgt_file ${TRGT} NAME)
-
-execute_process(COMMAND ${CMAKE_COMMAND} -E rename
-                            ${PKGDIR}/Gorgon.app/Contents/Frameworks/${trgt_file}
-                            ${PKGDIR}/Gorgon.app/Contents/Resources/lib/python2.7/lib-dynload/${trgt_file}
-                )
