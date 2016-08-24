3af109439876e9167b7fc2fb0baafa446e3eba73
shadow_walker <shadowwalkersb@gmail.com>
2016-08-24 11:44:00 -0500

    refactor programs.cmake

--- src/cmake/programs.cmake
+++ src/cmake/programs.cmake
@@ -1,14 +1,8 @@
-
 file(GLOB progs "${CMAKE_SOURCE_DIR}/bin/*.py")
-
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/bin/Toolkit ${CMAKE_SOURCE_DIR}/bin/Core
-        DESTINATION ${CMAKE_BINARY_DIR}/bin
-        COMPONENT Programs
-#        DEPENDS Hello Toolkit
-        )
 
 add_custom_target_and_install_to_destinations(TARGET Programs
         PROGRAMS ${progs}
+        DIRECTORY ${CMAKE_SOURCE_DIR}/bin/Toolkit ${CMAKE_SOURCE_DIR}/bin/Core
         DESTINATIONS ${CMAKE_BINARY_DIR}/bin
         COMPONENT Programs
         DEPENDS Hello Toolkit #Core
