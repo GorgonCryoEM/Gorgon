9c097b67c60e76616cac2e4acd234802c5132cc5
shadow_walker <shadowwalkersb@gmail.com>
2016-10-05 10:57:06 -0500

    cmake: remove bin/Core

--- src/cmake/programs.cmake
+++ src/cmake/programs.cmake
@@ -5,7 +5,6 @@ add_custom_target_wrapper(TARGET Programs
         )
 install_to_destinations(
         PROGRAMS ${progs}
-        DIRECTORY ${CMAKE_SOURCE_DIR}/bin/Core
         DESTINATIONS ${CMAKE_BINARY_DIR}/bin
         COMPONENT Programs
         )
