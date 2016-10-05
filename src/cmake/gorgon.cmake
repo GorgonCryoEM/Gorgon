c20955de346d54817d91a13660d4d1ade4f5873c
shadow_walker <shadowwalkersb@gmail.com>
2016-10-05 18:43:07 -0500

    install gorgon into bin/ so scripts work in build directory

--- src/cmake/gorgon.cmake
+++ src/cmake/gorgon.cmake
@@ -2,8 +2,8 @@ add_custom_target_wrapper(TARGET Gorgon
         DEPENDS Toolkit
         )
 install_to_destinations(
-        DIRECTORY ${CMAKE_SOURCE_DIR}/gorgon
-        DESTINATIONS ${CMAKE_BINARY_DIR}
+        DIRECTORY ${CMAKE_SOURCE_DIR}/gorgon/
+        DESTINATIONS ${CMAKE_BINARY_DIR}/bin
         COMPONENT Gorgon
         )
         
