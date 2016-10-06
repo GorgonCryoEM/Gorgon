8a85dd00a7defcfb184f64aa82a35c19034db4a2
shadow_walker <shadowwalkersb@gmail.com>
2016-10-06 13:17:12 -0500

    move set var into top listfile

--- src/cmake/programs.cmake
+++ src/cmake/programs.cmake
@@ -1,6 +1,4 @@
 file(GLOB progs "${CMAKE_SOURCE_DIR}/cli/*.py")
-#set(prog_build_dir ${build_install_dir})
-set(prog_build_dir cli)
 
 add_custom_target_wrapper(TARGET Programs
         DEPENDS Toolkit
