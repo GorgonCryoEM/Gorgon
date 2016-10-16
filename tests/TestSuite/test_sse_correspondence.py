fa207f606fd72dfe66456d770d86799382abdd43
shadow_walker <shadowwalkersb@gmail.com>
2016-10-15 21:54:16 -0500

    rename file: sse_correspondence.py -> gorgon_sse_correspondence.py

--- tests/TestSuite/test_sse_correspondence.py
+++ tests/TestSuite/test_sse_correspondence.py
@@ -4,7 +4,7 @@ from .toolkit_tests import ToolkitTestCases
 class TestSSECorrespondence(ToolkitTestCases.ToolkitTests):
 
 	def __init__(self):
-		ToolkitTestCases.ToolkitTests.__init__(self, 'corr', prog_name='sse_correspondence.py')
+		ToolkitTestCases.ToolkitTests.__init__(self, 'corr', prog_name='gorgon_sse_correspondence.py')
 		
 	def get_inputs(self):
 		return [self.skeleton, self.segment, self.helices]
