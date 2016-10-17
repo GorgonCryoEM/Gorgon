a7b732b3966792b691227849aacc05551495c510
shadow_walker <shadowwalkersb@gmail.com>
2016-10-16 23:49:22 -0500

    change signatures, add _run(), _run_derived()

--- gorgon/toolkit/filter.py
+++ gorgon/toolkit/filter.py
@@ -3,32 +3,32 @@ from .operation import Operation
 
 class Filter(Operation):
 
-    def __init__(self, input, output):
-        super(Filter, self).__init__(input, output)
+    def __init__(self, parser):
+        super(Filter, self).__init__(parser)
 
 
 class Normalize(Filter):
 
-    def __init__(self, input, output, args=None):
-        super(Normalize, self).__init__(input, output)
+    def __init__(self, parser):
+        super(Normalize, self).__init__(parser)
 
-    def _run(self):
+    def _run(self, args):
         self.renderer.normalizeVolume()
 
 
 class LowPass(Filter):
 
-    def __init__(self, input, output, args=None):
-        super(LowPass, self).__init__(input, output)
+    def __init__(self, parser):
+        super(LowPass, self).__init__(parser)
         
-    def _run(self):
+    def _run(self, args):
         print "\033[31m""Sorry, NOT implemented!""\033[0m"
         
 
 class Gaussian(Filter):
 
-    def __init__(self, input, output, args=None):
-        super(Gaussian, self).__init__(input, output)
+    def __init__(self, parser):
+        super(Gaussian, self).__init__(parser)
         
-    def _run(self):
+    def _run(self, args):
         print "\033[31m""Sorry, NOT implemented!""\033[0m"
