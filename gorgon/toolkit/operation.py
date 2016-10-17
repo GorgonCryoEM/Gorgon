1969603e00288487bb5af24a1074e7117b489d4d
shadow_walker <shadowwalkersb@gmail.com>
2016-10-17 13:57:52 -0500

    initialize renderer in loadVolume

--- gorgon/toolkit/operation.py
+++ gorgon/toolkit/operation.py
@@ -13,12 +13,13 @@ class Operation(object):
 
         self.logger = logging.getLogger(__name__)
 
-        self.renderer = Volume()
-
     def _loadVolume(self, input):
+        self.renderer = Volume()
+    
         self.logger.debug(__file__)
         self.logger.debug("Operation._loadVolume")
         self.logger.debug(self.renderer)
+
         self.renderer.loadFile(input)
         self.logger.debug(self.renderer)
         self.logger.debug(self.renderer.getSize())
