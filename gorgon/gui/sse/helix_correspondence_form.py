a4458baf49430d3d0060fa3dc4667f7d6f1c80e9
shadow_walker <shadowwalkersb@gmail.com>
2016-10-31 22:47:11 -0500

    calphaViewer -> calpha

--- gorgon/gui/sse/helix_correspondence_form.py
+++ gorgon/gui/sse/helix_correspondence_form.py
@@ -689,10 +689,10 @@ class SSEHelixCorrespondenceForm(QtGui.QDialog):
                 print match
                 print match.predicted.startIndex
                 print match.predicted.stopIndex
-                create_helix(self.parent.calphaViewer.renderer, self.parent.structPred.chain, match)
+                create_helix(self.parent.calpha.renderer, self.parent.structPred.chain, match)
 
-        self.parent.calphaViewer.loaded = True
-        self.parent.calphaViewer.modelChanged()
+        self.parent.calpha.loaded = True
+        self.parent.calpha.modelChanged()
         self.parent.mainCamera.updateGL()
         
     def accept(self):
