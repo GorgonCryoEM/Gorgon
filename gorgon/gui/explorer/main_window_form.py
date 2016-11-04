71b8a0ff937179ce0d7eb29db0669b7ce9970c9f
shadow_walker <shadowwalkersb@gmail.com>
2016-11-04 16:51:39 -0500

    add class inheriting from Window

---
+++ gorgon/gui/explorer/main_window_form.py
@@ -0,0 +1,16 @@
+from ..window import Window
+
+
+class MainWindowForm(Window):
+
+    def __init__(self, args):
+        super(MainWindowForm, self).__init__(args, None)
+
+    @classmethod
+    def set_parser(cls, parser):
+        parser.description = "Gorgon Explorer"
+        parser.add_argument('volume')
+        parser.add_argument('skeleton')
+        parser.add_argument('sequence')
+        parser.add_argument('helix')
+        parser.add_argument('calpha')
