8042e545c12f10ff37a6302cf9f650987804cc3b
shadow_walker <shadowwalkersb@gmail.com>
2016-11-06 18:09:50 -0600

    activate sliders: setSegments(), setSlices()

--- gorgon/gui/explorer/common/atom_visualization_form.py
+++ gorgon/gui/explorer/common/atom_visualization_form.py
@@ -29,8 +29,8 @@ class AtomVisualizationForm(ModelVisualizationForm):
         # self.ui.checkBoxShowHelices.toggled.connect(self.viewer.setHelixVisibility)
         # self.ui.checkBoxShowStrands.toggled.connect(self.viewer.setStrandVisibility)
         # self.ui.checkBoxShowLoops.toggled.connect(self.viewer.setLoopVisibility)
-        # self.ui.hSliderSmoothness.valueChanged.connect(self.viewer.setSegments)
-        # self.ui.hSliderRoundedness.valueChanged.connect(self.viewer.setSlices)
+        self.ui.hSliderSmoothness.valueChanged.connect(self.viewer.setSegments)
+        self.ui.hSliderRoundedness.valueChanged.connect(self.viewer.setSlices)
                                                  
         
     def updateFromViewer(self):
