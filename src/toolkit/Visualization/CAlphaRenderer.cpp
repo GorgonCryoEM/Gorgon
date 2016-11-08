6cfc028b1946ea0e028c55802b499c47398984f9
shadow_walker <shadowwalkersb@gmail.com>
2016-11-07 21:53:03 -0600

    remove hard-coded displayStyle in CAlphaRenderer
    Closes #163.

--- src/toolkit/Visualization/CAlphaRenderer.cpp
+++ src/toolkit/Visualization/CAlphaRenderer.cpp
@@ -1109,7 +1109,6 @@ namespace Visualization {
 
     void CAlphaRenderer::selectionToggle(int subsceneIndex, bool forceTrue, int ix0, int ix1, int ix2, int ix3, int ix4) {
 //        RendererBase::SelectionToggle(subsceneIndex, forceTrue, ix0, ix1, ix2, ix3, ix4);
-        displayStyle=5;
 //        #ifdef GORGON_DEBUG
               cout<<"\033[32mDEBUG: File:   CAlphaRenderer.cpp"<<endl;
               cout<<"DEBUG: Method: CAlphaRenderer::selectionToggle(int, bool, int, int, int, int, int)\033[0m"<<endl;
