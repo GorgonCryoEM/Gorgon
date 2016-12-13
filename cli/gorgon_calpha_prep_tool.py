79dd191f3cc750d20402a2198e755299d8b70a6a
shadow_walker <shadowwalkersb@gmail.com>
2016-12-12 19:10:07 -0600

    debug prints

--- cli/gorgon_calpha_prep_tool.py
+++ cli/gorgon_calpha_prep_tool.py
@@ -32,10 +32,19 @@ def main():
     logger = logging.getLogger(__name__)
     
     seq = Chain.load(args.sequence)
+    logger.debug("seq: %s" % seq)
+    
     pdb = Chain.load(args.pdb)
+    logger.debug("\npdb:\n%s" % str(pdb))
+    logger.debug(pdb.residueRange())
+    logger.debug(pdb.getSequence())
 
     atoms = pdb.atoms
     residues = pdb.residueList
+    logger.debug(residues)
+    logger.debug(atoms)
+    logger.debug("%d %d" % (len(residues), len(atoms.keys())))
+    logger.debug("pdb.differenceIndex: %d" % pdb.differenceIndex)
 
     # Group consecutive letters into words and corresponding atom ids
     words = []
@@ -53,6 +62,11 @@ def main():
         
         words[-1] += str(residues[i_residue])
         ids_atom[-1].append(i_atom)
+        
+        logger.debug("%s %s %s %s" % (i_residue, i_atom, residues[i_residue], atoms[i_atom]))
+    
+    logger.debug(ids_atom)
+    logger.debug(words)
 
     # Add atoms corresponding to words extracted to the  sequence
     seq_string = str(seq)
@@ -60,9 +74,16 @@ def main():
     
     for i in range(len(words)):
         word = words[i]
+        logger.debug("---%s---" % word)
+        logger.debug(ids_atom[i])
         l = len(word)
         for j in range(low, len(seq_string)-l):
+            logger.debug("%s %s %s" % (i, word, seq_string[j:j+l]))
             if word == seq_string[j:j+l]:
+                logger.debug("Found %s %s" % (word, seq_string[j:j+l]))
+                logger.debug("   --- %s" % ids_atom[i])
+                logger.debug("   --- %s" % [atoms[k] for k in ids_atom[i]])
+                
                 for k in range(j+1, j+l+1):
                     seq[k].addAtomObject(atoms[ids_atom[i][k-j-1]])
                 low = j+l
