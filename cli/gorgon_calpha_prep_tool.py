455ec66e0f701dab148497c1410670460d789bfb
shadow_walker <shadowwalkersb@gmail.com>
2016-12-12 19:10:07 -0600

    group consecutive letters into words and corresponding atom ids

--- cli/gorgon_calpha_prep_tool.py
+++ cli/gorgon_calpha_prep_tool.py
@@ -34,6 +34,26 @@ def main():
     seq = Chain.load(args.sequence)
     pdb = Chain.load(args.pdb)
 
+    atoms = pdb.atoms
+    residues = pdb.residueList
+
+    # Group consecutive letters into words and corresponding atom ids
+    words = []
+    ids_atom = []
+    i_old = -1
+
+    for i in atoms:
+        i_atom = i
+        i_residue = i_atom + pdb.differenceIndex
+        
+        if i - i_old > 1:
+            words.append('')
+            ids_atom.append([])
+        i_old = i
+        
+        words[-1] += str(residues[i_residue])
+        ids_atom[-1].append(i_atom)
+
     seq.saveToPDB(args.output)
 
 if __name__ == "__main__":
