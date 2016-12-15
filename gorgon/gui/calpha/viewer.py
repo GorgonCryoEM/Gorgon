9c71b17f8069549e5a7ef170e68000d9a98536d1
shadow_walker <shadowwalkersb@gmail.com>
2016-12-15 16:18:37 -0600

    remove: getDrawColors(), getDrawVisibility(), closes #171

--- gorgon/gui/calpha/viewer.py
+++ gorgon/gui/calpha/viewer.py
@@ -648,28 +648,3 @@ class CAlphaViewer(BaseViewer):
         self.strandsVisible = visible
         self.setAtomColorsAndVisibility(self.displayStyle)
         self.modelChanged()
-
-    # Overridden
-    def getDrawColors(self):
-        if (self.displayStyle == self.DisplayStyleBackbone):
-            colors = [self.getAtomColor(), self.getBondColor(), self.getBondColor()]
-        elif (self.displayStyle == self.DisplayStyleRibbon):
-            colors = [self.getHelixColor(), self.getStrandColor(), self.getLoopColor()]
-        elif (self.displayStyle == self.DisplayStyleSideChain):
-            colors = [self.getAtomColor(), self.getBondColor(), self.getAtomColor()]
-        else:
-            colors = [None, None, None]
-        return colors
-    
-    # Overridden
-    def getDrawVisibility(self):
-        if (self.displayStyle == self.DisplayStyleBackbone):
-            visibility = [self.atomsVisible, self.bondsVisible, self.bondsVisible and (not self.atomsVisible)]
-        elif (self.displayStyle == self.DisplayStyleRibbon):
-            visibility = [self.helicesVisible, self.strandsVisible, self.loopsVisible]
-        elif (self.displayStyle == self.DisplayStyleSideChain):
-            visibility = [self.atomsVisible, self.bondsVisible, self.bondsVisible and (not self.atomsVisible)]
-        else:
-            visibility = [False, False, False]
-        visibility = [True, True, True]
-        return visibility
