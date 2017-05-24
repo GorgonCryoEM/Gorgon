f410125fb8adc366ba4c5d659a069c821b82949a
danzeng8 <danzeng8@gmail.com>
2017-05-24 16:43:51 -0500

    can now view two pdb files in mainwindowform, volume now optional

--- gorgon/gui/explorer/main_window_form.py
+++ gorgon/gui/explorer/main_window_form.py
@@ -10,16 +10,26 @@ class MainWindowForm(Window):
 
     def __init__(self, args):
         super(MainWindowForm, self).__init__(args, None)
-
-        self.volume = VolumeViewer(self)
+        if args.volume != "none":
+                self.volume = VolumeViewer(self)
         self.skeleton = SkeletonViewer(self)
         self.sse    = SSEViewer(self)
         
         self.hasSemiAtomicPlacementForm = False
         self.structPred = StructurePrediction.load(self.args.calpha, self)
         self.calpha = CAlphaViewer(self)
-
-        self.mainCamera.append_scenes([self.skeleton, self.volume, self.sse, self.calpha])
+        if args.calpha2 != None:
+            self.calpha2 = CAlphaViewer(self)
+        if args.volume != "none":
+            if args.calpha2 != None:
+                self.mainCamera.append_scenes([self.skeleton, self.volume, self.sse, self.calpha, self.calpha2])
+            else:
+                self.mainCamera.append_scenes([self.skeleton, self.volume, self.sse, self.calpha])
+        else:
+            if args.calpha2 != None:
+                self.mainCamera.append_scenes([self.skeleton, self.sse, self.calpha, self.calpha2])
+            else:
+                self.mainCamera.append_scenes([self.skeleton, self.sse, self.calpha])
 
     @classmethod
     def set_parser(cls, parser):
@@ -29,11 +39,15 @@ class MainWindowForm(Window):
         parser.add_argument('sequence')
         parser.add_argument('helix')
         parser.add_argument('calpha')
+        parser.add_argument('--calpha2', action="store")
 
     def load(self):
         super(MainWindowForm, self).load()
-
-        self.volume.load(self.args.volume)
-        self.skeleton.load(self.args.skeleton)
+        if self.args.volume != "none":
+            self.volume.load(self.args.volume)
+        self.skeleton.skeletonFile = self.args.skeleton
+        self.skeleton.loadFile2()
         self.sse.loadHelixDataFromFile(self.args.helix)
         self.calpha.loadData(self.args.calpha)
+        if self.args.calpha2 != None:
+            self.calpha2.loadData(self.args.calpha2)
